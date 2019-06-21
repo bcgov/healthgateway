b13e1ab29fc4650ff3922de214fdfe46a817676d
Manuel Rodriguez <marobej@gmail.com>
2019-06-21 13:29:24 -0700

    Reverting changes to master. New dev will go to Dev.
    Revert "Adding the initial WebClient and FHIR server implementations"
    
    This reverts commit 64b5f41695e133d909ebe8dd24ab772b26b84e30.

--- Apps/WebClient/ClientApp/DataRepositories/ITestApi.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-public interface ITestApi
-{
-    Task<List<PatientData>> GetPatients();
-}
--- Apps/WebClient/ClientApp/Models/PatientData.cs
+++
@@ -1,7 +0,0 @@
-public class PatientData
-{
-    public string Name { get; set; }
-    public string Id { get; set; }
-
-    public string DivStruct { get; set; }
-}
