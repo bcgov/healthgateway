6239b8765ff8efeb4cbf272c2d585af7f2a547fa
Tiago <tiago.graf@gmail.com>
2019-07-04 09:53:31 -0700

    Initial jest configuration

---
+++ Apps/WebClient/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  globals: {
+    "ts-jest": {
+      skipBabel: true,
+    },
+  },
+  mapCoverage: true,
+  moduleFileExtensions: ["js", "ts"],
+  testResultsProcessor: "jest-sonar-reporter",
+  transform: {
+    "^.+\\.ts$": "<rootDir>/node_modules/ts-jest/preprocessor.js",
+  },
+  testMatch: ["<rootDir>/**/*.test.ts"],
+};
---
+++ Apps/WebClient/src/ClientApp/sum.ts
@@ -0,0 +1,4 @@
+export default function sum(f:number, s:number)
+{
+  return f + s;
+}
