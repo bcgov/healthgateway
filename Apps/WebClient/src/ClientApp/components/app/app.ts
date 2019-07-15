c448ff4b50d7546af86e841b97f81e0bed8e560b
Manuel Rodriguez <marobej@gmail.com>
2019-07-15 10:26:00 -0700

    Updated to Webpack 4 and added related dependencies

--- Apps/WebClient/src/ClientApp/components/app/app.ts
+++ Apps/WebClient/src/ClientApp/components/app/app.ts
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 
+import menu from '../navmenu/navmenu.vue';
+
 @Component({
     components: {
-        MenuComponent: require('../navmenu/navmenu.vue.html')
+        MenuComponent: menu
     }
 })
 export default class AppComponent extends Vue {
