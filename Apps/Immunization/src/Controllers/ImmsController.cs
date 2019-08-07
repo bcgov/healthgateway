1da5b07cd2b7f1e14a276301cf3df4f5aa7b4b4c
Tiago <tiago.graf@gmail.com>
2019-08-07 10:54:43 -0700

    Add test imms controller

---
+++ Apps/Immunization/src/Controllers/ImmsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Immunization.Controllers
+{
+    [Route("v1/api/[controller]")]
+    [ApiController]
+    public class ImmsController : ControllerBase
+    {
+    }
+}
