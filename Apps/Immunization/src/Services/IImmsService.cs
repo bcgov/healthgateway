2a3fbeed976a6b487adfbfca3580e534e8e7e327
Tiago Graf <tiago.graf@gmail.com>
2019-08-12 13:49:03 -0700

    Add Unit Tests to Imms Service

---
+++ Apps/Immunization/src/Services/IImmsService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+using HealthGateway.Engine.Core;
+using HealthGateway.Models;
+
+namespace HealthGateway.Service
+{
+    /// <summary>
+    /// The Immunization data service.
+    /// </summary>
+    public interface IImmsService
+    {
+        /// <summary>
+        /// Gets a list of mock immunization records.
+        /// </summary>
+        /// <returns>A list of ImmsDataModel object</returns>
+        IEnumerable<ImmsDataModel> GetMockData();
+    }
+}
---
+++ Apps/Immunization/test/Services.Test/ImmsService_Test.cs
@@ -0,0 +1,26 @@
+using Xunit;
+using HealthGateway.Service;
+using HealthGateway.Engine.Core;
+using HealthGateway.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HealthGateway.Immunization.Test.Controller
+{
+    public class ImmsService_Test
+    {
+        [Fact]
+        public void Should_GetMockData()
+        {
+            const byte expectedCount = 17;
+            // Create service to test
+            IImmsService service = new ImmsService();
+
+            Key key = Key.Create("Patient");
+            List<ImmsDataModel> actualResult = service.GetMockData().ToList();
+
+            // Verify the result
+            Assert.Equal(expectedCount, actualResult.Count);
+        }
+    }
+}
