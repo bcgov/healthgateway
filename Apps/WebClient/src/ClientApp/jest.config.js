91137eea452b5312753e66adbdb15123a3b9ad92
Manuel Rodriguez <marobej@gmail.com>
2019-08-13 11:22:21 -0700

    Minor formatting issue

--- Apps/WebClient/src/ClientApp/jest.config.js
+++ Apps/WebClient/src/ClientApp/jest.config.js
@@ -1,38 +1,30 @@
 module.exports = {
   verbose: true,
-  name: 'Client app',
-  displayName: 'Client app',
-  moduleFileExtensions: [
-    "js",
-    "json",
-    "vue",
-    "ts",
-  ],
-  testResultsProcessor: 'jest-sonar-reporter',
+  name: "Client app",
+  displayName: "Client app",
+  moduleFileExtensions: ["js", "json", "vue", "ts"],
+  testResultsProcessor: "jest-sonar-reporter",
   transform: {
-    '^.+\\.(js)$': '<rootDir>/node_modules/babel-jest',
+    "^.+\\.(js)$": "<rootDir>/node_modules/babel-jest",
     "^.+\\.tsx?$": "ts-jest",
     "^.+\\.vue$": "vue-jest",
-    ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
+    ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
+      "jest-transform-stub"
   },
   moduleNameMapper: {
     "@/(.*)$": "<rootDir>/app/$1",
     "\\.(css)$": "<rootDir>/test/mocks/styleMock.js"
   },
-  transformIgnorePatterns: [
-    "/node_modules/"
-  ],
+  transformIgnorePatterns: ["/node_modules/"],
   collectCoverage: true,
   collectCoverageFrom: [
-    '**/*.{ts,vue}',
-    '!**/node_modules/**',
-    '!**/vendor/**',
+    "**/*.{ts,vue}",
+    "!**/node_modules/**",
+    "!**/vendor/**"
   ],
-  coverageDirectory: '<rootDir>/../../sonar_reports/jest.out',
-  testMatch: [
-    "**/test/**/*.test.(ts)|**/__tests__/*.(ts)"
-  ],
+  coverageDirectory: "<rootDir>/../../sonar_reports/jest.out",
+  testMatch: ["**/test/**/*.test.(ts)|**/__tests__/*.(ts)"],
   globals: {
-    "_NODE_ENV": "development"
+    _NODE_ENV: "development"
   }
-}
+};
