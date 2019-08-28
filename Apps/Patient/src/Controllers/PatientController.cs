8fa0ea51ccf59a2ffc453ad6a894f1803b6edbc5
Stephen Laws <stephen.laws@quartech.com>
2019-08-28 15:58:07 -0700

    Create project for Patient service

---
+++ Apps/Patient/src/Controllers/PatientController.cs
@@ -0,0 +1,47 @@
+﻿//-------------------------------------------------------------------------
+// Copyright © 2019 Province of British Columbia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-------------------------------------------------------------------------
+namespace HealthGateway.Controllers
+{
+    using System.Collections.Generic;
+    using HealthGateway.Models;
+    using HealthGateway.Service;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// The Immunization controller.
+    /// </summary>
+    [Authorize]
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/api/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        /// <summary>
+        /// Gets or sets the immunization data service.
+        /// </summary>
+        private readonly IPatientService service;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PatientController"/> class.
+        /// </summary>
+        /// <param name="svc">The immunization data service.</param>
+        public PatientController(IPatientService svc)
+        {
+            this.service = svc;
+        }
+    }
+}
