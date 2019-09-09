e75a5b1f912b4607169c9ed492b168eec2ee986f
Tiago <tiago.graf@gmail.com>
2019-09-09 15:54:56 -0700

    initial project configuration for HNClient

---
+++ Apps/HNClient/src/Startup.cs
@@ -0,0 +1,67 @@
+//-------------------------------------------------------------------------
+// Copyright © 2019 Province of British Columbia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-------------------------------------------------------------------------
+#pragma warning disable CA1303 //disable literal strings check
+namespace HealthGateway.HNClient
+{
+    using System.ServiceModel.Description;
+    using System.ServiceModel.Dispatcher;
+    using HealthGateway.Common.Startup;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// Configures the application during startup.
+    /// </summary>
+    public class Startup
+    {
+        private readonly StartupConfiguration startupConfig;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="env">The injected Environment provider.</param>
+        /// <param name="configuration">The injected configuration provider.</param>
+        /// <param name="logger">The injected logger provider.</param>
+        public Startup(IHostingEnvironment env, IConfiguration configuration, ILogger<Startup> logger)
+        {
+            this.startupConfig = new StartupConfiguration(configuration, env, logger);
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services">The injected services provider.</param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            this.startupConfig.ConfigureHttpServices(services);
+            this.startupConfig.ConfigureSwaggerServices(services);
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">The application builder.</param>
+        /// <param name="env">The hosting environment.</param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            this.startupConfig.UseSwagger(app);
+            this.startupConfig.UseHttp(app);
+        }
+    }
+}
