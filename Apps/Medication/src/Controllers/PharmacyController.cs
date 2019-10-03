eeeeed1460f7df52e93d9741e2b90dc19cae82a3
Tiago <tiago.graf@gmail.com>
2019-10-03 15:48:45 -0700

    Fixed namespace

--- Apps/Medication/src/Controllers/PharmacyController.cs
+++ Apps/Medication/src/Controllers/PharmacyController.cs
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //-------------------------------------------------------------------------
-namespace HealthGateway.MedicationService.Controllers
+namespace HealthGateway.Medication.Controllers
 {
     using System.Collections.Generic;
     using System.Threading.Tasks;
