fe6d7cd07a293e09e611d4417ccd54bdbcdba40f
Manuel Rodriguez <marobej@gmail.com>
2019-10-03 12:39:31 -0700

    Code review changes

--- Apps/WebClient/src/ClientApp/app/models/medicationStatement.ts
+++ Apps/WebClient/src/ClientApp/app/models/medicationStatement.ts
@@ -1,25 +1,25 @@
 // Medication statement model
 export default class MedicationStatement {
   // Drug Identification Number for the medication statement.
-  public DIN!: string;
+  public DIN?: string;
   // Brand name of the medication statement.
-  public brandName!: string;
+  public brandName?: string;
   // Common or generic name of the medication statement.
-  public genericName!: string;
+  public genericName?: string;
   // Quantity for the medication statement.
-  public quantity!: number;
+  public quantity?: number;
   // Medication statement dosage.
-  public dosage!: number;
+  public dosage?: number;
   // Medication statement prescription status.
-  public prescriptionStatus!: string;
+  public prescriptionStatus?: string;
   // Date the medication statement was dispensed.
-  public dispensedDate!: Date;
+  public dispensedDate?: Date;
   // Surname of the Practitioner who issued the medication statement.
-  public practitionerSurname!: string;
+  public practitionerSurname?: string;
   // Drug medication discontinued date, if applicable.
-  public drugDiscontinuedDate!: Date;
+  public drugDiscontinuedDate?: Date;
   // Medication statement directions.
-  public directions!: string;
+  public directions?: string;
   // Date the medication statement was entered.
-  public dateEntered!: Date;
+  public dateEntered?: Date;
 }
