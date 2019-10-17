dc32c7d6a8d679c7015d4b72b610be71a7137eda
Manuel Rodriguez <marobej@gmail.com>
2019-10-17 11:35:14 -0700

    Created console project

---
+++ Apps/DrugMaintainer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HealthGateway.DrugMaintainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
