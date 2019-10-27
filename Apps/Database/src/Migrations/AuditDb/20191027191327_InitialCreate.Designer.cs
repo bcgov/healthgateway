f89cd42c6ddebcd72aa1e9bebddf80b186d5e971
Stephen Laws <stephen.laws@nomostech.com>
2019-10-27 12:22:38 -0700

    cleanup

--- Apps/Database/src/Migrations/AuditDb/20191027191327_InitialCreate.Designer.cs
+++ Apps/Database/src/Migrations/AuditDb/20191027191327_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace HealthGateway.Database.Migrations.AuditDb
 {
     [DbContext(typeof(AuditDbContext))]
-    [Migration("20191027165917_InitialCreate")]
+    [Migration("20191027191327_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
