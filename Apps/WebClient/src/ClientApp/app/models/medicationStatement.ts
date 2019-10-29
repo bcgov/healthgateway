fae552760b62792f14449e7b1c90543fccb6c865
Tiago Graf <tiago.graf@gmail.com>
2019-10-29 14:51:12 -0700

    add pharmacy lookup to timeline

--- Apps/WebClient/src/ClientApp/app/models/medicationStatement.ts
+++ Apps/WebClient/src/ClientApp/app/models/medicationStatement.ts
@@ -3,20 +3,20 @@ import Pharmacy from "./pharmacy";
 
 // Medication statement model
 export default class MedicationStatement {
-  // The prescription identifier for this statment.
-  public prescriptionIdentifier?: string;
-  // Medication statement prescription status.
-  public prescriptionStatus?: string;
-  // Date the medication statement was dispensed.
-  public dispensedDate?: Date;
-  // Surname of the Practitioner who issued the medication statement.
-  public practitionerSurname?: string;
-  // Drug medication discontinued date, if applicable.
-  public directions?: string;
-  // Date the medication statement was entered.
-  public dateEntered?: Date;
-  // The medication of this MedicationStatement.
-  public medication: Medication = new Medication();
-  // The pharmacy where the medication was filled.
-  public pharmacy: Pharmacy = new Pharmacy();
+    // The prescription identifier for this statment.
+    public prescriptionIdentifier?: string;
+    // Medication statement prescription status.
+    public prescriptionStatus?: string;
+    // Date the medication statement was dispensed.
+    public dispensedDate?: Date;
+    // Surname of the Practitioner who issued the medication statement.
+    public practitionerSurname?: string;
+    // Drug medication discontinued date, if applicable.
+    public directions?: string;
+    // Date the medication statement was entered.
+    public dateEntered?: Date;
+    // The medication of this MedicationStatement.
+    public medication: Medication = new Medication();
+    // The pharmacy identifier.
+    public pharmacyId?: string;
 }
--- Apps/WebClient/src/ClientApp/app/services/interfaces.ts
+++ Apps/WebClient/src/ClientApp/app/services/interfaces.ts
@@ -32,7 +32,8 @@ export interface IPatientService {
 
 export interface IMedicationService {
   initialize(config: ExternalConfiguration, http: IHttpDelegate): void;
-  getPatientMedicationStatemens(hdid: string): Promise<RequestResult>;
+  getPatientMedicationStatements(hdid: string): Promise<RequestResult>;
+  getPharmacyInfo(pharmacyId: string): Promise<RequestResult>;
 }
 
 export interface IConfigService {
