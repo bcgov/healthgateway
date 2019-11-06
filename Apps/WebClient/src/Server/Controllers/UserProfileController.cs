6f217f07aa443e4884b9efaae3fd4cb070a5284a
Tiago Graf <tiago.graf@gmail.com>
2019-11-06 15:56:49 -0800

    Add UserProfile controller and service

---
+++ Apps/WebClient/src/Server/Controllers/UserProfileController.cs
@@ -0,0 +1,92 @@
+﻿// -------------------------------------------------------------------------
+//  Copyright © 2019 Province of British Columbia
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------
+namespace HealthGateway.WebClient.Controllers
+{
+    using System.Diagnostics.Contracts;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+    using HealthGateway.Common.Authorization;
+    using HealthGateway.WebClient.Models;
+    using HealthGateway.WebClient.Services;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// Web API to handle user profile interactions.
+    /// </summary>
+    [Authorize]
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/api/[controller]")]
+    [ApiController]
+    public class UserProfileController
+    {
+        private readonly IUserProfileService userProfileService;
+
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        private readonly IAuthorizationService authorizationService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserProfileController"/> class.
+        /// </summary>
+        /// <param name="userProfileService">The injected user profile service.</param>
+        /// <param name="authorizationService">The injected authorization service.</param>
+        /// <param name="httpContextAccessor">The injected http context accessor provider.</param>
+        public UserProfileController(
+            IUserProfileService userProfileService,
+            IHttpContextAccessor httpContextAccessor,
+            IAuthorizationService authorizationService)
+        {
+            this.userProfileService = userProfileService;
+            this.httpContextAccessor = httpContextAccessor;
+            this.authorizationService = authorizationService;
+        }
+
+        /// <summary>
+        /// Posts a user profile json to be inserted into the database.
+        /// </summary>
+        /// <returns>The http status.</returns>
+        /// <param name="userProfile">The user profile model.</param>
+        /// <response code="200">Returns the medication statement bundle.</response>
+        /// <response code="401">the client must authenticate itself to get the requested response.</response>
+        /// <response code="403">The client does not have access rights to the content; that is, it is unauthorized, so the server is refusing to give the requested resource. Unlike 401, the client's identity is known to the server.</response>
+        [HttpPost]
+        [Authorize(Policy = "PatientOnly")]
+        public async Task<IActionResult> InsertUserProfile([FromBody] UserProfile userProfile)
+        {
+            Contract.Requires(userProfile != null);
+            ClaimsPrincipal user = this.httpContextAccessor.HttpContext.User;
+            var isAuthorized = await this.authorizationService
+                .AuthorizeAsync(user, userProfile.Hdid, PolicyNameConstants.UserIsPatient)
+                .ConfigureAwait(true);
+            if (!isAuthorized.Succeeded)
+            {
+                return new ForbidResult();
+            }
+
+            UserProfile existingUserProfile = this.userProfileService.GetUserProfile(userProfile.Hdid);
+            if (existingUserProfile != null)
+            {
+                // User profile record was already created.
+                return new BadRequestResult();
+            }
+
+            this.userProfileService.SaveUserProfile(userProfile);
+            return new OkResult();
+        }
+    }
+}
