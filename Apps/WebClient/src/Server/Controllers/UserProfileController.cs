54885f3233375c94ef37d48f786ebd5b043991ce
Tiago Graf <tiago.graf@gmail.com>
2019-11-06 16:03:29 -0800

    Add get user profile endpoint
    Setup services in startup

--- Apps/WebClient/src/Server/Controllers/UserProfileController.cs
+++ Apps/WebClient/src/Server/Controllers/UserProfileController.cs
@@ -88,5 +88,32 @@ namespace HealthGateway.WebClient.Controllers
             this.userProfileService.SaveUserProfile(userProfile);
             return new OkResult();
         }
+
+        /// <summary>
+        /// Gets a user profile json.
+        /// </summary>
+        /// <returns>The user profile model.</returns>
+        /// <param name="hdid">The user hdid.</param>
+        /// <response code="200">Returns the medication statement bundle.</response>
+        /// <response code="401">the client must authenticate itself to get the requested response.</response>
+        /// <response code="403">The client does not have access rights to the content; that is, it is unauthorized, so the server is refusing to give the requested resource. Unlike 401, the client's identity is known to the server.</response>
+        [HttpGet]
+        [Route("{hdid}")]
+        [Authorize(Policy = "PatientOnly")]
+        public async Task<IActionResult> GetUserProfile(string hdid)
+        {
+            Contract.Requires(hdid != null);
+            ClaimsPrincipal user = this.httpContextAccessor.HttpContext.User;
+            var isAuthorized = await this.authorizationService
+                .AuthorizeAsync(user, hdid, PolicyNameConstants.UserIsPatient)
+                .ConfigureAwait(true);
+            if (!isAuthorized.Succeeded)
+            {
+                return new ForbidResult();
+            }
+
+            UserProfile existingUserProfile = this.userProfileService.GetUserProfile(hdid);
+            return new JsonResult(existingUserProfile);
+        }
     }
 }
