faa268f235e448a99045b48b422f93b7ee2390d0
Tiago Graf <tiago.graf@gmail.com>
2019-11-06 16:07:36 -0800

    Fixed endpoint docs

--- Apps/WebClient/src/Server/Controllers/UserProfileController.cs
+++ Apps/WebClient/src/Server/Controllers/UserProfileController.cs
@@ -61,8 +61,9 @@ namespace HealthGateway.WebClient.Controllers
         /// </summary>
         /// <returns>The http status.</returns>
         /// <param name="userProfile">The user profile model.</param>
-        /// <response code="200">Returns the medication statement bundle.</response>
-        /// <response code="401">the client must authenticate itself to get the requested response.</response>
+        /// <response code="200">The user profile record was saved.</response>
+        /// <response code="400">The user profile was already inserted.</response>
+        /// <response code="401">The client must authenticate itself to get the requested response.</response>
         /// <response code="403">The client does not have access rights to the content; that is, it is unauthorized, so the server is refusing to give the requested resource. Unlike 401, the client's identity is known to the server.</response>
         [HttpPost]
         [Authorize(Policy = "PatientOnly")]
@@ -94,7 +95,7 @@ namespace HealthGateway.WebClient.Controllers
         /// </summary>
         /// <returns>The user profile model.</returns>
         /// <param name="hdid">The user hdid.</param>
-        /// <response code="200">Returns the medication statement bundle.</response>
+        /// <response code="200">Returns the user profile json.</response>
         /// <response code="401">the client must authenticate itself to get the requested response.</response>
         /// <response code="403">The client does not have access rights to the content; that is, it is unauthorized, so the server is refusing to give the requested resource. Unlike 401, the client's identity is known to the server.</response>
         [HttpGet]
