32a911d35fb01a34ef1833061f3afd73d94c81cf
Tiago Graf <tiago.graf@gmail.com>
2019-11-28 13:13:08 -0800

    Remove nav header language unit tests

--- Apps/WebClient/src/ClientApp/test/navbar.header.test.ts
+++ Apps/WebClient/src/ClientApp/test/navbar.header.test.ts
@@ -17,19 +17,4 @@ describe("NavBar Header Component", () => {
   test("is a Vue instance", () => {
     expect(wrapper.isVueInstance()).toBeTruthy();
   });
-
-  const languageWrapper = wrapper.find("#languageSelector");
-
-  test("has default language set", () => {
-    expect(languageWrapper.attributes("text")).toBe("English"); // Checks the dropdown element text
-    expect(wrapper.vm.$data.currentLanguage.code).toBe("en"); // Checks scope variable
-  });
-
-  test("updates the language localization", () => {
-    const frenchLink = languageWrapper.find("#fr");
-    frenchLink.trigger("click");
-
-    expect(languageWrapper.attributes("text")).toBe("French"); // Checks the dropdown element text
-    expect(wrapper.vm.$data.currentLanguage.code).toBe("fr"); // Checks scope variable
-  });
 });
