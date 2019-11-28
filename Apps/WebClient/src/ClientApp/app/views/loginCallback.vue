d92e5fc391c1acad15b5fda5224f2d912b069cb0
Tiago Graf <tiago.graf@gmail.com>
2019-11-27 16:47:05 -0800

    Update loginCallback.vue

--- Apps/WebClient/src/ClientApp/app/views/loginCallback.vue
+++ Apps/WebClient/src/ClientApp/app/views/loginCallback.vue
@@ -24,7 +24,7 @@ export default class LoginCallbackComponent extends Vue {
           if (this.userIsRegistered) {
             this.$router.push({ path: redirectPath });
           } else {
-            if (redirectPath.startsWith("/registration/")) {
+            if (redirectPath.startsWith("/registration")) {
               this.$router.push({ path: redirectPath });
             } else {
               this.$router.push({ path: "/registration" });
