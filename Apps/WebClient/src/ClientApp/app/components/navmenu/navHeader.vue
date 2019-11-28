ea1e1bc1b06aae5165d3f69c926e428f0d0b08e0
mandad <mandad.tabrizi@gmail.com>
2019-11-28 11:42:54 -0800

    clean up indentation

--- Apps/WebClient/src/ClientApp/app/components/navmenu/navHeader.vue
+++ Apps/WebClient/src/ClientApp/app/components/navmenu/navHeader.vue
@@ -1,83 +1,74 @@
 <template>
-  <b-navbar toggleable="lg" type="dark">
+<b-navbar toggleable="lg" type="dark">
     <!-- Brand -->
     <b-navbar-brand>
-      <router-link  to="/timeLine">
-        <img
-          class="img-fluid d-none d-md-block"
-          src="@/assets/images/gov/bcid-logo-rev-en.svg"
-          width="181"
-          height="44"
-          alt="B.C. Government Logo"
-        />
-        <img
-          class="img-fluid d-md-none"
-          src="@/assets/images/gov/bcid-symbol-rev.svg"
-          width="64"
-          height="44"
-          alt="B.C. Government Logo"
-        />
-          <b-navbar-brand>
-      <h4 class="nav-link" to="/timeLine">HealthGateway</h4>
+        <router-link to="/timeLine">
+            <img class="img-fluid d-none d-md-block" src="@/assets/images/gov/bcid-logo-rev-en.svg" width="181" height="44" alt="B.C. Government Logo" />
+            <img class="img-fluid d-md-none" src="@/assets/images/gov/bcid-symbol-rev.svg" width="64" height="44" alt="B.C. Government Logo" />
+            <b-navbar-brand>
+                <h4 class="nav-link " to="/timeLine">HealthGateway</h4>
+            </b-navbar-brand>
+        </router-link>
     </b-navbar-brand>
-      </router-link>
-    </b-navbar-brand>
-  
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <!-- Navbar links -->
     <b-collapse id="nav-collapse" is-nav>
-      <!-- Menu -->
- 
-      <b-navbar-nav v-if="displayRegistration">
-        <router-link class="nav-link" to="/registration">
-          <span class="fa fa-key"></span> Register
-        </router-link>
-      </b-navbar-nav>
+        <!-- Menu -->
 
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item-dropdown
-          v-if="oidcIsAuthenticated"
-          id="menuBtndUser"
-          :text="greeting"
-          right
-          variant="dark"
-        >
-        
-          <b-dropdown-item>
-             <router-link  variant="primary"  to="/timeLine">
-                <span  class="fa fa-stream" ></span> Timeline
-             </router-link>
-          <b-dropdown-divider/>
-            <router-link variant="primary" id="menuBtnLogout" to="/logout">
-              <span class="fa fa-user"></span> Logout
+        <b-navbar-nav v-if="displayRegistration">
+            <router-link class="nav-link" to="/registration">
+                <span class="fa fa-key"></span> Register
             </router-link>
-          </b-dropdown-item>
-        </b-nav-item-dropdown>
-        <router-link v-else id="menuBtnLogin" class="nav-link" to="/login">
-          <span class="fa fa-user"></span> Login
-        </router-link>
-       
-      </b-navbar-nav>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown v-if="oidcIsAuthenticated" id="menuBtndUser" :text="greeting" right variant="dark">
+
+                <b-dropdown-item>
+                    <router-link variant="primary" to="/timeLine">
+                        <span class="fa fa-stream"></span> Timeline
+                    </router-link>
+                    <b-dropdown-divider />
+                    <router-link variant="primary" id="menuBtnLogout" to="/logout">
+                        <span class="fa fa-user"></span> Logout
+                    </router-link>
+                </b-dropdown-item>
+            </b-nav-item-dropdown>
+            <router-link v-else id="menuBtnLogin" class="nav-link" to="/login">
+                <span class="fa fa-user"></span> Login
+            </router-link>
+
+        </b-navbar-nav>
     </b-collapse>
-  </b-navbar>
+</b-navbar>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import { Prop, Component, Watch } from "vue-property-decorator";
-import { Getter } from "vuex-class";
-import { User as OidcUser } from "oidc-client";
-import { IAuthenticationService } from "@/services/interfaces";
+import {
+    Prop,
+    Component,
+    Watch
+} from "vue-property-decorator";
+import {
+    Getter
+} from "vuex-class";
+import {
+    User as OidcUser
+} from "oidc-client";
+import {
+    IAuthenticationService
+} from "@/services/interfaces";
 import SERVICE_IDENTIFIER from "@/constants/serviceIdentifiers";
 import container from "@/inversify.config";
 import User from "@/models/user";
 
 interface ILanguage {
-  code: string;
-  description: string;
+    code: string;
+    description: string;
 }
 
 const auth: string = "auth";
@@ -85,78 +76,80 @@ const user: string = "user";
 
 @Component
 export default class HeaderComponent extends Vue {
-  @Getter("oidcIsAuthenticated", { namespace: auth })
-  oidcIsAuthenticated: boolean;
-  @Getter("user", { namespace: user }) user: User;
-  @Getter("userIsRegistered", { namespace: user })
-  userIsRegistered: boolean;
+    @Getter("oidcIsAuthenticated", {
+        namespace: auth
+    })
+    oidcIsAuthenticated: boolean;
+    @Getter("user", {
+        namespace: user
+    }) user: User;
+    @Getter("userIsRegistered", {
+        namespace: user
+    })
+    userIsRegistered: boolean;
 
-  private authenticationService: IAuthenticationService;
+    private authenticationService: IAuthenticationService;
 
-  
-  private name: string = "";
+    private name: string = "";
 
-  @Watch("oidcIsAuthenticated")
-  onPropertyChanged() {
-    // If there is no name in the scope, retrieve it from the service.
-    if (this.oidcIsAuthenticated && !this.name) {
-      this.loadName();
-    }
-  }
-
-  mounted() {
-    this.authenticationService = container.get(
-      SERVICE_IDENTIFIER.AuthenticationService
-    );
-    if (this.oidcIsAuthenticated) {
-      this.loadName();
-    }
-  }
-
-  
-
-  get displayMenu(): boolean {
-    let isLandingPage = this.$route.path === "/";
-    if (
-      this.oidcIsAuthenticated &&
-      this.name &&
-      this.userIsRegistered &&
-      !isLandingPage
-    ) {
-      return true;
+    @Watch("oidcIsAuthenticated")
+    onPropertyChanged() {
+        // If there is no name in the scope, retrieve it from the service.
+        if (this.oidcIsAuthenticated && !this.name) {
+            this.loadName();
+        }
     }
 
-    return false;
-  }
-
-  get displayRegistration(): boolean {
-    console.log(this.userIsRegistered);
-    if (this.oidcIsAuthenticated && !this.userIsRegistered) {
-      return true;
+    mounted() {
+        this.authenticationService = container.get(
+            SERVICE_IDENTIFIER.AuthenticationService
+        );
+        if (this.oidcIsAuthenticated) {
+            this.loadName();
+        }
     }
-    return false;
-  }
 
-  get greeting(): string {
-    if (this.oidcIsAuthenticated && this.name) {
-      return "Hi " + this.name;
-    } else {
-      return "";
+    get displayMenu(): boolean {
+        let isLandingPage = this.$route.path === "/";
+        if (
+            this.oidcIsAuthenticated &&
+            this.name &&
+            this.userIsRegistered &&
+            !isLandingPage
+        ) {
+            return true;
+        }
+
+        return false;
     }
-  }
 
-  private loadName(): void {
-    this.authenticationService.getOidcUserProfile().then(oidcUser => {
-      if (oidcUser) {
-        this.name = this.getFullname(oidcUser.given_name, oidcUser.family_name);
-      }
-    });
-  }
+    get displayRegistration(): boolean {
+        console.log(this.userIsRegistered);
+        if (this.oidcIsAuthenticated && !this.userIsRegistered) {
+            return true;
+        }
+        return false;
+    }
 
-  private getFullname(firstName: string, lastName: string): string {
-    return firstName + " " + lastName;
-  }
+    get greeting(): string {
+        if (this.oidcIsAuthenticated && this.name) {
+            return "Hi " + this.name;
+        } else {
+            return "";
+        }
+    }
 
+    private loadName(): void {
+        this.authenticationService.getOidcUserProfile().then(oidcUser => {
+            if (oidcUser) {
+                this.name = this.getFullname(oidcUser.given_name, oidcUser.family_name);
+            }
+        });
+    }
+
+    private getFullname(firstName: string, lastName: string): string {
+        return firstName + " " + lastName;
+    }
 
 }
 </script>
