45fce93b5ede0aefe41fc1a4dd1a166c59d01ee3
Tiago Graf <tiago.graf@gmail.com>
2019-12-03 13:54:39 -0800

    Fix merge

--- Apps/Database/src/Delegates/IEmailDelegate.cs
+++ Apps/Database/src/Delegates/IEmailDelegate.cs
@@ -78,11 +78,18 @@ namespace HealthGateway.Database.Delegates
         EmailTemplate GetEmailTemplate(string templateName);
 
         /// <summary>
-        /// Gets a particulare EmailInvite based on the users HDID and inviteKey.
+        /// Gets a particular EmailInvite based on the users HDID and inviteKey.
         /// </summary>
         /// <param name="inviteKey">The users inviteKey as emailed.</param>
         /// <returns>The EmailInvite that was fetched.</returns>
         EmailInvite GetEmailInvite(Guid inviteKey);
+
+        /// <summary>
+        /// Gets the last EmailInvite based on the users HDID.
+        /// </summary>
+        /// <param name="hdid">The users hdid.</param>
+        /// <returns>The EmailInvite that was fetched.</returns>
+        EmailInvite GetLastEmailInviteForUser(string hdid);
 
         /// <summary>
         /// Updates an Email Invite using a populated EmailInvite object.
