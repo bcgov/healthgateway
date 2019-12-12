87526feff50405eb0184a50808d6a184e7c98381
Manuel Rodriguez <marobej@gmail.com>
2019-12-12 13:09:07 -0800

    Fixed logging and initial work on medication

--- Apps/Medication/src/Startup.cs
+++ Apps/Medication/src/Startup.cs
@@ -39,18 +39,15 @@ namespace HealthGateway.Medication
     public class Startup
     {
         private readonly StartupConfiguration startupConfig;
-        private readonly IConfiguration configuration;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Startup"/> class.
         /// </summary>
         /// <param name="env">The injected Environment provider.</param>
         /// <param name="configuration">The injected configuration provider.</param>
-        /// <param name="logger">The injected logger provider.</param>
-        public Startup(IHostingEnvironment env, IConfiguration configuration, ILogger<Startup> logger)
+        public Startup(IWebHostEnvironment env, IConfiguration configuration)
         {
-            this.startupConfig = new StartupConfiguration(configuration, env, logger);
-            this.configuration = configuration;
+            this.startupConfig = new StartupConfiguration(configuration, env);
         }
 
         /// <summary>
