53fb5b83e3bf84ad7ba4ef5c7c77826d6ca21ba6
mandad <mandad.tabrizi@gmail.com>
2020-01-30 12:47:48 -0800

    updated healthlink card
    fixed issues raised in github PR.

--- Apps/WebClient/src/ClientApp/app/components/timeline/healthlink.vue
+++ Apps/WebClient/src/ClientApp/app/components/timeline/healthlink.vue
@@ -1,18 +1,12 @@
 <style lang="scss" scoped>
 .box {
-  box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.015),
-    0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
-    0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.05),
-    0 100px 80px rgba(0, 0, 0, 0.07);
-
   margin: 100px auto;
   width: 160px;
   background: white;
-  border-radius: 5px;
+  border-radius: 15px;
+  border: none;
 }
-.box:hover {
-  background: rgba(0, 0, 0, 0.022);
-}
+
 .stretched-link::after {
   position: absolute;
   top: 0;
@@ -40,10 +34,7 @@
 <template>
   <b-card
     :title="currentCard.title"
-    img-top
-    tag="article"
-    style="max-width: 50rem;border: none;"
-    class="mb-2 box d-none d-lg-block"
+    class="box d-none d-lg-block shadow mb-5 bg-white rounded"
   >
     <a
       href="https://www.healthlinkbc.ca/services-and-resources/about-8-1-1 "
@@ -56,33 +47,29 @@
 
 <script lang="ts">
 import Vue from "vue";
-import { Prop, Component } from "vue-property-decorator";
+import { Component } from "vue-property-decorator";
 interface Healthcard {
   title: string;
   description: string;
-  url: string;
 }
 @Component
 export default class HealthlinkSidebarComponent extends Vue {
-  private currentCard: Healthcard = { title: "", description: "", url: "" };
+  private currentCard: Healthcard = { title: "", description: "" };
   private cardPool: Healthcard[] = [
     {
       title: "8-1-1",
       description:
-        "Speak to a pharmacist from 5pm to 9am Pacific Time everyday of the year.",
-      url: "sdsa.com"
+        "Speak to a pharmacist from 5pm to 9am Pacific Time everyday of the year."
     },
     {
       title: "Healthlink",
       description:
-        "HealthLink BC provides reliable non-emergency health information and advice in British Columbia.",
-      url: "lab.com"
+        "HealthLink BC provides reliable non-emergency health information and advice in British Columbia."
     }
   ];
   mounted() {
     let cardIndex: number = Math.floor(Math.random() * this.cardPool.length);
     this.currentCard = this.cardPool[cardIndex];
-    console.log(this.currentCard);
   }
 }
 </script>
