a7b939cbe2205564fd1b693c8ad46bae0f9567e4
Brad Head <brad.head@gmail.com>
2020-01-29 16:06:39 -0800

    fixed spa

---
+++ Apps/AdminWebClient/src/ClientApp/src/store/user/actions.ts
@@ -0,0 +1,7 @@
+import { ActionTree } from 'vuex';
+import axios from 'axios';
+import { RootState } from '../types';
+import UserProfile from './node_modules/@/models/UserProfile';
+
+export const actions: ActionTree<UserProfile, RootState> = {
+};
