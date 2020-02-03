ff5e4bfa455bf36ac736e6e27a682e8a289beaf8
Brad Head <brad@zedwerks.com>
2020-02-03 13:42:20 -0800

    added auth controller

---
+++ Apps/AdminWebClient/src/ClientApp/src/views/Login.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="container my-5" align="center">
+    <LoadingComponent :is-loading="isLoading"></LoadingComponent>
+    <b-row>
+      <b-col>
+        <b-alert
+          style="max-width: 25rem;"
+          :show="isRetry"
+          dismissible
+          variant="danger"
+        >
+          <h4>Error</h4>
+          <span
+            >An unexpected error occured while processing the request, please
+            try again.</span
+          >
+        </b-alert>
+        <b-card
+          v-if="identityProviders && identityProviders.length > 0"
+          id="loginPicker"
+          class="shadow-lg bg-white"
+          style="max-width: 25rem;"
+          align="center"
+        >
+          <h3 slot="header">Log In</h3>
+          <b-card-body v-if="hasMultipleProviders || isRetry">
+            <div v-for="provider in identityProviders" :key="provider.id">
+              <b-row>
+                <b-col>
+                  <b-button
+                    :id="`${provider.id}Btn`"
+                    block
+                    :disabled="provider.disabled"
+                    variant="primary"
+                    @click="oidcLogin(provider.hint)"
+                  >
+                    <b-row>
+                      <b-col class="col-2">
+                        <font-awesome-icon
+                          :icon="`${provider.icon}`"
+                        ></font-awesome-icon>
+                      </b-col>
+                      <b-col class="text-justify">
+                        <span>{{ provider.name }}</span>
+                      </b-col>
+                    </b-row>
+                  </b-button>
+                </b-col>
+              </b-row>
+              <b-row
+                v-if="
+                  identityProviders.indexOf(provider) <
+                    identityProviders.length - 1
+                "
+                ><b-col>or</b-col>
+              </b-row>
+            </div>
+          </b-card-body>
+          <b-card-body v-else>
+            <span
+              >Redirecting to <strong>{{ identityProviders[0].name }}</strong
+              >...</span
+            >
+          </b-card-body>
+        </b-card>
+        <div v-else>No login providers configured</div>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+import { State, Action, Getter } from "vuex-class";
+import LoadingComponent from "@/components/loading.vue";
+import {
+  IdentityProviderConfiguration,
+  ExternalConfiguration
+} from "@/models/configData";
+
+const namespace: string = "auth";
+
+@Component({
+  components: {
+    LoadingComponent
+  }
+})
+export default class LoginComponent extends Vue {
+  @Action("authenticateOidc", { namespace }) authenticateOidc: any;
+  @Getter("oidcIsAuthenticated", { namespace }) oidcIsAuthenticated: boolean;
+  @Getter("userIsRegistered", { namespace: "user" }) userIsRegistered: boolean;
+  @Getter("identityProviders", { namespace: "config" })
+  identityProviders: IdentityProviderConfiguration[];
+  @Prop() isRetry?: boolean;
+
+  private isLoading: boolean = true;
+  private redirectPath: string = "";
+  private routeHandler = undefined;
+
+  mounted() {
+    if (this.$route.query.redirect && this.$route.query.redirect !== "") {
+      this.redirectPath = this.$route.query.redirect;
+    } else {
+      this.redirectPath = "/dashboard";
+    }
+
+    this.routeHandler = this.$router;
+    if (this.oidcIsAuthenticated && this.userIsRegistered) {
+      this.routeHandler.push({ path: this.redirectPath });
+    } else if (this.oidcIsAuthenticated) {
+      this.redirectPath = "/dashboard";
+      this.routeHandler.push({ path: this.redirectPath });
+    } else if (
+      !this.oidcIsAuthenticated &&
+      this.identityProviders.length == 1 &&
+      !this.isRetry
+    ) {
+      this.oidcLogin(this.identityProviders[0].hint);
+    } else {
+      this.isLoading = false;
+    }
+  }
+
+  get hasMultipleProviders(): boolean {
+    return this.identityProviders.length > 1;
+  }
+
+  oidcLogin(hint: string) {
+    // if the login action returns it means that the user already had credentials.
+    this.authenticateOidc({
+      idpHint: hint,
+      redirectPath: this.redirectPath
+    }).then(result => {
+      if (this.oidcIsAuthenticated) {
+        this.routeHandler.push({ path: this.redirectPath });
+      }
+    });
+  }
+}
+</script>
---
+++ Apps/AdminWebClient/src/Server/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+// -------------------------------------------------------------------------
+//  Copyright © 2020 Province of British Columbia
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------
+namespace HealthGateway.Admin.Controllers
+{
+    using Microsoft.AspNetCore.Authentication.Cookies;
+    using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
+    public class UserController : Controller
+    {
+        private ILogger<UserController> logger;
+        private IConfiguration configuration;
+
+        [Anonymous]
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            if (!HttpContext.User.Identity.IsAuthenticated)
+            {
+                return new ChallengeResult(OpenIdConnectDefaults.AuthenticationScheme, new AuthenticationProperties { RedirectUri = "/dashboard" });
+            }      
+            return RedirectUri("/dashboard");
+        }
+        public async Task<IActionResult> Logout()
+        {
+            return new SignOutResult(new[]
+            { OpenIdConnectDefaults.AuthenticationScheme,
+                CookieAuthenticationDefaults.AuthenticationScheme
+            });
+        }
+    }
+}
