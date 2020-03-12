4cdeb42cc85c1355d319d47ecc72e364dffbf987
Stephen Laws <stephen.laws@nomostech.com>
2020-03-12 12:51:03 -0700

    Clean up smells

--- Apps/WebClient/src/Server/Controllers/NoteController.cs
+++ Apps/WebClient/src/Server/Controllers/NoteController.cs
@@ -65,7 +65,7 @@ namespace HealthGateway.WebClient.Controllers
         /// Posts a patient note json to be inserted into the database.
         /// </summary>
         /// <returns>The http status.</returns>
-        /// <param name="model">The patient note request model.</param>
+        /// <param name="note">The patient note request model.</param>
         /// <response code="200">The note record was saved.</response>
         /// <response code="401">The client must authenticate itself to get the requested response.</response>
         /// <response code="403">The client does not have access rights to the content; that is, it is unauthorized, so the server is refusing to give the requested resource. Unlike 401, the client's identity is known to the server.</response>
@@ -140,35 +140,6 @@ namespace HealthGateway.WebClient.Controllers
 
             RequestResult<IEnumerable<Database.Models.Note>> result = this.noteService.GetNotes(userHdid);
             return new JsonResult(result);
-        }
-
-        /// <summary>
-        /// Gets a single notes by its id.
-        /// </summary>
-        /// <returns>The note model wrapped in a request result.</returns>
-        /// <param name="noteId">The note id.</param>
-        /// <response code="200">Returns the note model.</response>
-        /// <response code="401">the client must authenticate itself to get the requested response.</response>
-        /// <response code="403">The client does not have access rights to the content; that is, it is unauthorized, so the server is refusing to give the requested resource. Unlike 401, the client's identity is known to the server.</response>
-        [HttpGet]
-        [Route("{noteId}")]
-        public async Task<IActionResult> Get(Guid noteId)
-        {
-            ClaimsPrincipal user = this.httpContextAccessor.HttpContext.User;
-            string userHdid = user.FindFirst("hdid").Value;
-            var isAuthorized = await this.authorizationService
-                .AuthorizeAsync(user, userHdid, PolicyNameConstants.UserIsPatient)
-                .ConfigureAwait(true);
-            if (!isAuthorized.Succeeded)
-            {
-                return new ForbidResult();
-            }
-
-            // RequestResult<Database.Models.Note> result = this.noteService.Get(noteId);
-
-            // TODO: Compare note hdid with current jwt hdid.
-            // TODO: Do we need to implement this?
-            return new JsonResult(new object());
         }
     }
 }
