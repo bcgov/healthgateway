837394e014ee9f89c7ee5a61e20e6da956dcde47
Tiago Graf <tiago.graf@gmail.com>
2020-04-23 10:37:07 -0700

    Add communication migration

---
+++ Apps/Database/src/Migrations/20200423173644_Communication.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HealthGateway.Database.Migrations
+{
+    public partial class Communication : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Communication",
+                schema: "gateway",
+                columns: table => new
+                {
+                    CommunicationId = table.Column<Guid>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 60, nullable: false),
+                    CreatedDateTime = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<string>(maxLength: 60, nullable: false),
+                    UpdatedDateTime = table.Column<DateTime>(nullable: false),
+                    xmin = table.Column<uint>(type: "xid", nullable: false),
+                    Text = table.Column<string>(maxLength: 1000, nullable: true),
+                    Subject = table.Column<string>(maxLength: 1000, nullable: true),
+                    EffectiveDateTime = table.Column<DateTime>(nullable: false),
+                    ExpiryDateTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Communication", x => x.CommunicationId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Communication",
+                schema: "gateway");
+        }
+    }
+}
