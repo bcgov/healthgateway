6368e894c127b8ab7fd9b5349deb3be6844e0984
sslaws <51342761+sslaws@users.noreply.github.com>
2020-05-15 10:04:39 -0700

    Implement NotificationSettings delegate for AB#8351 (#496)

--- Apps/WebClient/src/Server/Startup.cs
+++ Apps/WebClient/src/Server/Startup.cs
@@ -25,6 +25,8 @@ namespace HealthGateway.WebClient
     using HealthGateway.Common.Delegates;
     using HealthGateway.Common.Services;
     using HealthGateway.Database.Delegates;
+    using HealthGateway.Laboratory.Delegates;
+    using HealthGateway.WebClient.Delegates;
     using HealthGateway.WebClient.Services;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
@@ -95,6 +97,7 @@ namespace HealthGateway.WebClient
             services.AddTransient<ICommentDelegate, DBCommentDelegate>();
             services.AddTransient<ICryptoDelegate, AESCryptoDelegate>();
             services.AddTransient<ICommunicationDelegate, DBCommunicationDelegate>();
+            services.AddSingleton<INotificationSettingsDelegate, RestNotificationSettingsDelegate>();
 
             services.Configure<ApiBehaviorOptions>(options =>
             {
