ef07d3d68b14bfe44b317c8939a5d1632d5e7f89
bradhead <brad@zedwerks.com>
2020-05-17 11:54:31 -0700

    updated shell scripts with instructions

--- Apps/Medication/test/performance/k6_login.sh
+++ Apps/Medication/test/performance/k6_login.sh
@@ -1,2 +1,5 @@
 ##
-docker run -a STDOUT -a STDERR -i loadimpact/k6 run -e USER_PASSWORD=$HG_USER_PASSWORD --iterations 500 --vus 500   -  <keycloak_login_k6.js
+## How to run this on command-line:
+## export HG_USER_PASSWORD=password ; ./k6_login.sh
+##
+docker run -a STDOUT -a STDERR -i loadimpact/k6 run -e USER_PASSWORD=$HG_USER_PASSWORD --iterations 500 --vus 500 --http-debug  -  <keycloak_login_k6.js
--- Apps/Medication/test/performance/k6_meds.sh
+++ Apps/Medication/test/performance/k6_meds.sh
@@ -1,2 +1,5 @@
 ##
+## How to run this on command-line:
+## export HG_USER_PASSWORD=password ; ./k6_meds.sh
+##
 docker run -a STDOUT -a STDERR -i loadimpact/k6 run -e USER_PASSWORD=$HG_USER_PASSWORD -d 120s --vus 10 --http-debug -  <medication_load_k6.js
