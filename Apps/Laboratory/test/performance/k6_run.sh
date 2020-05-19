c1013ec998608b200d124b7a09605aee31972fd1
bradhead <brad@zedwerks.com>
2020-05-19 11:01:23 -0700

    updated

---
+++ Apps/Laboratory/test/performance/k6_run.sh
@@ -0,0 +1,5 @@
+##
+## How to run this on command-line:
+## export HG_USER_PASSWORD=password ; ./k6_run.sh
+##
+docker run -a STDOUT -a STDERR -i loadimpact/k6 run -e USER_PASSWORD=$HG_USER_PASSWORD -d 120s --vus 10 --http-debug -  <laboratory_load_k6.js
