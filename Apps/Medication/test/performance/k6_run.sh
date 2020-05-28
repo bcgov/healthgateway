6bc80cf22b0e839bce68edba23d7d93a369e7f2f
bradhead <brad@zedwerks.com>
2020-05-28 12:21:47 -0700

    updated for test env configured

--- Apps/Medication/test/performance/k6_run.sh
+++ Apps/Medication/test/performance/k6_run.sh
@@ -2,4 +2,4 @@
 ## How to run this on command-line:
 ## export HG_USER_PASSWORD=password ; ./k6_run.sh
 ##
-docker run -a STDOUT -a STDERR -i loadimpact/k6 run -e HG_USER_PASSWORD=$HG_USER_PASSWORD --iterations 11 --vus 11  -  <medication_load_k6.js
+docker run -a STDOUT -a STDERR -i loadimpact/k6 run -e HG_PASSWORD=$HG_PASSWORD -e HG_ENV=$HG_ENV --iterations 20 --vus 20  -  <medication_load_k6.js
--- Apps/Medication/test/performance/medication_load_k6.js
+++ Apps/Medication/test/performance/medication_load_k6.js
@@ -5,10 +5,12 @@ import { Rate } from 'k6/metrics';
 
 export let errorRate = new Rate('errors');
 
-let TokenEndpointUrl = "https://sso-dev.pathfinder.gov.bc.ca/auth/realms/ff09qn3f/protocol/openid-connect/token";
-let ServiceEndPointUrl = "https://dev.healthgateway.gov.bc.ca/api/medicationservice/v1/api/MedicationStatement";
+let environment  = (__ENV.HG_ENV != undefined) ? __ENV.HG_ENV : 'dev';
 
-let passwd = __ENV.HG_USER_PASSWORD;
+let TokenEndpointUrl = "https://sso-" + environment + ".pathfinder.gov.bc.ca/auth/realms/ff09qn3f/protocol/openid-connect/token";
+let ServiceEndPointUrl = "https://" + environment + ".healthgateway.gov.bc.ca/api/medicationservice/v1/api/MedicationStatement";
+
+let passwd = __ENV.HG_PASSWORD;
 
 let users = [
   { username: "loadtest_01", password: passwd, hdid: null, token: null },
@@ -22,6 +24,12 @@ let users = [
   { username: "loadtest_09", password: passwd, hdid: null, token: null },
   { username: "loadtest_10", password: passwd, hdid: null, token: null },
   { username: "loadtest_11", password: passwd, hdid: null, token: null },
+  { username: "loadtest_12", password: passwd, hdid: null, token: null },
+  { username: "loadtest_13", password: passwd, hdid: null, token: null },
+  { username: "loadtest_14", password: passwd, hdid: null, token: null },
+  { username: "loadtest_15", password: passwd, hdid: null, token: null },
+  { username: "loadtest_20", password: passwd, hdid: null, token: null },
+
 ];
 
 function getRandom(min, max) {
@@ -47,6 +55,7 @@ export default function () {
   let access_token = null;
   let hdid = null;
 
+
   let user = users[__VU % users.length];
 
   let auth_form_data = {
@@ -60,6 +69,7 @@ export default function () {
 
   if (__ITER == 0) {
     if (user.hdid == null) {
+      console.log(TokenEndpointUrl);
       console.log("Authenticating username: " + auth_form_data.username);
       var res = http.post(TokenEndpointUrl, auth_form_data);
       var res_json = JSON.parse(res.body);
@@ -84,6 +94,6 @@ export default function () {
     "Status is 200": r => r.status == 200
   }) || errorRate.add(1);
 
-  sleep(getRandom(2, 10));
+  sleep(getRandom(3, 15));
 }
 
