0d0f68595a6652484b83bcb9f31b4b782079aae7
bradhead <brad@zedwerks.com>
2020-05-29 14:16:57 -0700

    updated script

--- Testing/load/k6_run.sh
+++ Testing/load/k6_run.sh
@@ -1,5 +1,5 @@
 ##
 ## How to run this on command-line:
-## export HG_USER_PASSWORD=password ; ./k6_run.sh
+## HG_PASSWORD=<password> HG_ENV=test iterations=500 vus=35 ./k6_run.sh
 ##
-docker run -a STDOUT -a STDERR -i loadimpact/k6 run -e HG_PASSWORD=$HG_PASSWORD --vus $vus --iterations $iterations - <timeline_load_k6.js
+docker run -a STDOUT -a STDERR -i loadimpact/k6 run -e HG_PASSWORD=$HG_PASSWORD -e HG_ENV=$HG_ENV --iterations $iterations --vus $vus  -  <timeline_load_k6.js
