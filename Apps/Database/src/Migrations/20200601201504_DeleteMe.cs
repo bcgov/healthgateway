a014b035991f78576ad98a80ba12cce349ade193
Stephen Laws <stephen.laws@nomostech.com>
2020-06-01 13:16:26 -0700

    Fix warnings for AB#8302

---
+++ Apps/Database/src/Migrations/20200601201504_DeleteMe.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HealthGateway.Database.Migrations
+{
+    public partial class DeleteMe : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
