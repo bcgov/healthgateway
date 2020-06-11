ffcb4861914ab944c64596c0dbc074cc80a6aea0
FuriousLlama <marobej@gmail.com>
2020-06-11 13:09:51 -0700

    Removed setting that prevented style errors

--- Apps/WebClient/src/ClientApp/vue.config.js
+++ Apps/WebClient/src/ClientApp/vue.config.js
@@ -9,7 +9,5 @@ module.exports = {
   chainWebpack: (config) => {
     config.plugins.delete("split-manifest").delete("inline-manifest");
     config.plugin("CompressionPlugin").use(CompressionPlugin);
-    // TODO: Only needed while typescrit has errors
-    config.plugins.delete("fork-ts-checker");
   },
 };
