08c41337db8098118afa0e5c8a4de7b78cb66d7c
Sam <samwarren4@gmail.com>
2020-06-16 11:26:40 -0700

    Fix lint error on PC

--- Apps/WebClient/src/ClientApp/.eslintrc.js
+++ Apps/WebClient/src/ClientApp/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     rules: {
         "no-console": "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+        "linebreak-style": ["error", "unix"]
     },
     parserOptions: {
         parser: "@typescript-eslint/parser",
