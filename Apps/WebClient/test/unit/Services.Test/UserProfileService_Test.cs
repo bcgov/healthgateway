9f7085df64a64afade0b43b3c2d9567131f8d532
Leo <leo.pham@cgi.com>
2020-07-09 14:13:18 -0700

    fix the failed test on UserProfileService

--- Apps/WebClient/test/unit/Services.Test/UserProfileService_Test.cs
+++ Apps/WebClient/test/unit/Services.Test/UserProfileService_Test.cs
@@ -64,7 +64,19 @@ namespace HealthGateway.WebClient.Test.Services
             Mock<IUserProfileDelegate> profileDelegateMock = new Mock<IUserProfileDelegate>();
             profileDelegateMock.Setup(s => s.GetUserProfile(hdid)).Returns(userProfileDBResult);
             profileDelegateMock.Setup(s => s.Update(userProfile, true)).Returns(userProfileDBResult);
+
+            UserPreference dbUserPreference = new UserPreference
+            {
+                HdId = hdid,
+                DismissedMyNotePopover = true,
+            };
+            DBResult<UserPreference> readResult = new DBResult<UserPreference>
+            {
+                Payload = dbUserPreference,
+                Status = DBStatusCode.Read
+            };
             Mock<IUserPreferenceDelegate> preferenceDelegateMock = new Mock<IUserPreferenceDelegate>();
+            preferenceDelegateMock.Setup(s => s.GetUserPreference(hdid)).Returns(readResult);
 
             Mock<IEmailDelegate> emailDelegateMock = new Mock<IEmailDelegate>();
             Mock<IMessagingVerificationDelegate> emailInviteDelegateMock = new Mock<IMessagingVerificationDelegate>();
@@ -215,7 +227,7 @@ namespace HealthGateway.WebClient.Test.Services
         }
 
         [Fact]
-        public void ShouldInsertPreference()
+        public void ShouldInsertUserPreference()
         {
             UserPreference dbUserPreference = new UserPreference
             {
@@ -263,7 +275,7 @@ namespace HealthGateway.WebClient.Test.Services
         }
 
         [Fact]
-        public void ShouldGetPreference()
+        public void ShouldGetUserPreference()
         {
             UserPreference dbUserPreference = new UserPreference
             {
