6d9640ea78f98f504368aa6a46c3592c4757620a
Tiago Graf <tiago.graf@gmail.com>
2020-07-14 15:10:50 -0700

    Fixes build

--- Apps/WebClient/src/ClientApp/src/components/timelineLoading.vue
+++ Apps/WebClient/src/ClientApp/src/components/timelineLoading.vue
@@ -132,7 +132,7 @@ import {
     faSyringe,
 } from "@fortawesome/free-solid-svg-icons";
 
-@Component()
+@Component
 export default class TimelineLoadingComponent extends Vue {
     private step: number = 0;
     private get ellipsis(): string {
@@ -172,13 +172,13 @@ export default class TimelineLoadingComponent extends Vue {
     }
 
     private resetAnimation(elementId: string): boolean {
-        var el: HTMLElement = document.getElementById(elementId);
+        var el: HTMLElement | null = document.getElementById(elementId);
         if (el == null) {
             return false;
         }
         el.style.animation = "none";
         el.offsetHeight; /* trigger reflow */
-        el.style.animation = null;
+        el.style.animation = "";
         return true;
     }
 }
