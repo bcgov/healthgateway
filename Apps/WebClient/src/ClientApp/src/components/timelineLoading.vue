20102b96527cdbaa6dd4078a99568a236dac22b1
FuriousLlama <marobej@gmail.com>
2020-07-14 15:17:27 -0700

    Increaded transparency

--- Apps/WebClient/src/ClientApp/src/components/timelineLoading.vue
+++ Apps/WebClient/src/ClientApp/src/components/timelineLoading.vue
@@ -7,7 +7,7 @@
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(254, 254, 254, 0.8);
+    background-color: rgba(254, 254, 254, 0.5);
     display: table;
     transition: opacity 0.3s ease;
 }
