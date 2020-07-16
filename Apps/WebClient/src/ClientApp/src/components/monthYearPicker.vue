83ce95fed337be695a4d506c964ee01983359c6a
Tiago Graf <tiago.graf@gmail.com>
2020-07-16 14:25:27 -0700

    Create monthYearPicker.vue

---
+++ Apps/WebClient/src/ClientApp/src/components/monthYearPicker.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div v-for="year in years" :key="year">
+            <button>{{ year }}</button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class MonthYearPickerComponent extends Vue {
+    private years: number[] = [
+        2012,
+        2013,
+        2014,
+        2015,
+        2016,
+        2017,
+        2018,
+        2019,
+        2020,
+    ];
+}
+</script>
