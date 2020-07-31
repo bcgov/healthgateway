87ec3d5211b5766f92677384ecd4ae1380d655bd
Sam <samwarren4@gmail.com>
2020-07-31 13:57:22 -0700

    add error card component

---
+++ Apps/WebClient/src/ClientApp/src/components/timeline/errorCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <b-alert dismissible variant="danger" class="no-print">
+        <h4>{{ code }}: {{ title }}</h4>
+        <span>{{ description }}</span>
+    </b-alert>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+import ErrorCard from "@/models/errorCard";
+
+@Component
+export default class ErrorCardComponent extends Vue {
+    @Prop() error!: ErrorCard;
+    private code: string = "";
+    private title: string = "";
+    private description: string = "";
+
+    private mounted() {
+        this.code = this.error.code;
+        this.title = this.error.title;
+        this.description = this.error.description;
+        console.log("Error: ", this.error);
+    }
+}
+</script>
---
+++ Apps/WebClient/src/ClientApp/src/models/errorCard.ts
@@ -0,0 +1,10 @@
+export default interface ErrorCard {
+    // The title of the error message
+    title: string;
+
+    // The descriptive text below the title
+    description: string;
+
+    // The error code
+    code: string;
+}
