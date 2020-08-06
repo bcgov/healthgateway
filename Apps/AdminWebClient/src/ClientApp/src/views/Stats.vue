2ff5cdb3916dde0c6b31b87b8eed5fbeb74a5a46
Sam <samwarren4@gmail.com>
2020-08-06 15:34:14 -0700

    add stats download UI

---
+++ Apps/AdminWebClient/src/ClientApp/src/views/Stats.vue
@@ -0,0 +1,55 @@
+<style scoped></style>
+<template>
+    <v-container fill-height fluid grid-list-xl>
+        <v-layout justify-center wrap>
+            <v-flex xs6 md4>
+                <v-card dark>
+                    <v-card-title>
+                        <span class="headline">System Stats Download</span>
+                    </v-card-title>
+                    <v-form>
+                        <v-container>
+                            <v-row>
+                                <v-col align="center" justify="center">
+                                    <h3>User Profile</h3>
+                                    <v-btn
+                                        @click="downloadUserInfoCSV()"
+                                        class="info"
+                                        ><v-icon>fa-download</v-icon></v-btn
+                                    >
+                                </v-col>
+                                <v-col align="center" justify="center">
+                                    <h3>User Notes</h3>
+                                    <v-btn
+                                        @click="downloadUserNotesCSV()"
+                                        class="info"
+                                        ><v-icon>fa-download</v-icon></v-btn
+                                    >
+                                </v-col>
+                                <v-col align="center" justify="center">
+                                    <h3>User Comments</h3>
+                                    <v-btn
+                                        @click="downloadUserCommentsCSV()"
+                                        class="info"
+                                        ><v-icon>fa-download</v-icon></v-btn
+                                    >
+                                </v-col>
+                            </v-row>
+                        </v-container>
+                    </v-form>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { SERVICE_IDENTIFIER } from "@/plugins/inversify";
+import container from "@/plugins/inversify.config";
+@Component
+export default class StatsView extends Vue {
+    private downloadUserTable() {}
+    private downloadNotesTable() {}
+    private downloadCommentsTable() {}
+}
+</script>
