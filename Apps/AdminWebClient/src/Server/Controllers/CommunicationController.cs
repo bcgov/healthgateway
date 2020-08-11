013aded2481e1f58eed9e6fa10d3aa5d48b098d7
Stephen Laws <stephen.laws@nomostech.com>
2020-08-11 14:49:48 -0700

    Added missing controller annotation for delete AB#8786

--- Apps/AdminWebClient/src/Server/Controllers/CommunicationController.cs
+++ Apps/AdminWebClient/src/Server/Controllers/CommunicationController.cs
@@ -90,6 +90,7 @@ namespace HealthGateway.Admin.Controllers
         /// <response code="200">Returns the communication json of the deleted object wrapped in a request result.</response>
         /// <response code="401">the client must authenticate itself to get the requested response.</response>
         /// <response code="403">The client does not have access rights to the content; that is, it is unauthorized, so the server is refusing to give the requested.</response>
+        [HttpDelete]
         public IActionResult Delete([FromBody] Communication communication)
         {
             return new JsonResult(this.communicationService.Delete(communication));
