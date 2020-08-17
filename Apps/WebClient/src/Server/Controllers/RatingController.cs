b6af1d53f04725a6069257e8060b692b707cac87
Tiago Graf <tiago.graf@gmail.com>
2020-08-17 15:13:27 -0700

    Add rating controller AB#8889

---
+++ Apps/WebClient/src/Server/Controllers/RatingController.cs
@@ -0,0 +1,68 @@
+﻿// -------------------------------------------------------------------------
+//  Copyright © 2019 Province of British Columbia
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------
+namespace HealthGateway.WebClient.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+    using HealthGateway.Common.AccessManagement.Authorization;
+    using HealthGateway.Common.Filters;
+    using HealthGateway.Common.Models;
+    using HealthGateway.Database.Models;
+    using HealthGateway.WebClient.Models;
+    using HealthGateway.WebClient.Services;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// Web API to handle user ratings.
+    /// </summary>
+    [Authorize]
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/api/[controller]")]
+    [ApiController]
+    public class RatingController
+    {
+        private readonly object ratingService; // TODO: Update with Interface.
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RatingController"/> class.
+        /// </summary>
+        /// <param name="ratingService">The injected rating service.</param>
+        public RatingController(
+            object ratingService) // TODO: Update with Interface.
+        {
+            this.ratingService = ratingService;
+        }
+
+        /// <summary>
+        /// Saves the rating model.
+        /// </summary>
+        /// <param name="Rating">The rating to be saved.</param>
+        /// <returns>The saved rating wrapped in a request result.</returns>
+        /// <response code="200">Returns the saved rating json.</response>
+        /// <response code="401">the client must authenticate itself to get the requested response.</response>
+        /// <response code="403">The client does not have access rights to the content; that is, it is unauthorized, so the server is refusing to give the requested resource. Unlike 401, the client's identity is known to the server.</response>
+        [HttpPost]
+        public IActionResult Save(object Rating) // TODO: update with Model.
+        {
+            //RequestResult<Rating> result = this.ratingService.Save(rating);
+            return new JsonResult(null); // TODO: update with result.
+        }
+    }
+}
