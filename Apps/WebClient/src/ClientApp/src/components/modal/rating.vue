7b14dea370fc05e7badc7e51880ca4f9cc98bdc5
leo <leo.pham@cgi.com>
2020-08-18 11:01:50 -0700

    Fixed rating.vue AB#8893

--- Apps/WebClient/src/ClientApp/src/components/modal/rating.vue
+++ Apps/WebClient/src/ClientApp/src/components/modal/rating.vue
@@ -1,41 +1,17 @@
-<script lang="ts">
-import Vue from "vue";
-import { Component, Prop } from "vue-property-decorator";
-import { ILogger } from "@/services/interfaces";
-import { SERVICE_IDENTIFIER } from "@/plugins/inversify";
-import container from "@/plugins/inversify.config";
-
-@Component
-export default class RatingComponent extends Vue {
-    private logger: ILogger = container.get(SERVICE_IDENTIFIER.Logger);
-    @Prop({
-        default:
-            "Did the Health Gateway improve your access to health information today? Please provide a rating.",
-        required: false,
-    })
-    question!: string;
-    private ratingValue: number = 0;
-    private isVisible: boolean = false;
-    public showModal() {
-        this.isVisible = true;
-    }
-    public hideModal() {
-        this.isVisible = false;
-    }
-    private handleRating(value: number, skip: boolean = false) {
-        this.logger.debug(
-            `Emitting the selected rating value: ${value} to the modal's caller ...`
-        );
-        this.$emit("selected-rating", { ratingValue: value, skip: skip });
-        this.hideModal();
-    }
-    private skip() {
-        this.logger.debug(`clicked on skip button (value: 0)...`);
-        this.handleRating(0, true);
+<style lang="scss" scoped>
+@import "@/assets/scss/_variables.scss";
+.modal-content {
+    max-width: 500px;
+    .modal-body {
+        padding-bottom: 0px !important;
+        .row-answer {
+            margin-top: 10px;
+            padding-left: 50px;
+            padding-right: 50px;
+        }
     }
 }
-</script>
-
+</style>
 <template>
     <div>
         <b-modal
@@ -72,34 +48,41 @@ export default class RatingComponent extends Vue {
             <template v-slot:modal-footer>
                 <b-row>
                     <b-col>
-                        <b-row>
-                            <b-col>
-                                <b-button
-                                    id="skipButton"
-                                    variant="outline-primary"
-                                    @click="skip()"
-                                    >Skip</b-button
-                                >
-                            </b-col>
-                        </b-row>
+                        <b-button
+                            id="skipButton"
+                            variant="outline-primary"
+                            @click="skip(0, true)"
+                            >Skip</b-button
+                        >
                     </b-col>
                 </b-row>
             </template>
         </b-modal>
     </div>
 </template>
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+import { ILogger } from "@/services/interfaces";
+import { SERVICE_IDENTIFIER } from "@/plugins/inversify";
+import container from "@/plugins/inversify.config";
 
-<style lang="scss" scoped>
-@import "@/assets/scss/_variables.scss";
-.modal-content {
-    max-width: 500px;
-    .modal-body {
-        padding-bottom: 0px !important;
-        .row-answer {
-            margin-top: 10px;
-            padding-left: 50px;
-            padding-right: 50px;
-        }
+@Component
+export default class RatingComponent extends Vue {
+    private logger: ILogger = container.get(SERVICE_IDENTIFIER.Logger);
+    question: string =
+        "Did the Health Gateway improve your access to health information today? Please provide a rating.";
+    private ratingValue: number = 0;
+    private isVisible: boolean = false;
+    public showModal() {
+        this.isVisible = true;
+    }
+    public hideModal() {
+        this.isVisible = false;
+    }
+    private handleRating(value: number, skip: boolean = false) {
+        // Todo: call rating vue service
+        this.hideModal();
     }
 }
-</style>
+</script>
