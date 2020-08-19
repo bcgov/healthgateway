1453aa76a466d241a4e31436f5e9d4b5c6dd1232
Stephen Laws <51342761+sslaws@users.noreply.github.com>
2020-08-18 20:34:47 -0700

    Fixes AB#8943 and cleans up config (#742)

--- Apps/AdminWebClient/src/Server/Startup.cs
+++ Apps/AdminWebClient/src/Server/Startup.cs
@@ -122,19 +122,6 @@ namespace HealthGateway.AdminWebClient
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
-            if (env.IsDevelopment())
-            {
-                this.logger.LogInformation("Environment is Development");
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/ErrorPage");
-
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
             if (!env.IsDevelopment())
             {
                 app.UseResponseCompression();
@@ -216,7 +203,7 @@ namespace HealthGateway.AdminWebClient
                             }
                         }
 
-                        return Task.CompletedTask;
+                        return Task.FromResult(0);
                     },
                     OnRedirectToIdentityProvider = ctx =>
                     {
