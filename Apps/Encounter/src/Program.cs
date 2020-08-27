10295d366772d32bbcca4ce2d6ae0d67d74d95c4
Tiago Graf <tiago.graf@gmail.com>
2020-08-27 14:36:48 -0700

    Updated namespaces

--- Apps/Encounter/src/Program.cs
+++ Apps/Encounter/src/Program.cs
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //-------------------------------------------------------------------------
-namespace HealthGateway.Laboratory
+namespace HealthGateway.Encounter
 {
     using HealthGateway.Common.AspNetConfiguration;
     using Microsoft.Extensions.Hosting;
