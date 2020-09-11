c49c1e1f13219bc10955c2a3ff3e8a117191ad69
leo <leo.pham@cgi.com>
2020-09-11 14:06:43 -0700

    Updated the nav footer AB#9801

--- Apps/WebClient/src/ClientApp/src/components/navmenu/navFooter.vue
+++ Apps/WebClient/src/ClientApp/src/components/navmenu/navFooter.vue
@@ -37,11 +37,26 @@
                 >Copyright</b-nav-item
             >
 
+            <b-nav-item class="nav-link" to="/termsOfService" target="_blank"
+                >Terms Of Service</b-nav-item
+            >
+
             <b-nav-item
                 class="nav-link"
-                href="https://www2.gov.bc.ca/gov/content/home/contact-us"
+                href="https://github.com/bcgov/healthgateway/wiki"
                 target="_blank"
+                >Release Notes</b-nav-item
+            >
+
+            <b-nav-item class="nav-link" to="/contactUs" target="_blank"
                 >Contact Us</b-nav-item
+            >
+
+            <b-nav-item
+                class="nav-link"
+                href="https://www2.gov.bc.ca/gov/content/health/healthgateway"
+                target="_blank"
+                >About Us</b-nav-item
             >
         </b-navbar-nav>
     </b-navbar>
