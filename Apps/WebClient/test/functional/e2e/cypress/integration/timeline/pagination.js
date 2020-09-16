f0cac072258868bd8970dde2a10aee7aea41f342
Stephen Laws <stephen.laws@nomostech.com>
2020-09-15 19:23:26 -0700

    Completes work on AB#9133 - Prep for Functional Tests

--- Apps/WebClient/test/functional/e2e/cypress/integration/timeline/pagination.js
+++ Apps/WebClient/test/functional/e2e/cypress/integration/timeline/pagination.js
@@ -1,3 +1,4 @@
+require('cypress-xpath')
 const { AuthMethod } = require("../../support/constants")
 
 describe('Pagination', () => {
@@ -7,8 +8,11 @@ describe('Pagination', () => {
                  AuthMethod.KeyCloak)
     })
 
-    it('Test1', () => {
-
+    it('Count Records', () => {
+        // xpath is an additional library and we should probably not use it much but this should
+        // help in migrating over Selenium Tests
+        cy.xpath('//*[contains(@class, "entryCard")]').should('have.length', 13)
+        cy.get('#listControls').find('.col').contains('Displaying 13 out of 442 records')
     })
 
     it('Test2', () => {
