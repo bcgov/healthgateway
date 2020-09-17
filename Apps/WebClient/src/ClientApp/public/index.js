ed76102d30524dcbeb4de8e14d77fdf03e7cb8e6
leo <leo.pham@cgi.com>
2020-09-17 13:40:13 -0700

    Moved JS code to a separate JS file AB#9106

---
+++ Apps/WebClient/src/ClientApp/public/index.js
@@ -0,0 +1,14 @@
+/*! index.js */
+function detectBrowser() {
+    const body = document.getElementsByTagName("BODY")[0];
+    const unsupportedBrowser = document.getElementById("unsupported-browser");
+    console.log("Modernizr.es6number: " + Modernizr.es6number);
+    if (Modernizr.es6number) {
+        body.removeChild(unsupportedBrowser);
+    } else {
+        body.removeChild(document.getElementById("app-root"));
+        const unsupportedBrowserMsg =
+            "Health Gateway is not compatible with this browser. Please use another browser.";
+        unsupportedBrowser.innerHTML = unsupportedBrowserMsg;
+    }
+}
