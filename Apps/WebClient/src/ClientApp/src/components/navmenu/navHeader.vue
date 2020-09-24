696548ebe1bf2c20c36a38d1f02b19967e437de6
FuriousLlama <marobej@gmail.com>
2020-09-24 14:22:45 -0700

    Fixing typo

--- Apps/WebClient/src/ClientApp/src/components/navmenu/navHeader.vue
+++ Apps/WebClient/src/ClientApp/src/components/navmenu/navHeader.vue
@@ -140,7 +140,7 @@ export default class HeaderComponent extends Vue {
         this.sidebarExpanded = false;
     }
 
-    private mouned() {
+    private mounted() {
         this.logger = container.get<ILogger>(SERVICE_IDENTIFIER.Logger);
     }
 
