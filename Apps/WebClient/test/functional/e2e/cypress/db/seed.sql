761bfe8b8b44525e743ef328e785f39b9f06e382
Tiago Graf <tiago.graf@gmail.com>
2020-09-30 13:26:01 -0700

    Update seed.sql

--- Apps/WebClient/test/functional/e2e/cypress/db/seed.sql
+++ Apps/WebClient/test/functional/e2e/cypress/db/seed.sql
@@ -1,36 +1,8 @@
-
-/* Unregistered HealthGateway User */
-DELETE FROM gateway."UserProfile" WHERE "UserProfileId" = 'S22BPV6WHS5TRLBL4XKGQDBVDUKLPIRSBGYSEJAHYMYRP22SP2TA';
-INSERT INTO gateway."UserProfile"(
-	"UserProfileId", 
-	"CreatedBy", 
-	"CreatedDateTime", 
-	"UpdatedBy", 
-	"UpdatedDateTime", 
-	"AcceptedTermsOfService", 
-	"Email", 
-	"ClosedDateTime", 
-	"IdentityManagementId", 
-	"LastLoginDateTime", 
-	"EncryptionKey", 
-	"SMSNumber")
-VALUES (
-	'S22BPV6WHS5TRLBL4XKGQDBVDUKLPIRSBGYSEJAHYMYRP22SP2TA',	
-	'System', 
-	current_timestamp, 
-	'System', 
-	current_timestamp, 
-	false, 
-	null,
-	null,
-	null,
-	null,
-	'9FdkVaUS2F/q0yiJiPR1aiOAsIe5LkAewFPG0vawg1Y=',
-	null
-);
+/* Data Cleanup */
+TRUNCATE gateway."UserProfile" CASCADE;
+TRUNCATE gateway."Communication" CASCADE;
 
 /* Registered HealthGateway User */
-DELETE FROM gateway."UserProfile" WHERE "UserProfileId" = 'P6FFO433A5WPMVTGM7T4ZVWBKCSVNAYGTWTU3J2LWMGUMERKI72A';
 INSERT INTO gateway."UserProfile"(
 	"UserProfileId", 
 	"CreatedBy", 
@@ -60,7 +32,6 @@ VALUES (
 );
 
 /* Protected User */
-DELETE FROM gateway."UserProfile" WHERE "UserProfileId" = 'RD33Y2LJEUZCY2TCMOIECUTKS3E62MEQ62CSUL6Q553IHHBI3AWQ';
 INSERT INTO gateway."UserProfile"(
 	"UserProfileId", 
 	"CreatedBy", 
@@ -86,5 +57,37 @@ VALUES (
 	null,
 	null,
 	'iHa5atSWqppGzWsR1Z8nbL9OHJamPHLMwYqdKmsf4jU=',
+	null
+);
+
+/* Communication Banner */
+
+INSERT INTO gateway."Communication"(
+	"CommunicationId", 
+	"CreatedBy", 
+	"CreatedDateTime", 
+	"UpdatedBy", 
+	"UpdatedDateTime", 
+	"Text", 
+	"Subject", 
+	"EffectiveDateTime", 
+	"ExpiryDateTime", 
+	"CommunicationStatusCode", 
+	"CommunicationTypeCode", 
+	"Priority", 
+	"ScheduledDateTime"
+) VALUES (
+	'f3ce0b06-9372-495f-baff-784c38b09480',	
+	'System',
+	current_timestamp,	
+	'System',
+	current_timestamp,	
+	'<p>Test Banner - <a href="healthgateway@gov.bc.ca" rel="noopener noreferrer nofollow">healthgateway@gov.bc.ca</a></p>',
+	'Test Banner',
+	current_timestamp,
+	current_timestamp + INTERVAL '1 day',
+	'New',
+	'Banner',
+	10,
 	null
 );
