1ffc0da4e51bdc1ef291454bb50f902a00abf557
Stephen Laws <stephen.laws@nomostech.com>
2020-10-02 16:34:26 -0700

    Fix CSP issue and Auth Tests

--- Apps/WebClient/test/functional/e2e/cypress/integration/authentication/auth.js
+++ Apps/WebClient/test/functional/e2e/cypress/integration/authentication/auth.js
@@ -4,16 +4,17 @@ describe('Authentication', () => {
     beforeEach(() => {
     })
 
+
     it('BCSC Login', () => {
         cy.login(Cypress.env('bcsc.username'), Cypress.env('bcsc.password'), AuthMethod.BCSC)
         cy.get('#menuBtnLogout').should('be.visible')
-            .should('have.text', ' Logout ')
+            .should('have.text', 'Logout')
     })
 
     it('KeyCloak Login', () => {
         cy.login(Cypress.env('keycloak.username'), Cypress.env('keycloak.password'), AuthMethod.KeyCloak)
         cy.get('#menuBtnLogout').should('be.visible')
-            .should('have.text', ' Logout ')
+            .should('have.text', 'Logout')
     })
 
     it('Logout', () => {
@@ -26,7 +27,7 @@ describe('Authentication', () => {
         cy.contains('h3', 'You signed out of your account')
         cy.get('#menuBtnLogin').should('be.visible')
             .should('have.attr', 'href', '/login')
-            .should('have.text', ' Login ')
+            .should('have.text', 'Login')
     })
 
     // it('Idle Timeout', () => {
