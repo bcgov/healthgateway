82d543fb94826e21824101eef5761090f19c4453
FuriousLlama <marobej@gmail.com>
2020-10-05 13:07:11 -0700

    Prevented the check if registered if the idp is not valid

--- Apps/WebClient/src/ClientApp/src/views/loginCallback.vue
+++ Apps/WebClient/src/ClientApp/src/views/loginCallback.vue
@@ -17,10 +17,10 @@ export default class LoginCallbackView extends Vue {
     @Action("checkRegistration", { namespace: "user" })
     checkRegistration!: (params: { hdid: string }) => Promise<boolean>;
 
-    @Getter("userIsRegistered", { namespace: "user" })
-    userIsRegistered!: boolean;
-
     @Getter("user", { namespace: "user" }) user!: User;
+
+    @Getter("isValidIdentityProvider", { namespace: "auth" })
+    isValidIdentityProvider!: boolean;
 
     private logger!: ILogger;
 
@@ -30,24 +30,18 @@ export default class LoginCallbackView extends Vue {
                 this.logger.debug(
                     `oidcSignInCallback for user: ${JSON.stringify(this.user)}`
                 );
-                this.checkRegistration({ hdid: this.user.hdid }).then(() => {
-                    if (this.userIsRegistered) {
-                        this.$router.push({ path: redirectPath });
-                    } else {
-                        if (redirectPath.startsWith("/registration")) {
+
+                // If the idp is valid, check the registration status and continue the route.
+                // Otherwise the router will handle the path.
+                if (this.isValidIdentityProvider) {
+                    this.checkRegistration({ hdid: this.user.hdid }).then(
+                        () => {
                             this.$router.push({ path: redirectPath });
-                        } else {
-                            this.$router.push({
-                                path: "/registration",
-                            });
                         }
-                    }
-                    this.logger.debug(
-                        `checkRegistration RedirectPath: ${JSON.stringify(
-                            redirectPath
-                        )}`
                     );
-                });
+                } else {
+                    this.$router.push({ path: redirectPath });
+                }
             })
             .catch((err) => {
                 // Login failed redirect it back to the login page.
