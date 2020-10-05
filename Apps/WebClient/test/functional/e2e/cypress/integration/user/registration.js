7ccc18a5ae7c003d2d1ed47915edce5cfac4ddc4
leo <leo.pham@cgi.com>
2020-10-05 16:27:35 -0700

    reverted registration AB#9176a

--- Apps/WebClient/test/functional/e2e/cypress/integration/user/registration.js
+++ Apps/WebClient/test/functional/e2e/cypress/integration/user/registration.js
@@ -2,16 +2,16 @@ const { AuthMethod } = require("../../support/constants")
 
 describe('Registration', () => {
     beforeEach(() => {
-        cy.login(Cypress.env('keycloak.accountclosure.username'), 
+        cy.login(Cypress.env('keycloak.username'), 
                  Cypress.env('keycloak.password'), 
                  AuthMethod.KeyCloak)
     })
-    
+
     it('Test1', () => {
 
     })
-    
+
     it('Test2', () => {
 
     })
-})
+}) 
