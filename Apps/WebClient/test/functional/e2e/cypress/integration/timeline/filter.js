18c8e4ec8f71d26760eb79eb53ab2e22b897d52c
FuriousLlama <marobej@gmail.com>
2020-10-07 16:04:50 -0700

    Updated tests

--- Apps/WebClient/test/functional/e2e/cypress/integration/timeline/filter.js
+++ Apps/WebClient/test/functional/e2e/cypress/integration/timeline/filter.js
@@ -124,4 +124,26 @@ describe("Filters", () => {
             .its("length")
             .should("eq", 25);
     });
+
+    it("Validate disabled filters", () => {
+        cy.server();
+        cy.fixture("AllDisabledConfig").as("config");
+        cy.fixture("AllDisabledConfig")
+            .then((config) => {
+                config.webClient.modules["Medication"] = true;
+            })
+            .as("config");
+        cy.route("GET", "/v1/api/configuration/", "@config");
+        cy.login(
+            Cypress.env("keycloak.username"),
+            Cypress.env("keycloak.password"),
+            AuthMethod.KeyCloak
+        );
+        cy.get("[data-testid=filterDropdown]").click();
+        cy.get("[data-testid=medicationCount]").should("be.visible");
+        cy.get("[data-testid=immunizationCount]").should("not.be.visible");
+        cy.get("[data-testid=encounterCount]").should("not.be.visible");
+        cy.get("[data-testid=noteCount]").should("not.be.visible");
+        cy.get("[data-testid=laboratoryCount]").should("not.be.visible");
+    });
 });
