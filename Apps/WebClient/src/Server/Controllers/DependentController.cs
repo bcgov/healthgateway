379ccbfb22f3cb10e6287651acc89596277a5aeb
leo <leo.pham@cgi.com>
2020-10-09 13:24:53 -0700

    Added DependentController AB#9236

---
+++ Apps/WebClient/src/Server/Controllers/DependentController.cs
@@ -0,0 +1,82 @@
+// -------------------------------------------------------------------------
+//  Copyright © 2019 Province of British Columbia
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------
+namespace HealthGateway.WebClient.Controllers
+{
+    using System.Security.Claims;
+    using HealthGateway.Common.AccessManagement.Authorization.Policy;
+    using HealthGateway.Common.Models;
+    using HealthGateway.Database.Constants;
+    using HealthGateway.Database.Models;
+    using HealthGateway.Database.Wrapper;
+    using HealthGateway.WebClient.Models;
+    using HealthGateway.WebClient.Services;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// Web API to handle dependent interactions.
+    /// </summary>
+    [Authorize]
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/api/[controller]")]
+    [ApiController]
+    public class DependentController
+    {
+        private readonly IDependentService dependentService;
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DependentController"/> class.
+        /// </summary>
+        /// <param name="dependentService">The injected user feedback service.</param>
+        /// <param name="httpContextAccessor">The injected http context accessor provider.</param>
+        public DependentController(
+            IDependentService dependentService,
+            IHttpContextAccessor httpContextAccessor)
+        {
+            this.dependentService = dependentService;
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        /// <summary>
+        /// Posts a dependent json to be inserted into the database.
+        /// </summary>
+        /// <returns>The http status.</returns>
+        /// <param name="dependent">The user feedback model.</param>
+        /// <response code="200">The dependent record was saved.</response>
+        /// <response code="400">The dependent object is invalid.</response>
+        /// <response code="409">The dependent was already inserted.</response>
+        /// <response code="401">The client must authenticate itself to get the requested response.</response>
+        /// <response code="403">The client does not have access rights to the content; that is, it is unauthorized, so the server is refusing to give the requested resource. Unlike 401, the client's identity is known to the server.</response>
+        [HttpPost]
+        [Authorize(Policy = UserPolicy.UserOnly)]
+        public IActionResult CreateUserFeedback([FromBody] DependentModel dependent)
+        {
+            if (dependent == null)
+            {
+                return new BadRequestResult();
+            }
+            else
+            {
+                ClaimsPrincipal user = this.httpContextAccessor.HttpContext.User;
+                dependent.ParentHdId = user.FindFirst("hdid").Value;
+                RequestResult<DependentModel> result = this.dependentService.CreateDependent(dependent);
+                return new JsonResult(result);
+            }
+        }
+    }
+}
---
+++ Apps/WebClient/src/Server/Models/DependentModel.cs
@@ -0,0 +1,117 @@
+//-------------------------------------------------------------------------
+// Copyright © 2019 Province of British Columbia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-------------------------------------------------------------------------
+namespace HealthGateway.WebClient.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.Json.Serialization;
+
+    /// <summary>
+    /// Represents a Dependent Model.
+    /// </summary>
+    public class DependentModel
+    {
+        /// <summary>
+        /// Gets or sets the dependent hdid.
+        /// </summary>
+        [JsonPropertyName("hdId")]
+        public string HdId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the parent's hdid.
+        /// </summary>
+        [JsonPropertyName("parentHdId")]
+        public string ParentHdId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the PHN.
+        /// </summary>
+        [JsonPropertyName("phn")]
+        public string PHN { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the Date of Birth datetime.
+        /// </summary>
+        [JsonPropertyName("dateOfBirth")]
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        [JsonPropertyName("firstName")]
+        public string FirstName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        [JsonPropertyName("lastName")]
+        public string LastName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the gender.
+        /// </summary>
+        [JsonPropertyName("gender")]
+        public string Gender { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Constructs a DependentModel from a Dependent database model.
+        /// </summary>
+        /// <param name="model">The dependent database model.</param>
+        /// <returns>The user note model.</returns>
+        public static DependentModel CreateFromDbModel(Database.Models.UserDelegate model)
+        {
+            if (model == null)
+            {
+                return null!;
+            }
+
+            return new DependentModel()
+            {
+                HdId = model.OwnerId,
+                ParentHdId = model.DelegateId,
+            };
+        }
+
+        /// <summary>
+        /// Constructs a List of DependentModels from a List of Dependent database models.
+        /// </summary>
+        /// <param name="models">The list of Dependent database model.</param>
+        /// <returns>A list of DependentModels.</returns>
+        public static IEnumerable<DependentModel> CreateListFromDbModel(IEnumerable<Database.Models.UserDelegate> models)
+        {
+            List<DependentModel> newList = new List<DependentModel>();
+            foreach (Database.Models.UserDelegate model in models)
+            {
+                newList.Add(DependentModel.CreateFromDbModel(model));
+            }
+
+            return newList;
+        }
+
+        /// <summary>
+        /// Constructs a database Note model from a user Node model.
+        /// </summary>
+        /// <returns>The database Dependent model.</returns>
+        public Database.Models.UserDelegate ToDbModel()
+        {
+            return new Database.Models.UserDelegate()
+            {
+                OwnerId = this.HdId,
+                DelegateId = this.ParentHdId,
+            };
+        }
+    }
+}
