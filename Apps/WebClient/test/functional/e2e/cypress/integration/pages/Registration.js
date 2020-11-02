c56d4df41e79b7e3f210599929bc8e826b633006
leo <leo.pham@cgi.com>
2020-11-02 15:48:55 -0800

    Switched to Keycloak user AB#9374

--- Apps/WebClient/test/functional/e2e/cypress/integration/pages/Registration.js
+++ Apps/WebClient/test/functional/e2e/cypress/integration/pages/Registration.js
@@ -16,7 +16,8 @@ describe("Registration Page", () => {
             config.webClient.modules.MedicationHistory = false
             config.webClient.modules.Note = false
             cy.server();
-            cy.route('GET', '/v1/api/configuration/', config);cy.login(Cypress.env('bcsc.hlthgw401.username'), Cypress.env('bcsc.hlthgw401.password'), AuthMethod.BCSC);
+            cy.route('GET', '/v1/api/configuration/', config);
+            cy.login(Cypress.env('bcsc.hlthgw401.username'), Cypress.env('bcsc.hlthgw401.password'), AuthMethod.BCSC);
         });
         cy.url().should("include", registrationPage);
         cy.get('[data-testid="minimumAgeErrorText"]').should('be.visible');
@@ -34,7 +35,7 @@ describe("Registration Page", () => {
             config.webClient.modules.Note = false
             cy.server();
             cy.route('GET', '/v1/api/configuration/', config);
-            cy.login(Cypress.env('bcsc.hthgtwy12.username'), Cypress.env('bcsc.hthgtwy12.password'), AuthMethod.BCSC);   
+            cy.login(Cypress.env('keycloak.healthgateway12.username'), Cypress.env('keycloak.healthgateway12.password'), AuthMethod.KeyCloak, "/registration");   
         });
         cy.url().should("include", registrationPage);
         cy.get('[data-testid="clientRegistryErrorText"]').should('be.visible');
