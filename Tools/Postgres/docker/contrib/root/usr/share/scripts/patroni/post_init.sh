6ce8862b75e5743400c0f89c699e870437ac4908
Stephen Laws <stephen.laws@nomostech.com>
2020-11-02 23:02:56 -0800

    Setup Gateway specific db config on post-init for AB#9481

--- Tools/Postgres/docker/contrib/root/usr/share/scripts/patroni/post_init.sh
+++ Tools/Postgres/docker/contrib/root/usr/share/scripts/patroni/post_init.sh
@@ -8,6 +8,12 @@ if [[ (! -z "$APP_USER") &&  (! -z "$APP_PASSWORD") && (! -z "$APP_DATABASE")]];
   echo "Creating database ${APP_DATABASE}"
   psql "$1" -w -c "CREATE DATABASE ${APP_DATABASE} OWNER ${APP_USER} ENCODING '${APP_DB_ENCODING:-UTF8}' LC_COLLATE = '${APP_DB_LC_COLLATE:-en_US.UTF-8}' LC_CTYPE = '${APP_DB_LC_CTYPE:-en_US.UTF-8}'"
 
+  echo "Alter public to be owned by ${APP_USER}"
+  psql "$1" -w -c "ALTER DATABASE gateway OWNER TO gateway;"
+
+  echo "Add UUID Extension and alter public to be owned by ${APP_USER}"
+  psql "$1" -d ${APP_DATABASE} -w -c "CREATE EXTENSION IF NOT EXISTS 'uuid-ossp';ALTER SCHEMA public OWNER to gateway;"
+
 else
   echo "Skipping user creation"
   echo "Skipping database creation"
