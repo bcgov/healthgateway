9519a5379c72e45e281b324541308a777a6b9dfc
Brad Head <38706615+bradhead@users.noreply.github.com>
2020-11-03 13:30:07 -0800

    Bugs/9483 - fixes SSO signon for JobScheduler AND  Chromium OIDC correlation issues. (#1215)
    * fixed emailJob deprecated call.
    
    * Got SSO working Hangfire. w/ change to common code.
    
    * added route for access denied when not in role
    
    * updated
    
    * updated to have cookie life match the JWT.
    
    * got http://localhost:5005 working on Chromium OIDC secure
    
    * fixed refactor common service name for oidc
    
    * clean up unused usings
    
    * reverted iis launch to use https
    
    * fixed path as per PR comment
    
    * Fixed hardcoded name for Cookie, fixed smells, jobscheduler routes

--- Apps/AdminWebClient/README.md
+++ Apps/AdminWebClient/README.md
@@ -39,7 +39,7 @@ dotnet run
 Open another terminal Window to run the VUE Cli
 
 ```bash
-cd $GATEWAYHOME/Apps/AdminWebClient/src/
+cd $GATEWAYHOME/Apps/AdminWebClient/src/ClientApp
 npm run serve
 ```
 
--- Apps/JobScheduler/src/Startup.cs
+++ Apps/JobScheduler/src/Startup.cs
@@ -13,21 +13,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //-------------------------------------------------------------------------
-
 namespace HealthGateway.JobScheduler
 {
     using System;
-    using System.IdentityModel.Tokens.Jwt;
-    using System.Security.Claims;
-    using System.Threading.Tasks;
+    using System.Collections.Generic;
 
     using Hangfire;
+    using Hangfire.Dashboard;
     using Hangfire.PostgreSql;
 
     using HealthGateway.Common.AccessManagement.Administration;
     using HealthGateway.Common.AccessManagement.Authentication;
     using HealthGateway.Common.AspNetConfiguration;
-    using HealthGateway.Common.Authorization.Admin;
     using HealthGateway.Common.Delegates;
     using HealthGateway.Common.FileDownload;
     using HealthGateway.Common.Jobs;
@@ -35,21 +32,17 @@ namespace HealthGateway.JobScheduler
     using HealthGateway.Database.Context;
     using HealthGateway.Database.Delegates;
     using HealthGateway.DrugMaintainer;
-    using HealthGateway.JobScheduler.Authorization;
     using Healthgateway.JobScheduler.Jobs;
     using Healthgateway.JobScheduler.Utils;
 
-    using Microsoft.AspNetCore.Authentication.Cookies;
     using Microsoft.AspNetCore.Authentication.OpenIdConnect;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.AspNetCore.StaticFiles;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
     using Microsoft.Extensions.Logging;
-    using Microsoft.IdentityModel.Tokens;
 
     /// <summary>
     /// The startup class.
@@ -82,7 +75,36 @@ namespace HealthGateway.JobScheduler
         {
             this.startupConfig.ConfigureForwardHeaders(services);
             this.startupConfig.ConfigureHttpServices(services);
-            this.ConfigureAuthentication(services);
+            services.AddRazorPages();
+            this.startupConfig.ConfigureOpenIdConnectServices(services);
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("allowAny", policy =>
+                {
+                    policy
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
+
+            string requiredUserRole = this.configuration.GetValue<string>("OpenIdConnect:UserRole");
+            string userRoleClaimType = this.configuration.GetValue<string>("OpenIdConnect:RolesClaim");
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("AdminUserPolicy", policy =>
+                    {
+                        policy.AddAuthenticationSchemes(OpenIdConnectDefaults.AuthenticationScheme);
+                        policy.RequireAuthenticatedUser();
+                        policy.RequireClaim(userRoleClaimType, requiredUserRole);
+                        policy.RequireAssertion(ctx =>
+                        {
+                            return ctx.User.HasClaim(userRoleClaimType, requiredUserRole);
+                        });
+                    });
+            });
 
             services.AddDbContextPool<GatewayDbContext>(options =>
                 options.UseNpgsql(
@@ -118,6 +140,9 @@ namespace HealthGateway.JobScheduler
 
             // Enable Hangfire
             services.AddHangfire(x => x.UsePostgreSqlStorage(this.configuration.GetConnectionString("GatewayConnection")));
+
+            // Add processing server as IHostedService
+            services.AddHangfireServer();
         }
 
         /// <summary>
@@ -128,23 +153,29 @@ namespace HealthGateway.JobScheduler
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             this.logger.LogInformation($"Hosting Environment: {env!.EnvironmentName}");
-            this.startupConfig.UseForwardHeaders(app!);
-            this.startupConfig.UseAuth(app!);
-            this.startupConfig.UseHttp(app!);
+            this.startupConfig.UseForwardHeaders(app);
+            this.startupConfig.UseHttp(app);
+            this.startupConfig.UseContentSecurityPolicy(app);
+            app.UseStaticFiles();
+            this.startupConfig.UseAuth(app);
+
+            if (!env.IsDevelopment())
+            {
+                app.UseResponseCompression();
+            }
+
             app.UseEndpoints(endpoints =>
             {
-                // Mapping of endpoints goes here:
-                endpoints.MapControllers();
+                endpoints.MapDefaultControllerRoute();
                 endpoints.MapRazorPages();
-                endpoints.MapControllerRoute(@"default", "{controller=Home}/{action=Index}/{id?}");
-            });
-
-            // Empty string signifies the root URL
-            app.UseHangfireDashboard(string.Empty, new DashboardOptions
-            {
-                DashboardTitle = this.configuration.GetValue<string>("DashboardTitle", "Hangfire Dashboard"),
-                Authorization = new[] { new AuthorizationDashboardFilter(this.configuration, this.logger) },
-                AppPath = $"{this.configuration.GetValue<string>("JobScheduler:AdminHome")}",
+                endpoints.MapControllers();
+                endpoints.MapHangfireDashboard(string.Empty, new DashboardOptions
+                {
+                    DashboardTitle = this.configuration.GetValue<string>("DashboardTitle", "Hangfire Dashboard"),
+                    AppPath = $"{this.configuration.GetValue<string>("JobScheduler:AdminHome")}",
+                    Authorization = new List<IDashboardAuthorizationFilter> { }, // Very important to set this, or Authorization won't work.
+                })
+                .RequireAuthorization("AdminUserPolicy");
             });
 
             app.UseHangfireServer();
@@ -167,116 +198,6 @@ namespace HealthGateway.JobScheduler
             SchedulerHelper.ScheduleJob<CleanCacheJob>(this.configuration, "CleanCache", j => j.Process());
 
             // SchedulerHelper.ScheduleJob<DeleteEmailJob>(this.configuration, "DeleteEmailJob", j => j.DeleteOldEmails());
-            app.UseStaticFiles(new StaticFileOptions
-            {
-                OnPrepareResponse = (content) =>
-                {
-                    var headers = content.Context.Response.Headers;
-                    var contentType = headers["Content-Type"];
-                    if (contentType != "application/x-gzip" && !content.File.Name.EndsWith(".gz", StringComparison.CurrentCultureIgnoreCase))
-                    {
-                        return;
-                    }
-
-                    var mimeTypeProvider = new FileExtensionContentTypeProvider();
-                    var fileNameToTry = content.File.Name.Substring(0, content.File.Name.Length - 3);
-                    if (mimeTypeProvider.TryGetContentType(fileNameToTry, out var mimeType))
-                    {
-                        headers.Add("Content-Encoding", "gzip");
-                        headers["Content-Type"] = mimeType;
-                    }
-                },
-            });
-        }
-
-        /// <summary>
-        /// This sets up the OIDC authentication for Hangfire.
-        /// </summary>
-        /// <param name="services">The passed in IServiceCollection.</param>
-        private void ConfigureAuthentication(IServiceCollection services)
-        {
-            string basePath = this.GetBasePath();
-            services.AddAuthentication(auth =>
-                {
-                    auth.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                    auth.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                    auth.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-                })
-                .AddCookie(options =>
-                {
-                    options.Cookie.Name = AuthorizationConstants.CookieName;
-                    options.LoginPath = $"{basePath}{AuthorizationConstants.LoginPath}";
-                    options.LogoutPath = $"{basePath}{AuthorizationConstants.LogoutPath}";
-                })
-                .AddOpenIdConnect(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                    };
-                    this.configuration.GetSection(@"OpenIdConnect").Bind(options);
-                    if (string.IsNullOrEmpty(options.Authority))
-                    {
-                        this.logger.LogCritical(@"OpenIdConnect Authority is missing, bad things are going to occur");
-                    }
-
-                    options.Events = new OpenIdConnectEvents()
-                    {
-                        OnTokenValidated = ctx =>
-                        {
-                            JwtSecurityToken accessToken = ctx.SecurityToken;
-                            if (accessToken != null)
-                            {
-                                if (ctx.Principal.Identity is ClaimsIdentity claimsIdentity)
-                                {
-                                    claimsIdentity.AddClaim(new Claim("access_token", accessToken.RawData));
-                                }
-                                else
-                                {
-                                    throw new TypeAccessException(@"Error setting access_token: ctx.Principal.Identity is not a ClaimsIdentity object.");
-                                }
-                            }
-
-                            return Task.CompletedTask;
-                        },
-                        OnRedirectToIdentityProvider = ctx =>
-                        {
-                            if (!string.IsNullOrEmpty(this.configuration["Keycloak:IDPHint"]))
-                            {
-                                this.logger.LogDebug("Adding IDP Hint on Redirect to provider");
-                                ctx.ProtocolMessage.SetParameter(this.configuration["Keycloak:IDPHintKey"], this.configuration["Keycloak:IDPHint"]);
-                            }
-
-                            return Task.FromResult(0);
-                        },
-                        OnAuthenticationFailed = c =>
-                        {
-                            c.HandleResponse();
-                            c.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                            c.Response.ContentType = "text/plain";
-                            this.logger.LogError(c.Exception.ToString());
-
-                            return c.Response.WriteAsync(c.Exception.ToString());
-                        },
-                    };
-                });
-        }
-
-        /// <summary>
-        /// Fetches the base path from the configuration.
-        /// </summary>
-        /// <returns>Theh BasePath config for the ForwardProxies.</returns>
-        private string GetBasePath()
-        {
-            string basePath = string.Empty;
-            IConfigurationSection section = this.configuration.GetSection("ForwardProxies");
-            if (section.GetValue<bool>("Enabled", false))
-            {
-                basePath = section.GetValue<string>("BasePath");
-            }
-
-            this.logger.LogDebug($"JobScheduler basePath = {basePath}");
-            return basePath;
         }
     }
 }
