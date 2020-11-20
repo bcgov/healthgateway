a8b509464c6f28fa0f24e3bd4a3a3a61b91b4867
Sam Warren <samuelwarren@uvic.ca>
2020-11-19 16:51:23 -0800

    add max date to fix the 6 digit date input

--- Apps/WebClient/src/ClientApp/src/components/modal/newDependent.vue
+++ Apps/WebClient/src/ClientApp/src/components/modal/newDependent.vue
@@ -226,6 +226,7 @@ export default class NewDependentComponent extends Vue {
                                     <b-form-input
                                         id="dateOfBirth"
                                         v-model="dependent.dateOfBirth"
+                                        max="2999-12-31"
                                         data-testid="dateOfBirthInput"
                                         required
                                         type="date"
