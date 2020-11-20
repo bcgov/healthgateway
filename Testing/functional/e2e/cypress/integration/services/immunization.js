46a1f9fdcdef3ba9b132870d235eb8bef2e6cf8c
FuriousLlama <marobej@gmail.com>
2020-11-20 12:40:54 -0800

    Added medication and Immunization tests

---
+++ Testing/functional/e2e/cypress/integration/services/immunization.js
@@ -0,0 +1,82 @@
+const { AuthMethod } = require("../../support/constants");
+
+describe("Immunization Service", () => {
+    beforeEach(() => {
+        cy.readConfig().as("config");
+        cy.getTokens(Cypress.env("keycloak.username"), Cypress.env("keycloak.password")).as("tokens");
+    });
+
+    it("Verify Swagger", () => {
+        cy.get("@config").then((config) => {
+            cy.log(
+                `Verifying Swagger exists for Immunization at Endpoint: ${config.serviceEndpoints.Immunization}swagger`
+            );
+            cy.visit(`${config.serviceEndpoints.Immunization}swagger`).contains(
+                "Health Gateway Immunization Services documentation"
+            );
+        });
+    });
+
+    it("Verify Immunization Unauthorized", () => {
+        const HDID = "P6FFO433A5WPMVTGM7T4ZVWBKCSVNAYGTWTU3J2LWMGUMERKI72A";
+        cy.get("@config").then((config) => {
+            cy.log(`Immunization Service Endpoint: ${config.serviceEndpoints.Immunization}`);
+            cy.request({
+                url: `${config.serviceEndpoints.Immunization}v1/api/Immunization/${HDID}`,
+                followRedirect: false,
+                failOnStatusCode: false,
+            }).should((response) => {
+                expect(response.status).to.eq(401);
+            });
+        });
+    });
+
+    it("Verify Immunization Forbidden", () => {
+        const HDID = "BOGUSHDID";
+        cy.get("@tokens").then((tokens) => {
+            cy.log("Tokens", tokens);
+            cy.get("@config").then((config) => {
+                cy.log(`Immunization Service Endpoint: ${config.serviceEndpoints.Immunization}`);
+                cy.request({
+                    url: `${config.serviceEndpoints.Immunization}v1/api/Immunization/${HDID}`,
+                    followRedirect: false,
+                    failOnStatusCode: false,
+                    auth: {
+                        bearer: tokens.access_token,
+                    },
+                    headers: {
+                        accept: "application/json",
+                    },
+                }).should((response) => {
+                    expect(response.status).to.eq(403);
+                });
+            });
+        });
+    });
+
+    it("Verify Immunization Authorized", () => {
+        const HDID = "P6FFO433A5WPMVTGM7T4ZVWBKCSVNAYGTWTU3J2LWMGUMERKI72A";
+        cy.fixture("ImmunizationService/Immunization.json").then((ImmunizationResponse) => {
+            cy.get("@tokens").then((tokens) => {
+                cy.log("Tokens", tokens);
+                cy.get("@config").then((config) => {
+                    cy.log(`Immunization Service Endpoint: ${config.serviceEndpoints.Immunization}`);
+                    cy.request({
+                        url: `${config.serviceEndpoints.Immunization}v1/api/Immunization/${HDID}`,
+                        followRedirect: false,
+                        auth: {
+                            bearer: tokens.access_token,
+                        },
+                        headers: {
+                            accept: "application/json",
+                        },
+                    }).should((response) => {
+                        expect(response.status).to.eq(200);
+                        expect(response.body).to.not.be.null;
+                        expect(response.body).to.deep.equal(ImmunizationResponse);
+                    });
+                });
+            });
+        });
+    });
+});
---
+++ Testing/functional/e2e/cypress/integration/services/medication.js
@@ -0,0 +1,82 @@
+const { AuthMethod } = require("../../support/constants");
+
+describe("Medication Service", () => {
+    beforeEach(() => {
+        cy.readConfig().as("config");
+        cy.getTokens(Cypress.env("keycloak.username"), Cypress.env("keycloak.password")).as("tokens");
+    });
+
+    it("Verify Swagger", () => {
+        cy.get("@config").then((config) => {
+            cy.log(
+                `Verifying Swagger exists for Medication at Endpoint: ${config.serviceEndpoints.Medication}swagger`
+            );
+            cy.visit(`${config.serviceEndpoints.Medication}swagger`).contains(
+                "Health Gateway Medication Services documentation"
+            );
+        });
+    });
+
+    it("Verify MedicationStatement Unauthorized", () => {
+        const HDID = "P6FFO433A5WPMVTGM7T4ZVWBKCSVNAYGTWTU3J2LWMGUMERKI72A";
+        cy.get("@config").then((config) => {
+            cy.log(`Medication Service Endpoint: ${config.serviceEndpoints.Medication}`);
+            cy.request({
+                url: `${config.serviceEndpoints.Medication}v1/api/MedicationStatement/${HDID}`,
+                followRedirect: false,
+                failOnStatusCode: false,
+            }).should((response) => {
+                expect(response.status).to.eq(401);
+            });
+        });
+    });
+
+    it("Verify MedicationStatement Forbidden", () => {
+        const HDID = "BOGUSHDID";
+        cy.get("@tokens").then((tokens) => {
+            cy.log("Tokens", tokens);
+            cy.get("@config").then((config) => {
+                cy.log(`Medication Service Endpoint: ${config.serviceEndpoints.Medication}`);
+                cy.request({
+                    url: `${config.serviceEndpoints.Medication}v1/api/MedicationStatement/${HDID}`,
+                    followRedirect: false,
+                    failOnStatusCode: false,
+                    auth: {
+                        bearer: tokens.access_token,
+                    },
+                    headers: {
+                        accept: "application/json",
+                    },
+                }).should((response) => {
+                    expect(response.status).to.eq(403);
+                });
+            });
+        });
+    });
+
+    it("Verify MedicationStatement Authorized", () => {
+        const HDID = "P6FFO433A5WPMVTGM7T4ZVWBKCSVNAYGTWTU3J2LWMGUMERKI72A";
+        cy.fixture("MedicationService/MedicationStatement.json").then((MedicationStatementResponse) => {
+            cy.get("@tokens").then((tokens) => {
+                cy.log("Tokens", tokens);
+                cy.get("@config").then((config) => {
+                    cy.log(`Medication Service Endpoint: ${config.serviceEndpoints.Medication}`);
+                    cy.request({
+                        url: `${config.serviceEndpoints.Medication}v1/api/MedicationStatement/${HDID}`,
+                        followRedirect: false,
+                        auth: {
+                            bearer: tokens.access_token,
+                        },
+                        headers: {
+                            accept: "application/json",
+                        },
+                    }).should((response) => {
+                        expect(response.status).to.eq(200);
+                        expect(response.body).to.not.be.null;
+                        expect(response.body).to.deep.equal(MedicationStatementResponse);
+                    });
+                });
+            });
+        });
+    });
+});
