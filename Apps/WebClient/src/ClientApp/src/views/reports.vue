d47aade03af92d4dad1ce0ad1856718ff8334c8e
Tiago Graf <tiago.graf@gmail.com>
2020-11-27 12:25:16 -0800

    Add preview to reports AB#9625

--- Apps/WebClient/src/ClientApp/src/views/reports.vue
+++ Apps/WebClient/src/ClientApp/src/views/reports.vue
@@ -68,7 +68,11 @@ export default class ReportsView extends Vue {
     private logger!: ILogger;
     private isDataLoaded = false;
     private fileMaxRecords = 1000;
-
+    private reportType = "";
+    private reportTypeOptions = [
+        { value: "", text: "Select a service" },
+        { value: "MED", text: "Medications" },
+    ];
     private get totalFiles(): number {
         return Math.ceil(
             this.medicationStatementHistory.length / this.fileMaxRecords
@@ -152,6 +156,11 @@ export default class ReportsView extends Vue {
                 if (results.resultStatus == ResultType.Success) {
                     this.protectiveWordAttempts = 0;
                     this.medicationStatementHistory = results.resourcePayload;
+                    // Required for the sample page
+                    this.medicationStatementHistoryPage = this.medicationStatementHistory.slice(
+                        0,
+                        50
+                    );
                     this.sortEntries();
                     this.isDataLoaded = true;
                 } else if (results.resultStatus == ResultType.Protected) {
@@ -226,46 +235,77 @@ export default class ReportsView extends Vue {
 <template>
     <div>
         <LoadingComponent :is-loading="isLoading"></LoadingComponent>
-        <b-row class="my-3 fluid">
-            <b-col
-                id="healthInsights"
-                class="col-12 col-md-10 col-lg-9 column-wrapper"
-            >
-                <PageTitleComponent
-                    :title="`Health Gateway Medication History Report`"
-                />
-                <div>
-                    <p>
-                        Download a copy of your PharmaNet record of prescription
-                        medication dispenses. This report will generate your
-                        full history in the PharmaNet system.
-                    </p>
-                </div>
-            </b-col>
-        </b-row>
-        <b-row>
-            <b-col>
-                <img
-                    class="mx-auto d-block"
-                    src="@/assets/images/reports/reports.png"
-                    width="200"
-                    height="auto"
-                    alt="..."
-                />
-            </b-col>
-        </b-row>
-        <b-row>
-            <b-col>
-                <b-button
-                    variant="primary"
-                    class="mx-auto mt-3 d-block"
-                    :disabled="!isDataLoaded"
-                    @click="showConfirmationModal"
+        <div class="my-3 fluid">
+            <b-row>
+                <b-col
+                    id="healthInsights"
+                    class="col-12 col-md-10 col-lg-9 column-wrapper"
                 >
-                    Download your report
-                </b-button>
-            </b-col>
-        </b-row>
+                    <PageTitleComponent :title="`Export Records`" />
+                    <div class="my-5 p-5 form">
+                        <b-row>
+                            <b-col class="col-md-6 col-12">
+                                <label for="reportType">Report Type</label>
+                                <b-form-select
+                                    id="reportType"
+                                    v-model="reportType"
+                                    data-testid="reportType"
+                                    :options="reportTypeOptions"
+                                >
+                                </b-form-select>
+                            </b-col>
+                            <b-col class="pt-4 col-md-6 col-12">
+                                <b-button
+                                    variant="primary"
+                                    data-testid="exportRecordBtn"
+                                    class="mx-auto mt-1 d-block"
+                                    :disabled="!isDataLoaded"
+                                    @click="showConfirmationModal"
+                                >
+                                    Export Records
+                                </b-button>
+                            </b-col>
+                        </b-row>
+                    </div>
+                    <div v-if="reportType == 'MED'">
+                        <b-col>
+                            <div class="mx-auto sample">
+                                <div class="scale">
+                                    <div ref="report">
+                                        <MedicationHistoryReportComponent
+                                            :medication-statement-history="
+                                                medicationStatementHistoryPage
+                                            "
+                                            :name="fullName"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </b-col>
+                    </div>
+                    <div v-else>
+                        <b-row>
+                            <b-col>
+                                <img
+                                    class="mx-auto d-block"
+                                    src="@/assets/images/reports/reports.png"
+                                    width="200"
+                                    height="auto"
+                                    alt="..."
+                                />
+                            </b-col>
+                        </b-row>
+                        <b-row>
+                            <b-col class="text-center">
+                                <h5>
+                                    Select a service above to create a report
+                                </h5>
+                            </b-col>
+                        </b-row>
+                    </div>
+                </b-col>
+            </b-row>
+        </div>
         <MessageModalComponent
             ref="messageModal"
             title="Sensitive Document Download"
@@ -279,16 +319,6 @@ export default class ReportsView extends Vue {
             @submit="onProtectiveWordSubmit"
             @cancel="onProtectiveWordCancel"
         />
-        <div class="d-none">
-            <div ref="report">
-                <MedicationHistoryReportComponent
-                    :medication-statement-history="
-                        medicationStatementHistoryPage
-                    "
-                    :name="fullName"
-                />
-            </div>
-        </div>
     </div>
 </template>
 
@@ -304,5 +334,22 @@ export default class ReportsView extends Vue {
 
 #pageTitle hr {
     border-top: 2px solid $primary;
+}
+
+.sample {
+    border: 2px $lightGrey solid;
+    padding: 15px;
+    width: 300px;
+    height: 400px;
+    overflow: hidden;
+}
+.form {
+    background-color: $soft_background;
+    border: $lightGrey solid 1px;
+    border-radius: 5px 5px 5px 5px;
+}
+.scale {
+    zoom: 0.5;
+    -moz-transform: scale(0.5);
 }
 </style>
