5f583da8148253c8d9123e11e90c55cb45f5a57f
Tiago Graf <tiago.graf@gmail.com>
2020-12-01 15:18:49 -0800

    Update newDependent.vue

--- Apps/WebClient/src/ClientApp/src/components/modal/newDependent.vue
+++ Apps/WebClient/src/ClientApp/src/components/modal/newDependent.vue
@@ -236,7 +236,7 @@ export default class NewDependentComponent extends Vue {
                                             isValid($v.dependent.dateOfBirth)
                                         "
                                     >
-                                        Dependent must be under the age of 12
+                                        Dependent must be under the age of {{ webClientConfig.maxDependentAge }}
                                     </b-form-invalid-feedback>
                                 </b-col>
                             </b-row>
