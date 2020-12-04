9c37752395cb32c67a609b0005938788848cbfb6
Tiago Graf <tiago.graf@gmail.com>
2020-12-03 17:06:55 -0800

    Update report.js

--- Testing/functional/e2e/cypress/integration/report/report.js
+++ Testing/functional/e2e/cypress/integration/report/report.js
@@ -4,6 +4,7 @@ describe('Reports', () => {
     let sensitiveDocText = ' The file that you are downloading contains personal information. If you are on a public computer, please ensure that the file is deleted before you log off. ';
     before(() => {
         cy.readConfig().as("config").then(config => {
+            config.webClient.modules.Encounter = true
             cy.server();
             cy.route('GET', '/v1/api/configuration/', config);
             cy.login(Cypress.env('keycloak.username'), Cypress.env('keycloak.password'), AuthMethod.KeyCloak, "/reports");
