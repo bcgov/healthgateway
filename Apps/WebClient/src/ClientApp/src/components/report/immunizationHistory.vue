f97c1fd91faad3e175982940bd1e754108920fe1
leobcgov <phamminhhoang@gmail.com>
2020-12-16 09:35:20 -0800

    improved the quality AB#9729

--- Apps/WebClient/src/ClientApp/src/components/report/immunizationHistory.vue
+++ Apps/WebClient/src/ClientApp/src/components/report/immunizationHistory.vue
@@ -108,7 +108,7 @@ export default class ImmunizationHistoryReportComponent extends Vue {
         let opt = {
             margin: [25, 15],
             filename: `HealthGateway_ImmunizationHistory.pdf`,
-            image: { type: "jpeg", quality: 1 },
+            image: { type: "jpeg", quality: 0.98 },
             html2canvas: { dpi: 192, scale: 1.1, letterRendering: true },
             jsPDF: { unit: "pt", format: "letter", orientation: "portrait" },
             pagebreak: { mode: ["avoid-all", "css", "legacy"] },
