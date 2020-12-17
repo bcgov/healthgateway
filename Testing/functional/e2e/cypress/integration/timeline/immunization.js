c02e6f05120328599241702255002655b74b055d
Stephen Laws <stephen.laws@nomostech.com>
2020-12-17 12:29:50 -0800

    Add functional tests for AB#9722

--- Testing/functional/e2e/cypress/integration/timeline/immunization.js
+++ Testing/functional/e2e/cypress/integration/timeline/immunization.js
@@ -1,24 +1,46 @@
 const { AuthMethod, localDevUri } = require("../../support/constants")
-
+let immunizationAPIPath=""
 describe('Immunization', () => {
-    beforeEach(() => {
+    before(() => {
         cy.readConfig().as("config").then(config => {
-            config.webClient.modules.CovidLabResults = false
-            config.webClient.modules.Comment = false
-            config.webClient.modules.Encounter = false
-            config.webClient.modules.Immunization = true
-            config.webClient.modules.Laboratory = false
-            config.webClient.modules.Medication = false
-            config.webClient.modules.MedicationHistory = false
-            config.webClient.modules.Note = false
+          config.webClient.modules.CovidLabResults = false
+          config.webClient.modules.Comment = false
+          config.webClient.modules.Encounter = false
+          config.webClient.modules.Immunization = true
+          config.webClient.modules.Laboratory = false
+          config.webClient.modules.Medication = false
+          config.webClient.modules.MedicationHistory = false
+          config.webClient.modules.Note = false
+          immunizationAPIPath=config.serviceEndpoints.Immunization
+          cy.fixture("ImmunizationService/immunizationrefresh.json").then((ImmunizationResponse) => {
             cy.server();
             cy.route('GET', '/v1/api/configuration/', config);
+            cy.route('GET', `${immunizationAPIPath}v1/api/Immunization/*`, ImmunizationResponse);
             cy.log("Configuration", config);
+            cy.log("ImmunizationResponse", ImmunizationResponse)
+          })
         })
         cy.login(Cypress.env('keycloak.username'),
             Cypress.env('keycloak.password'),
             AuthMethod.KeyCloak);
         cy.checkTimelineHasLoaded();
+    })
+
+    it('Validate Immunization Loading', () => {
+          cy.get('[data-testid=immunizationLoading]')
+          .should('be.visible')
+          cy.fixture("ImmunizationService/immunization.json").then((ImmunizationResponse) => {
+            cy.server();
+            cy.route('GET', `${immunizationAPIPath}v1/api/Immunization/*`, ImmunizationResponse);
+            cy.log("ImmunizationResponse", ImmunizationResponse);
+            cy.get('[data-testid=immunizationLoading]')
+              .should('not.be.visible')
+            cy.get('[data-testid=immunizationReady]')
+              .should('be.visible')
+              .find('[data-testid=immunizationBtnReady]')
+                .should('be.visible')
+                .click()
+          })
     })
 
     it('Validate Card Details', () => {
