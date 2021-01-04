c38a590338af25fa39555201b916dcccb7ea84bc
leobcgov <phamminhhoang@gmail.com>
2021-01-04 15:11:54 -0800

    fixed spacing AB#9616

--- Apps/WebClient/src/Server/Startup.cs
+++ Apps/WebClient/src/Server/Startup.cs
@@ -161,8 +161,10 @@ namespace HealthGateway.WebClient
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+
                 // Redirect http to https
                 app.UseHttpsRedirection();
             }
