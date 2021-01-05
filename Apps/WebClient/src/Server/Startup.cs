f703222edb92357956bdf08975ca909d3004b077
leobcgov <phamminhhoang@gmail.com>
2021-01-05 13:42:16 -0800

    Removed Redirection AB#9616

--- Apps/WebClient/src/Server/Startup.cs
+++ Apps/WebClient/src/Server/Startup.cs
@@ -125,12 +125,6 @@ namespace HealthGateway.WebClient
                 options.MaxAge = TimeSpan.FromDays(365);
                 options.Preload = true;
             });
-            // Configure HTTPS redirection
-            services.AddHttpsRedirection(options =>
-            {
-                options.RedirectStatusCode = StatusCodes.Status301MovedPermanently;
-                options.HttpsPort = 443;
-            });
 
             // In production, the Vue files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -164,9 +158,6 @@ namespace HealthGateway.WebClient
 
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-
-                // Redirect http to https
-                app.UseHttpsRedirection();
             }
 
             if (!env.IsDevelopment())
