eade171ca2432f138e1df52bf3c51cec679a37e9
Tiago Graf <tiago.graf@gmail.com>
2021-01-26 15:22:54 -0800

    Update authenticationData.ts

--- Apps/AdminWebClient/src/ClientApp/src/models/authenticationData.ts
+++ Apps/AdminWebClient/src/ClientApp/src/models/authenticationData.ts
@@ -4,7 +4,6 @@ export default interface AuthenticationData {
     token: string;
     isAuthenticated: boolean;
     isAuthorized: boolean;
-    roles: string[];
     userProfile: UserProfile;
     roles: string[];
 }
