a18cbc58d435a40d97d25f0caf4b49fc6f1134af
leobcgov <phamminhhoang@gmail.com>
2021-01-26 10:27:28 -0800

    Add commentTypeCode validation AB#9960

--- Testing/functional/e2e/cypress/integration/timeline/comments.js
+++ Testing/functional/e2e/cypress/integration/timeline/comments.js
@@ -1,6 +1,8 @@
 const { AuthMethod } = require("../../support/constants")
+const BASEURL = "/v1/api/UserProfile/"
+const HDID='P6FFO433A5WPMVTGM7T4ZVWBKCSVNAYGTWTU3J2LWMGUMERKI72A'
 
-function verifyAdd() {
+function verifyAdd(entryTypeCode) {
     cy.log('Validate ADD comment...')
     cy.get('[data-testid=addCommentTextArea]')
         .first()
@@ -10,6 +12,25 @@ function verifyAdd() {
         .click();
     cy.get('[data-testid=commentText]')
         .contains('test comment goes here!');
+    
+    cy.get("@tokens").then(tokens => {
+        cy.request({
+            url: `${BASEURL}${HDID}/Comment/`,
+            followRedirect: false,
+            auth: {
+                bearer: tokens.access_token
+            },
+            headers: {
+                accept: 'application/json'
+            }
+        })
+        .should((response) => { 
+            expect(response.status).to.eq(200)
+            expect(response.body).to.not.be.null
+            cy.log(`response.body: ${JSON.stringify(response.body)}`)
+            expect(JSON.stringify(response.body.resourcePayload)).contains(`"entryTypeCode":"${entryTypeCode}"`);
+        })
+    });
 }
 function verifyEdit() {
     cy.log('Validate EDIT comment...')
@@ -42,11 +63,16 @@ function verifyDelete() {
 }
 
 describe('Comments', () => {
+    beforeEach(() => {
+        cy.getTokens(Cypress.env('keycloak.username'), Cypress.env('keycloak.password'))
+            .as("tokens")
+    })
+
     it('Validate Add/Edit/Delete for MED', () => {
         cy.enableModules(["Medication", "Comment"]);
         cy.login(Cypress.env('keycloak.username'), Cypress.env('keycloak.password'), AuthMethod.KeyCloak);
         cy.checkTimelineHasLoaded();
-        verifyAdd();
+        verifyAdd('Med');
         verifyEdit();
         verifyDelete();
     });
@@ -54,7 +80,7 @@ describe('Comments', () => {
         cy.enableModules(["Laboratory", "Comment"]);
         cy.login(Cypress.env('keycloak.username'), Cypress.env('keycloak.password'), AuthMethod.KeyCloak);
         cy.checkTimelineHasLoaded();
-        verifyAdd();
+        verifyAdd('Lab');
         verifyEdit();
         verifyDelete();
     });
@@ -62,7 +88,7 @@ describe('Comments', () => {
         cy.enableModules(["Encounter", "Comment"]);
         cy.login(Cypress.env('keycloak.username'), Cypress.env('keycloak.password'), AuthMethod.KeyCloak);
         cy.checkTimelineHasLoaded();
-        verifyAdd();
+        verifyAdd('Enc');
         verifyEdit();
         verifyDelete();
     });
