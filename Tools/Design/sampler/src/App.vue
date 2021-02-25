f2d46ef2bffd0fc3b14919a72f82962defb0aedb
Manuel Rodriguez <marobej@gmail.com>
2021-02-25 11:42:47 -0800

    Added initial sample page draft AB#10192

---
+++ Tools/Design/sampler/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    Welcome to the HG Sampler!
+    <Samples />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+import Samples from "./components/Samples.vue";
+
+@Component({
+  components: {
+    Samples,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
