f01b41247595b2a8c407d7edb3f42c8b447e1b5b
leobcgov <phamminhhoang@gmail.com>
2021-03-02 09:23:29 -0800

    Fixed lint & corrected IDs AB#10200

--- Apps/WebClient/src/ClientApp/src/views/faq.vue
+++ Apps/WebClient/src/ClientApp/src/views/faq.vue
@@ -122,20 +122,32 @@
                 role="tab"
             >
                 <b-button
-                    v-b-toggle.accordion-faqAnswer2
+                    v-b-toggle.accordion-faqAnswer3
                     data-testid="questionBtn"
                     block
                     variant="info"
                     class="text-left"
-                    >Q: I see immunizations that I received in the pharmacy, like flu shots, but I don’t see immunizations from my family doctor or travel clinic. Why is that? </b-button
-                >
+                    >Q: I see immunizations that I received in the pharmacy,
+                    like flu shots, but I don’t see immunizations from my family
+                    doctor or travel clinic. Why is that?
+                </b-button>
             </b-card-header>
-            <b-collapse id="accordion-faqAnswer2" role="tabpanel">
+            <b-collapse id="accordion-faqAnswer3" role="tabpanel">
                 <b-card-body>
                     <b-card-text
                         ><div data-testid="answerTxt">
                             <p>
-                                A: The Health Gateway provides information from the Provincial Immunization Registry, which contains most immunizations administered by public health and pharmacies in BC  . However, immunizations administered by family practice or travel clinics are not recorded in the Registry. Note that immunizations administered by a pharmacy may be displayed in your profile as a dispensed medication rather than an immunization , but these will be added to the Registry in the future. 
+                                A: The Health Gateway provides information from
+                                the Provincial Immunization Registry, which
+                                contains most immunizations administered by
+                                public health and pharmacies in BC . However,
+                                immunizations administered by family practice or
+                                travel clinics are not recorded in the Registry.
+                                Note that immunizations administered by a
+                                pharmacy may be displayed in your profile as a
+                                dispensed medication rather than an immunization
+                                , but these will be added to the Registry in the
+                                future.
                             </p>
                         </div></b-card-text
                     >
@@ -149,48 +161,32 @@
                 role="tab"
             >
                 <b-button
-                    v-b-toggle.accordion-faqAnswer2
+                    v-b-toggle.accordion-faqAnswer4
                     data-testid="questionBtn"
                     block
                     variant="info"
                     class="text-left"
-                    >Q: What is the difference between the Health Gateway and other portals, such as My Health?</b-button
+                    >Q: What is the difference between the Health Gateway and
+                    other portals, such as My Health?</b-button
                 >
             </b-card-header>
-            <b-collapse id="accordion-faqAnswer2" role="tabpanel">
+            <b-collapse id="accordion-faqAnswer4" role="tabpanel">
                 <b-card-body>
                     <b-card-text
                         ><div data-testid="answerTxt">
                             <p>
-                                A: The Health Gateway is a provincial service which provides British Columbians with access to information, regardless of where they received care or services.  </p>
- <p> My Health and other regional portals provide access to information within the regional systems. We are collaborating with our health sector partners to ensure we are providing complimentary services. </p>  
-                                                </div></b-card-text
-                    >
-                </b-card-body>
-            </b-collapse>
-        </b-card>
-        <b-card no-body class="mb-1 border-0">
-            <b-card-header
-                header-tag="header"
-                class="p-1 faq-header"
-                role="tab"
-            >
-                <b-button
-                    v-b-toggle.accordion-faqAnswer2
-                    data-testid="questionBtn"
-                    block
-                    variant="info"
-                    class="text-left"
-                    >Q: I manage my elderly parent’s health care. Can I access their information through the Health Gateway? </b-button
-                >
-            </b-card-header>
-            <b-collapse id="accordion-faqAnswer2" role="tabpanel">
-                <b-card-body>
-                    <b-card-text
-                        ><div data-testid="answerTxt">
+                                A: The Health Gateway is a provincial service
+                                which provides British Columbians with access to
+                                information, regardless of where they received
+                                care or services.
+                            </p>
                             <p>
-                               The Health Gateway currently provides guardians with access to their children’s COVID-19 test results. We are working to enable family and caregiver access in the future.  
-                                                           </p>
+                                My Health and other regional portals provide
+                                access to information within the regional
+                                systems. We are collaborating with our health
+                                sector partners to ensure we are providing
+                                complimentary services.
+                            </p>
                         </div></b-card-text
                     >
                 </b-card-body>
@@ -203,7 +199,38 @@
                 role="tab"
             >
                 <b-button
-                    v-b-toggle.accordion-faqAnswer2
+                    v-b-toggle.accordion-faqAnswer5
+                    data-testid="questionBtn"
+                    block
+                    variant="info"
+                    class="text-left"
+                    >Q: I manage my elderly parent’s health care. Can I access
+                    their information through the Health Gateway?
+                </b-button>
+            </b-card-header>
+            <b-collapse id="accordion-faqAnswer5" role="tabpanel">
+                <b-card-body>
+                    <b-card-text
+                        ><div data-testid="answerTxt">
+                            <p>
+                                The Health Gateway currently provides guardians
+                                with access to their children’s COVID-19 test
+                                results. We are working to enable family and
+                                caregiver access in the future.
+                            </p>
+                        </div></b-card-text
+                    >
+                </b-card-body>
+            </b-collapse>
+        </b-card>
+        <b-card no-body class="mb-1 border-0">
+            <b-card-header
+                header-tag="header"
+                class="p-1 faq-header"
+                role="tab"
+            >
+                <b-button
+                    v-b-toggle.accordion-faqAnswer6
                     data-testid="questionBtn"
                     block
                     variant="info"
@@ -211,12 +238,16 @@
                     >Q: How frequently do you add new features?</b-button
                 >
             </b-card-header>
-            <b-collapse id="accordion-faqAnswer2" role="tabpanel">
+            <b-collapse id="accordion-faqAnswer6" role="tabpanel">
                 <b-card-body>
                     <b-card-text
                         ><div data-testid="answerTxt">
                             <p>
-                                The Health Gateway is updated weekly with new features and information. We prioritize our enhancements based on your feedback, and we encourage you to provide suggestions by e-mailing HealthGateway@gov.bc.ca 
+                                The Health Gateway is updated weekly with new
+                                features and information. We prioritize our
+                                enhancements based on your feedback, and we
+                                encourage you to provide suggestions by
+                                e-mailing HealthGateway@gov.bc.ca
                             </p>
                         </div></b-card-text
                     >
