987fd36fa1ab8faec270d7e4858938a8142deb5c
ninosamson <33761012+ninosamson@users.noreply.github.com>
2021-03-02 09:14:46 -0800

    FAQ Update AB#10200

--- Apps/WebClient/src/ClientApp/src/views/faq.vue
+++ Apps/WebClient/src/ClientApp/src/views/faq.vue
@@ -115,6 +115,114 @@
                 </b-card-body>
             </b-collapse>
         </b-card>
+        <b-card no-body class="mb-1 border-0">
+            <b-card-header
+                header-tag="header"
+                class="p-1 faq-header"
+                role="tab"
+            >
+                <b-button
+                    v-b-toggle.accordion-faqAnswer2
+                    data-testid="questionBtn"
+                    block
+                    variant="info"
+                    class="text-left"
+                    >Q: I see immunizations that I received in the pharmacy, like flu shots, but I don’t see immunizations from my family doctor or travel clinic. Why is that? </b-button
+                >
+            </b-card-header>
+            <b-collapse id="accordion-faqAnswer2" role="tabpanel">
+                <b-card-body>
+                    <b-card-text
+                        ><div data-testid="answerTxt">
+                            <p>
+                                A: The Health Gateway provides information from the Provincial Immunization Registry, which contains most immunizations administered by public health and pharmacies in BC  . However, immunizations administered by family practice or travel clinics are not recorded in the Registry. Note that immunizations administered by a pharmacy may be displayed in your profile as a dispensed medication rather than an immunization , but these will be added to the Registry in the future. 
+                            </p>
+                        </div></b-card-text
+                    >
+                </b-card-body>
+            </b-collapse>
+        </b-card>
+        <b-card no-body class="mb-1 border-0">
+            <b-card-header
+                header-tag="header"
+                class="p-1 faq-header"
+                role="tab"
+            >
+                <b-button
+                    v-b-toggle.accordion-faqAnswer2
+                    data-testid="questionBtn"
+                    block
+                    variant="info"
+                    class="text-left"
+                    >Q: What is the difference between the Health Gateway and other portals, such as My Health?</b-button
+                >
+            </b-card-header>
+            <b-collapse id="accordion-faqAnswer2" role="tabpanel">
+                <b-card-body>
+                    <b-card-text
+                        ><div data-testid="answerTxt">
+                            <p>
+                                A: The Health Gateway is a provincial service which provides British Columbians with access to information, regardless of where they received care or services.  </p>
+ <p> My Health and other regional portals provide access to information within the regional systems. We are collaborating with our health sector partners to ensure we are providing complimentary services. </p>  
+                                                </div></b-card-text
+                    >
+                </b-card-body>
+            </b-collapse>
+        </b-card>
+        <b-card no-body class="mb-1 border-0">
+            <b-card-header
+                header-tag="header"
+                class="p-1 faq-header"
+                role="tab"
+            >
+                <b-button
+                    v-b-toggle.accordion-faqAnswer2
+                    data-testid="questionBtn"
+                    block
+                    variant="info"
+                    class="text-left"
+                    >Q: I manage my elderly parent’s health care. Can I access their information through the Health Gateway? </b-button
+                >
+            </b-card-header>
+            <b-collapse id="accordion-faqAnswer2" role="tabpanel">
+                <b-card-body>
+                    <b-card-text
+                        ><div data-testid="answerTxt">
+                            <p>
+                               The Health Gateway currently provides guardians with access to their children’s COVID-19 test results. We are working to enable family and caregiver access in the future.  
+                                                           </p>
+                        </div></b-card-text
+                    >
+                </b-card-body>
+            </b-collapse>
+        </b-card>
+        <b-card no-body class="mb-1 border-0">
+            <b-card-header
+                header-tag="header"
+                class="p-1 faq-header"
+                role="tab"
+            >
+                <b-button
+                    v-b-toggle.accordion-faqAnswer2
+                    data-testid="questionBtn"
+                    block
+                    variant="info"
+                    class="text-left"
+                    >Q: How frequently do you add new features?</b-button
+                >
+            </b-card-header>
+            <b-collapse id="accordion-faqAnswer2" role="tabpanel">
+                <b-card-body>
+                    <b-card-text
+                        ><div data-testid="answerTxt">
+                            <p>
+                                The Health Gateway is updated weekly with new features and information. We prioritize our enhancements based on your feedback, and we encourage you to provide suggestions by e-mailing HealthGateway@gov.bc.ca 
+                            </p>
+                        </div></b-card-text
+                    >
+                </b-card-body>
+            </b-collapse>
+        </b-card>
     </div>
 </template>
 
