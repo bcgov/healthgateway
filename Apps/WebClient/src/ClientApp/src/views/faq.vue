dc7b3fb123863dd8c4d528858b1bc147d8d92f3a
ninosamson <33761012+ninosamson@users.noreply.github.com>
2021-03-09 15:04:34 -0800

    update date format

--- Apps/WebClient/src/ClientApp/src/views/faq.vue
+++ Apps/WebClient/src/ClientApp/src/views/faq.vue
@@ -9,7 +9,7 @@
             </b-col>
         </b-row>
         <b-row class="m-1 fluid text-right">
-            <b-col> Last updated: <strong>March 3, 2021</strong> </b-col>
+            <b-col> Last updated: <strong>2021-Mar-10</strong> </b-col>
         </b-row>
         <b-card no-body class="mb-1 border-0">
             <b-card-header
