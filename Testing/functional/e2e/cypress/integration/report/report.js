ca2b6d6ca075b239c9d1be9ba3620c27d86c6fb4
Stephen Laws <51342761+sslaws@users.noreply.github.com>
2021-03-22 10:12:08 -0700

    Added functional tests for Export SA Requests AB#10339 (#2068)

--- Testing/functional/e2e/cypress/integration/report/report.js
+++ Testing/functional/e2e/cypress/integration/report/report.js
@@ -4,7 +4,7 @@ describe('Reports', () => {
     let sensitiveDocText = ' The file that you are downloading contains personal information. If you are on a public computer, please ensure that the file is deleted before you log off. ';
     before(() => {
         cy.setupDownloads();
-        cy.enableModules(["Encounter", "Medication", "Laboratory", "Immunization"]);
+        cy.enableModules(["Encounter", "Medication", "Laboratory", "Immunization", "MedicationRequest"]);
         cy.login(Cypress.env('keycloak.username'), Cypress.env('keycloak.password'), AuthMethod.KeyCloak, "/reports");
     })
 
@@ -223,4 +223,34 @@ describe('Reports', () => {
         cy.get('[data-testid=genericMessageModal]')
             .should('not.exist');
     })
+
+    it('Validate Special Authority Report', () => {
+      cy.get('[data-testid=reportType]')
+          .should('be.enabled', 'be.visible')
+          .select("Special Authority Requests")
+
+      cy.get('[data-testid=medicationRequestReportSample]')
+          .should('be.visible')
+
+      cy.viewport('iphone-6');
+      cy.get('[data-testid=medicationRequestReportSample]')
+          .should('not.be.visible');
+      cy.viewport(1000, 600);
+
+      cy.get('[data-testid=exportRecordBtn]')
+          .should('be.enabled', 'be.visible')
+          .click();
+
+      cy.get('[data-testid=genericMessageModal]')
+          .should('be.visible');
+
+      cy.get('[data-testid=genericMessageText]')
+          .should('have.text', sensitiveDocText);
+
+      cy.get('[data-testid=genericMessageSubmitBtn]')
+          .click();
+
+      cy.get('[data-testid=genericMessageModal]')
+          .should('not.exist');
+  })
 }) 
