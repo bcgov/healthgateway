cc95be984de0eb64c64b01be2f22b2a884f15874
Mike Lyttle <mikelyttle@gmail.com>
2021-03-26 18:48:29 -0700

    Add StatusLabel component AB#10378

---
+++ Apps/WebClient/src/ClientApp/src/components/shared/statusLabel.vue
@@ -0,0 +1,46 @@
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class StatusLabelComponent extends Vue {
+    @Prop({ required: true }) status!: string;
+    @Prop({ required: false, default: "" }) variant!: string;
+    @Prop({ required: false, default: "Status" }) heading!: string;
+
+    private get classes(): string[] {
+        switch (this.variant) {
+            case "success":
+                return ["text-success"];
+            case "danger":
+                return ["text-danger"];
+            default:
+                return [];
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="hg-status-label">
+        <span class="text-muted">{{ heading }}: </span>
+        <span v-bind="$attrs" :class="classes">{{ status }}</span>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/_variables.scss";
+
+.hg-status-label {
+    color: $hg-text-primary !important;
+    .text-muted {
+        color: $hg-text-secondary !important;
+    }
+    .text-success {
+        color: $hg-state-success !important;
+    }
+    .text-danger {
+        color: $hg-state-danger !important;
+    }
+}
+</style>
