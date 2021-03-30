33e9ba40c75ec3e342ce1d75f591df553ec7cf47
mk08sh <mandad.tabrizi@gmail.com>
2021-03-30 13:19:05 -0700

    Linting

--- Apps/WebClient/src/ClientApp/src/views/releaseNotes.vue
+++ Apps/WebClient/src/ClientApp/src/views/releaseNotes.vue
@@ -6,7 +6,7 @@
                     <b-col>
                         <div id="pageTitle">
                             <h1 id="subject">Health Gateway Release Notes</h1>
-                            <hr/>
+                            <hr />
                         </div>
                     </b-col>
                 </b-row>
@@ -15,25 +15,30 @@
                         <b-row class="mb-2">
                             <b-col>
                                 <strong
-                                    >March 31, 2021 - v1.3.13 - Exclude on Medication Export, 
-                                    Email Verification, Timeline Updates
-                            </strong>
+                                    >March 31, 2021 - v1.3.13 - Exclude on
+                                    Medication Export, Email Verification,
+                                    Timeline Updates
+                                </strong>
                             </b-col>
                         </b-row>
                         <b-row>
                             <b-col>
                                 <ul>
                                     <li>
-                                        We added the ability to exclude certain medications by 
-                                        brand name on the medication history export. 
+                                        We added the ability to exclude certain
+                                        medications by brand name on the
+                                        medication history export.
                                     </li>
                                     <li>
-                                        We updated the email verification flow to indicate 
-                                        when you've already verified your email. 
+                                        We updated the email verification flow
+                                        to indicate when you've already verified
+                                        your email.
                                     </li>
                                     <li>
-                                        We added an icon to be displayed when pdf attachments are available. 
-                                        The input for comments was moved to the bottom of the expanded timeline card page.                                        
+                                        We added an icon to be displayed when
+                                        pdf attachments are available. The input
+                                        for comments was moved to the bottom of
+                                        the expanded timeline card page.
                                     </li>
                                 </ul>
                             </b-col>
