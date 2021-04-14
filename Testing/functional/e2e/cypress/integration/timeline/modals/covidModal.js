8576c3bddbb12efa513d397570dbcb109b604856
Tiago Graf <tiago.graf@gmail.com>
2021-04-14 16:09:13 -0700

    Update covidModal.js

--- Testing/functional/e2e/cypress/integration/timeline/modals/covidModal.js
+++ Testing/functional/e2e/cypress/integration/timeline/modals/covidModal.js
@@ -1,62 +1,27 @@
 const { AuthMethod } = require("../../../support/constants");
 
-const BASEURL = "/v1/api/UserProfile/";
-const HDID = "P6FFO433A5WPMVTGM7T4ZVWBKCSVNAYGTWTU3J2LWMGUMERKI72A";
-let accessToken = {};
-let userProfile = {};
-
-function enableCovidModal(actionedCovidModalAt) {
-    if (actionedCovidModalAt != undefined) {
-        actionedCovidModalAt.value = "2019-12-02T15:48:13.981-08:00";
-        cy.request({
-            method: "PUT",
-            url: `${BASEURL}${HDID}/preference`,
-            auth: {
-                bearer: accessToken,
-            },
-            headers: {
-                accept: "application/json",
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(actionedCovidModalAt),
-        }).should((response) => {
-            cy.log(`response.body: ${JSON.stringify(response.body)}`);
-        });
-    }
-}
+const actionedCovidModalAt = "2019-12-02T15:48:13.981-08:00";
+let interceptPreference = true;
 
 describe("Validate Modals Popup", () => {
     beforeEach(() => {
-        cy.getTokens(
-            Cypress.env("keycloak.username"),
-            Cypress.env("keycloak.password")
-        ).as("tokens");
-        cy.get("@tokens").then((tokens) => {
-            cy.log("Tokens", tokens);
-            accessToken = tokens.access_token;
-            cy.request({
-                url: `${BASEURL}${HDID}`,
-                followRedirect: false,
-                auth: {
-                    bearer: accessToken,
-                },
-                headers: {
-                    accept: "application/json",
-                },
-            }).should((response) => {
-                userProfile = response.body.resourcePayload;
-                cy.log(`response.body: ${JSON.stringify(response.body)}`);
-                enableCovidModal(userProfile.preferences.actionedCovidModalAt);
+        cy.intercept("GET", "/v1/api/UserProfile/*", (req) => {
+            req.reply((res) => {
+                if (interceptPreference) {
+                    res.body.resourcePayload.preferences.actionedCovidModalAt.value = actionedCovidModalAt;
+                }
             });
         });
-    });
-
-    it("Covid Modal", () => {
+        cy.enableModules("Laboratory");
         cy.login(
             Cypress.env("keycloak.username"),
             Cypress.env("keycloak.password"),
             AuthMethod.KeyCloak
         );
+        cy.checkTimelineHasLoaded();
+    });
+
+    it("Covid Modal", () => {
         cy.get("[data-testid=covidModal]").contains("COVID-19");
         cy.get("[data-testid=covidModalText]").contains(
             "Check the status of your COVID-19 test and view your result when it is available"
@@ -64,7 +29,6 @@ describe("Validate Modals Popup", () => {
         cy.get("[data-testid=covidViewResultBtn]")
             .should("be.visible")
             .contains("View Result");
-        cy.reload();
         cy.get("[data-testid=covidModal]").contains("COVID-19");
         cy.get("[data-testid=covidModalText]").contains(
             "Check the status of your COVID-19 test and view your result when it is available"
@@ -85,11 +49,6 @@ describe("Validate Modals Popup", () => {
     });
 
     it("Dismiss Covid Modal", () => {
-        cy.login(
-            Cypress.env("keycloak.username"),
-            Cypress.env("keycloak.password"),
-            AuthMethod.KeyCloak
-        );
         cy.get("[data-testid=covidModal] header:first")
             .find("button")
             .should("have.text", "×")
@@ -100,7 +59,9 @@ describe("Validate Modals Popup", () => {
         cy.get("[data-testid=filterDropdown] > span").contains("0");
 
         // Verify that Covid Modal doens't display after reload the timeline.
+        interceptPreference = false;
         cy.reload();
+        cy.checkTimelineHasLoaded();
         cy.get("[data-testid=covidModal]").should("not.exist");
         cy.get("[data-testid=timelineLabel]").should("be.visible");
     });
