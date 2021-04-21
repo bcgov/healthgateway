e1a545d29f2415aba520a16aa3e66e30accba5c2
Mike Lyttle <mikelyttle@gmail.com>
2021-04-20 17:19:58 -0700

    Add icon component for design system AB#10459

---
+++ Apps/WebClient/src/ClientApp/src/components/shared/hgIcon.vue
@@ -0,0 +1,45 @@
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class HgIcon extends Vue {
+    @Prop({ required: false, default: "medium" }) size!: string;
+
+    private get classes(): string[] {
+        switch (this.size) {
+            case "small":
+                return ["hg-icon", "hg-small"];
+            case "medium":
+                return ["hg-icon", "hg-medium"];
+            case "large":
+                return ["hg-icon", "hg-large"];
+            default:
+                return [];
+        }
+    }
+}
+</script>
+
+<template>
+    <font-awesome-icon v-bind="$attrs" :class="classes" v-on="$listeners">
+    </font-awesome-icon>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/_variables.scss";
+
+.hg-icon {
+    box-sizing: content-box;
+    height: 1em;
+    &.hg-small {
+        font-size: 0.875rem;
+    }
+    &.hg-medium {
+        font-size: 1rem;
+    }
+    &.hg-large {
+        font-size: 1.5rem;
+    }
+}
+</style>
