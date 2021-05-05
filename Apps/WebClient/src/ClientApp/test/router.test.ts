36d8cb600a63759e1d7f4779c10eccc566ab82f9
Manuel Rodriguez <marobej@gmail.com>
2021-05-04 18:11:39 -0700

    Fixed missing import

--- Apps/WebClient/src/ClientApp/test/router.test.ts
+++ Apps/WebClient/src/ClientApp/test/router.test.ts
@@ -1,10 +1,12 @@
+// eslint-disable-next-line
+import container from "@/plugins/inversify.container";
+
 import StoreOptionsStub from "@test/stubs/store/storeOptionsStub";
 import { Route } from "vue-router";
 
 import { RegistrationStatus } from "@/constants/registrationStatus";
 import { WebClientConfiguration } from "@/models/configData";
 import { SERVICE_IDENTIFIER, STORE_IDENTIFIER } from "@/plugins/inversify";
-import container from "@/plugins/inversify.container";
 import router, { beforeEachGuard, ClientModule, UserState } from "@/router";
 import { ILogger, IStoreProvider } from "@/services/interfaces";
 import { WinstonLogger } from "@/services/winstonLogger";
