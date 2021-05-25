c6ed6a2d3e41013319fd4a21594c0d91e1ed2754
Stephen Laws <stephen.laws@nomostech.com>
2021-05-24 18:49:59 -0700

    DB Model for Wallet Connection and Credentials for AB#10605

--- Apps/Database/src/Context/GatewayDbContext.cs
+++ Apps/Database/src/Context/GatewayDbContext.cs
@@ -78,6 +78,9 @@ namespace HealthGateway.Database.Context
         public DbSet<EventLog> EventLog { get; set; } = null!;
         public DbSet<AdminTag> AdminTag { get; set; } = null!;
         public DbSet<UserFeedbackTag> UserFeedbackTag { get; set; } = null!;
+        public DbSet<WalletConnection> WalletConnection { get; set; } = null!;
+        public DbSet<WalletCredential> WalletCredential { get; set; } = null!;
+
 #pragma warning restore CS1591, SA1600
 
         /// <inheritdoc />
@@ -324,6 +327,33 @@ namespace HealthGateway.Database.Context
                 .HasIndex(p => p.Name)
                 .IsUnique(true);
 
+            // Wallet Setup
+            modelBuilder.Entity<WalletConnection>()
+                .HasOne<WalletConnectionStatusCode>()
+                .WithMany()
+                .HasPrincipalKey(k => k.StatusCode)
+                .HasForeignKey(k => k.Status);
+
+            var walletConnectionStatusConverter = new ValueConverter<WalletConnectionStatus, string>(
+                v => EnumUtility.ToEnumString<WalletConnectionStatus>(v, false),
+                v => EnumUtility.ToEnum<WalletConnectionStatus>(v, false));
+
+            modelBuilder.Entity<WalletConnection>()
+                .Property(e => e.Status)
+                .HasConversion(walletConnectionStatusConverter);
+
+            modelBuilder.Entity<WalletCredential>()
+                .HasOne<WalletCredentialStatusCode>()
+                .WithMany()
+                .HasPrincipalKey(k => k.StatusCode)
+                .HasForeignKey(k => k.Status);
+
+            modelBuilder.Entity<WalletCredential>()
+                .Property(e => e.Status)
+                .HasConversion(new ValueConverter<WalletCredentialStatus, string>(
+                    v => EnumUtility.ToEnumString<WalletCredentialStatus>(v, false),
+                    v => EnumUtility.ToEnum<WalletCredentialStatus>(v, false)));
+
             // Initial seed data
             this.SeedProgramTypes(modelBuilder);
             this.SeedEmail(modelBuilder);
@@ -334,6 +364,7 @@ namespace HealthGateway.Database.Context
             this.SeedCommunication(modelBuilder);
             this.SeedResourceDelegateReason(modelBuilder);
             this.SeedCommentEntryTypeCode(modelBuilder);
+            this.SeedWalletCodes(modelBuilder);
         }
 
         /// <summary>
@@ -918,6 +949,71 @@ namespace HealthGateway.Database.Context
                 {
                     CommentEntryCode = CommentEntryType.Encounter,
                     Description = "Comment for an Encounter Entry",
+                    CreatedBy = UserId.DefaultUser,
+                    CreatedDateTime = this.DefaultSeedDate,
+                    UpdatedBy = UserId.DefaultUser,
+                    UpdatedDateTime = this.DefaultSeedDate,
+                });
+        }
+
+        /// <summary>
+        /// Seeds the Wallet Connection and Credential codes.
+        /// </summary>
+        /// <param name="modelBuilder">The passed in model builder.</param>
+        private void SeedWalletCodes(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WalletConnectionStatusCode>().HasData(
+                new WalletConnectionStatusCode
+                {
+                    StatusCode = WalletConnectionStatus.Pending,
+                    Description = "Wallet Connection is Pending",
+                    CreatedBy = UserId.DefaultUser,
+                    CreatedDateTime = this.DefaultSeedDate,
+                    UpdatedBy = UserId.DefaultUser,
+                    UpdatedDateTime = this.DefaultSeedDate,
+                },
+                new WalletConnectionStatusCode
+                {
+                    StatusCode = WalletConnectionStatus.Connected,
+                    Description = "Wallet Connection has been created and added",
+                    CreatedBy = UserId.DefaultUser,
+                    CreatedDateTime = this.DefaultSeedDate,
+                    UpdatedBy = UserId.DefaultUser,
+                    UpdatedDateTime = this.DefaultSeedDate,
+                },
+                new WalletConnectionStatusCode
+                {
+                    StatusCode = WalletConnectionStatus.Disconnected,
+                    Description = "Wallet Connection has been revoked",
+                    CreatedBy = UserId.DefaultUser,
+                    CreatedDateTime = this.DefaultSeedDate,
+                    UpdatedBy = UserId.DefaultUser,
+                    UpdatedDateTime = this.DefaultSeedDate,
+                });
+
+            modelBuilder.Entity<WalletCredentialStatusCode>().HasData(
+                new WalletCredentialStatusCode
+                {
+                    StatusCode = WalletCredentialStatus.Created,
+                    Description = "Wallet Credential has been created",
+                    CreatedBy = UserId.DefaultUser,
+                    CreatedDateTime = this.DefaultSeedDate,
+                    UpdatedBy = UserId.DefaultUser,
+                    UpdatedDateTime = this.DefaultSeedDate,
+                },
+                new WalletCredentialStatusCode
+                {
+                    StatusCode = WalletCredentialStatus.Added,
+                    Description = "Credential has been added to Wallet",
+                    CreatedBy = UserId.DefaultUser,
+                    CreatedDateTime = this.DefaultSeedDate,
+                    UpdatedBy = UserId.DefaultUser,
+                    UpdatedDateTime = this.DefaultSeedDate,
+                },
+                new WalletCredentialStatusCode
+                {
+                    StatusCode = WalletCredentialStatus.Revoked,
+                    Description = "Credential has been revoked",
                     CreatedBy = UserId.DefaultUser,
                     CreatedDateTime = this.DefaultSeedDate,
                     UpdatedBy = UserId.DefaultUser,
