748c5748f2f23e3a69567586d257cef29222dd1b
Manuel Rodriguez <marobej@gmail.com>
2021-07-07 16:11:53 -0700

    Fixed linter issues

--- Apps/WebClient/src/ClientApp/test/vueSetup.ts
+++ Apps/WebClient/src/ClientApp/test/vueSetup.ts
@@ -3,8 +3,10 @@ import Vue from "vue";
 
 import { SnowplowWindow } from "@/plugins/extensions";
 
+import { voidMethod } from "./stubs/util";
+
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
 declare let window: SnowplowWindow;
-window.snowplow = function () {};
+window.snowplow = voidMethod;
