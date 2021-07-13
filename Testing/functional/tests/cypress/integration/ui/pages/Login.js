04ccd9bd4380fc7c0c965aa973be6f981477e26f
Stephen Laws <51342761+sslaws@users.noreply.github.com>
2021-07-13 09:32:32 -0700

    Fix functional tests for login and nav for AB#10887 (#2576)

--- Testing/functional/tests/cypress/integration/ui/pages/Login.js
+++ Testing/functional/tests/cypress/integration/ui/pages/Login.js
@@ -15,6 +15,6 @@ describe("Login Page", () => {
         cy.get('[data-testid="loginBtn"]')
             .should("be.visible")
             .should("have.attr", "href", "/login")
-            .should("have.text", "Login");
+            .should("have.text", "Log In");
     });
 });
