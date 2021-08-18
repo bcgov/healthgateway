8ae0c9d80e57a1a78c35df8860de15d4bd0615b6
Mike Lyttle <mikelyttle@gmail.com>
2021-08-17 20:13:33 -0700

    Wire up vaccination status front-end to back-end AB#11068

---
+++ Apps/WebClient/src/ClientApp/src/services/restVaccinationStatusService.ts
@@ -0,0 +1,69 @@
+import { injectable } from "inversify";
+
+import { Dictionary } from "@/models/baseTypes";
+import { ExternalConfiguration } from "@/models/configData";
+import { StringISODate } from "@/models/dateWrapper";
+import { ServiceName } from "@/models/errorInterfaces";
+import RequestResult from "@/models/requestResult";
+import VaccinationStatus from "@/models/vaccinationStatus";
+import { SERVICE_IDENTIFIER } from "@/plugins/inversify";
+import container from "@/plugins/inversify.container";
+import {
+    IHttpDelegate,
+    ILogger,
+    IVaccinationStatusService,
+} from "@/services/interfaces";
+import ErrorTranslator from "@/utility/errorTranslator";
+
+@injectable()
+export class RestVaccinationStatusService implements IVaccinationStatusService {
+    private logger: ILogger = container.get(SERVICE_IDENTIFIER.Logger);
+    private readonly VACCINATION_STATUS_BASE_URI: string =
+        "v1/api/VaccineStatus";
+    private baseUri = "";
+    private http!: IHttpDelegate;
+    private isEnabled = false;
+
+    public initialize(
+        config: ExternalConfiguration,
+        http: IHttpDelegate
+    ): void {
+        this.baseUri = config.serviceEndpoints["Immunization"];
+        this.http = http;
+        this.isEnabled = config.webClient.modules["Immunization"];
+    }
+
+    public getVaccinationStatus(
+        phn: string,
+        dateOfBirth: StringISODate
+    ): Promise<RequestResult<VaccinationStatus>> {
+        return new Promise((resolve, reject) => {
+            if (!this.isEnabled) {
+                reject();
+                return;
+            }
+
+            const headers: Dictionary<string> = {};
+            headers["phn"] = phn;
+            headers["dateOfBirth"] = dateOfBirth;
+
+            return this.http
+                .getWithCors<RequestResult<VaccinationStatus>>(
+                    `${this.baseUri}${this.VACCINATION_STATUS_BASE_URI}`,
+                    headers
+                )
+                .then((requestResult) => {
+                    resolve(requestResult);
+                })
+                .catch((err) => {
+                    this.logger.error(`Fetch error: ${err}`);
+                    reject(
+                        ErrorTranslator.internalNetworkError(
+                            err,
+                            ServiceName.Immunization
+                        )
+                    );
+                });
+        });
+    }
+}
