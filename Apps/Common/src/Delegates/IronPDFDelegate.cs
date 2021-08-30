b84e2af90c0b49d32e2bda17f14bc070bb78a2de
Stephen Laws <stephen.laws@nomostech.com>
2021-08-30 16:22:43 -0700

    Added license and metadata for IronPDF for AB#11173

--- Apps/Common/src/Delegates/IronPDFDelegate.cs
+++ Apps/Common/src/Delegates/IronPDFDelegate.cs
@@ -21,16 +21,36 @@ namespace HealthGateway.Common.Delegates
     using HealthGateway.Common.Models;
     using HealthGateway.Common.Models.CDogs;
     using IronPdf;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
 
     /// <inheritdoc/>
     public class IronPDFDelegate : IIronPDFDelegate
     {
+        private const string ConfigKey = "IronPDF";
+        private readonly ILogger logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IronPDFDelegate"/> class.
+        /// </summary>
+        /// <param name="logger">The injected logger to use.</param>
+        /// <param name="configuration">The injected configuration provider.</param>
+        public IronPDFDelegate(ILogger<IronPDFDelegate> logger, IConfiguration configuration)
+        {
+            this.logger = logger;
+            configuration.Bind(ConfigKey, this.Configuration);
+            InitializeLicense(logger, this.Configuration);
+        }
+
         private static ActivitySource Source { get; } = new ActivitySource(nameof(IronPDFDelegate));
+
+        private IronPDFConfig Configuration { get; set; } = new ();
 
         /// <inheritdoc/>
         public RequestResult<ReportModel> GeneratePDF(IronPDFRequestModel request)
         {
             string html = request.HtmlTemplate;
+            this.logger.LogTrace("Generating PDF");
             using (Source.StartActivity("GeneratePDF"))
             {
                 if (request.Data != null)
@@ -41,8 +61,21 @@ namespace HealthGateway.Common.Delegates
                     }
                 }
 
-                using HtmlToPdf renderer = new HtmlToPdf();
+                using HtmlToPdf renderer = new ();
+                renderer.RenderingOptions.PrintHtmlBackgrounds = true;
                 PdfDocument pdfDoc = renderer.RenderHtmlAsPdf(html);
+
+                this.logger.LogTrace("Applying metadata to PDF");
+                pdfDoc.SecuritySettings.AllowUserAnnotations = this.Configuration.AllowUserAnnotations;
+                if (this.Configuration.AllowUserEdits)
+                {
+                    pdfDoc.SecuritySettings.AllowUserEdits = IronPdf.Security.PdfEditSecurity.NoEdit;
+                }
+
+                pdfDoc.MetaData.Producer = this.Configuration.Producer;
+                pdfDoc.MetaData.Author = this.Configuration.Author;
+                pdfDoc.MetaData.Subject = this.Configuration.Subject;
+                pdfDoc.MetaData.Title = this.Configuration.Title;
 
                 return new RequestResult<ReportModel>()
                 {
@@ -53,6 +86,23 @@ namespace HealthGateway.Common.Delegates
                         FileName = request.FileName,
                     },
                 };
+            }
+        }
+
+        private static void InitializeLicense(ILogger logger, IronPDFConfig config)
+        {
+            if (!License.IsLicensed && !string.IsNullOrEmpty(config.LicenseKey))
+            {
+                logger.LogDebug("Applying IronPdf license");
+                License.LicenseKey = config.LicenseKey;
+                if (!License.IsLicensed)
+                {
+                    logger.LogWarning("IronPdf is NOT licensed and will result in watermarks");
+                }
+            }
+            else
+            {
+                logger.LogWarning("IronPdf license was not configured and will result in watermarks");
             }
         }
     }
