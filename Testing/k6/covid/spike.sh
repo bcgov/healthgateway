7b135c6238e757b4a9e85d359c9e9dec7aa339ef
bradhead <brad@zedwerks.com>
2021-09-06 08:52:20 -0700

    removed data. and sh

--- Testing/k6/covid/spike.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-##
-## How to run this on command-line:
-## HG_PASSWORD=<password> HG_ENV=<env> ./load.sh
-##
-docker run -v $PWD:/app  -a STDOUT -a STDERR -i loadimpact/k6 run -e HG_ENV=$HG_ENV -e TYPE=spike k6_card.js
