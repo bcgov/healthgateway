fdaea12db6382383eae9749c2a6735a58d3f12d5
Brian Jang <58790456+BrianMaki@users.noreply.github.com>
2021-10-04 13:41:48 -0700

    Fix functional tests for invalid doses to use Keycloak User instead of BCSC for AB#11418 (#2846)
    Co-authored-by: Brian Jang <brian.jang@quartech.com>

--- Testing/functional/tests/cypress/integration/e2e/report/immunizationInvalidDoses.js
+++ Testing/functional/tests/cypress/integration/e2e/report/immunizationInvalidDoses.js
@@ -5,33 +5,30 @@ describe("Export Reports - Immunizations - Invalid Doses", () => {
         cy.enableModules("Immunization");
     });
 
-    it("Immunization Report - Show all doses for Partially Vaccinated with 1 Valid Dose and 2 Invalid Doses - Registered BCSC user", () => {
+    it("Immunization Report - Show all doses for Partially Vaccinated with 1 Valid Dose and 2 Invalid Doses - Registered Keycloak user", () => {
         const validDoseDate1 = "2021-Jul-14";
         const invalidDoseDate1 = "2021-Mar-30";
         const invalidDoseDate2 = "2021-Apr-02";
-        if (Cypress.config().baseUrl != localDevUri) {
-            cy.login(
-                Cypress.env("bcsc.20.username"),
-                Cypress.env("bcsc.20.password"),
-                AuthMethod.BCSC,
-                "/reports"
-            );
-            cy.get("[data-testid=reportType]")
-                .should("be.enabled", "be.visible")
-                .select("Immunizations");
-            cy.get("[data-testid=timelineLoading]").should("be.visible");
-            cy.get("[data-testid=timelineLoading]").should("not.be.visible");
-            cy.get("[data-testid=immunizationDateItem]")
-                .contains(validDoseDate1)
-                .should("be.visible");
-            cy.get("[data-testid=immunizationDateItem]")
-                .contains(invalidDoseDate1)
-                .should("be.visible");
-            cy.get("[data-testid=immunizationDateItem]")
-                .contains(invalidDoseDate2)
-                .should("be.visible");
-        } else {
-            cy.log("Cannot test via BCSC UI Login as running locally");
-        }
+
+        cy.login(
+            Cypress.env("keycloak.invaliddoses.username"),
+            Cypress.env("keycloak.password"),
+            AuthMethod.KeyCloak,
+            "/reports"
+        );
+        cy.get("[data-testid=reportType]")
+            .should("be.enabled", "be.visible")
+            .select("Immunizations");
+        cy.get("[data-testid=timelineLoading]").should("be.visible");
+        cy.get("[data-testid=timelineLoading]").should("not.be.visible");
+        cy.get("[data-testid=immunizationDateItem]")
+            .contains(validDoseDate1)
+            .should("be.visible");
+        cy.get("[data-testid=immunizationDateItem]")
+            .contains(invalidDoseDate1)
+            .should("be.visible");
+        cy.get("[data-testid=immunizationDateItem]")
+            .contains(invalidDoseDate2)
+            .should("be.visible");
     });
 });
