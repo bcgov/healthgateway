59c900ecf77256d1bb207ba3923765852c989b07
Mike Lyttle <mikelyttle@gmail.com>
2021-10-19 13:58:37 -0700

    Fix method names to include "Async" AB#11414

--- Apps/Common/src/Delegates/IVaccineProofDelegate.cs
+++ Apps/Common/src/Delegates/IVaccineProofDelegate.cs
@@ -32,7 +32,7 @@ namespace HealthGateway.Common.Delegates
         /// <param name="address">The address where the Vaccine Proof should be mailed.</param>
         /// <param name="addressee">The name of the person to which the Vaccine Proof should be addressed.</param>
         /// <returns>A response object that includes the status and identifier of the Vaccine Proof.</returns>
-        public Task<RequestResult<VaccineProofResponse>> Mail(VaccineProofTemplate vaccineProofTemplate, VaccineProofRequest request, Address address, string addressee);
+        public Task<RequestResult<VaccineProofResponse>> MailAsync(VaccineProofTemplate vaccineProofTemplate, VaccineProofRequest request, Address address, string addressee);
 
         /// <summary>
         /// Initiates the creation of a Vaccine Proof for later retrieval.
@@ -40,14 +40,14 @@ namespace HealthGateway.Common.Delegates
         /// <param name="vaccineProofTemplate">The template to be used for the Vaccine Proof.</param>
         /// <param name="request">The vaccination data to be used for generating the Vaccine Proof.</param>
         /// <returns>A response object that includes the status and identifier of the Vaccine Proof.</returns>
-        public Task<RequestResult<VaccineProofResponse>> Generate(VaccineProofTemplate vaccineProofTemplate, VaccineProofRequest request);
+        public Task<RequestResult<VaccineProofResponse>> GenerateAsync(VaccineProofTemplate vaccineProofTemplate, VaccineProofRequest request);
 
         /// <summary>
         /// Fetches the status of a Vaccine Proof to determine if it has been generated and can be retrieved.
         /// </summary>
         /// <param name="id">The id from the VaccineProofResponse object.</param>
         /// <returns>A response object that includes the status and identifier of the Vaccine Proof.</returns>
-        public Task<RequestResult<VaccineProofResponse>> GetStatus(string id);
+        public Task<RequestResult<VaccineProofResponse>> GetStatusAsync(string id);
 
         /// <summary>
         /// Fetches the generated Vaccine Proof.
