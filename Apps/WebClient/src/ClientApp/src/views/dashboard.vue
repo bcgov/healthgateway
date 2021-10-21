af712025c6189fd11f5fa2af1d970237388ef5b1
Princess Agustin <Princess.Agustin@quartech.com>
2021-10-21 16:29:20 -0700

    Enable or Disable the federal card button AB#11471

--- Apps/WebClient/src/ClientApp/src/views/dashboard.vue
+++ Apps/WebClient/src/ClientApp/src/views/dashboard.vue
@@ -52,6 +52,10 @@ export default class DashboardView extends Vue {
     private showSensitiveDocumentDownloadModal() {
         this.sensitivedocumentDownloadModal.showModal();
     }
+
+    private get showFederalCardButton(): boolean {
+        return this.config.modules["FederalCardButton"];
+    }
 }
 </script>
 
@@ -119,7 +123,13 @@ export default class DashboardView extends Vue {
         <page-title title="Dashboard" />
         <h2>What do you want to focus on today?</h2>
         <b-row>
-            <b-col md="4" class="p-3">
+            <b-col
+                class="p-3"
+                :class="{
+                    'md-6': showFederalCardButton,
+                    'md-4': !showFederalCardButton,
+                }"
+            >
                 <hg-card-button
                     title="BC Vaccine Card"
                     to="/covid19"
@@ -140,7 +150,13 @@ export default class DashboardView extends Vue {
                     </div>
                 </hg-card-button>
             </b-col>
-            <b-col md="4" class="p-3">
+            <b-col
+                class="p-3"
+                :class="{
+                    'md-6': showFederalCardButton,
+                    'md-4': !showFederalCardButton,
+                }"
+            >
                 <hg-card-button
                     title="Health Records"
                     to="/timeline"
@@ -160,7 +176,7 @@ export default class DashboardView extends Vue {
                     </div>
                 </hg-card-button>
             </b-col>
-            <b-col md="4" class="p-3">
+            <b-col v-show="showFederalCardButton" md="4" class="p-3">
                 <hg-card-button
                     title="Proof of Vaccination"
                     data-testid="proof-vaccination-card-btn"
