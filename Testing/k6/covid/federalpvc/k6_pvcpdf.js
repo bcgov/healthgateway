a20f35a2a3443846b1614ec98d77fd6d558306ea
bradhead <brad@zedwerks.com>
2021-10-28 20:16:32 -0700

    updated

--- Testing/k6/covid/federalpvc/k6_pvcpdf.js
+++ Testing/k6/covid/federalpvc/k6_pvcpdf.js
@@ -19,14 +19,14 @@ import { check, sleep, group } from 'k6';
 import { SharedArray } from 'k6/data';
 import PapaParse from "./papaparse.js";
 
-export let testType = __ENV.HG_TYPE  ? __ENV.HG_TYPE : "smoke";
-export let environment = __ENV.HG_ENV  ? __ENV.HG_ENV : "test"; // default to test environment
+export let testType = __ENV.HG_TYPE ? __ENV.HG_TYPE : "smoke";
+export let environment = __ENV.HG_ENV ? __ENV.HG_ENV : "test"; // default to test environment
 export let specialHeaderKey = __ENV.HG_KEY ? __ENV.HG_KEY : "nokey"; // special key 
 
 export let baseSiteUrl = "https://" + environment + ".healthgateway.gov.bc.ca";
 export let cardUrl = baseSiteUrl + "/api/immunizationservice/v1/api/VaccineStatus";
 export let pdfUrl = cardUrl + "/pdf";
-export let entryPageUrl  = baseSiteUrl + "/vaccinecard";
+export let entryPageUrl = baseSiteUrl + "/vaccinecard";
 export let vendorChunkJsUrl = baseSiteUrl + "/js/chunk-vendors.c61f122d.js";
 export let siteChunkJsUrl = baseSiteUrl + "/js/app.8136e1c8.js";
 export let cssUrl = baseSiteUrl + "/css/app.c90e9393.css";
@@ -34,24 +34,50 @@ export let cssVendorsUrl = baseSiteUrl + "/css/chunk-vendors.21f4bba7.css";
 
 console.log(baseSiteUrl);
 
+function isObject(val) {
+    if (val === null) {
+        return false;
+    }
+    return typeof val === "object";
+}
+
+export function checkResponse(resp) {
+    if (isObject(resp)) {
+        check(resp, { "HttpStatusCode is 200": (r) => r.status === 200 });
+        check(resp, { "HttpStatusCode is NOT 3xx Redirection": (r) => !(r.status >= 300 && r.status <= 306) });
+        check(resp, { "HttpStatusCode is NOT 401 Unauthorized": (r) => r.status != 401 });
+        check(resp, { "HttpStatusCode is NOT 402 Payment Required": (r) => r.status != 402 });
+        check(resp, { "HttpStatusCode is NOT 403 Forbidden": (r) => r.status != 403 });
+        check(resp, { "HttpStatusCode is NOT 404 Not Found": (r) => r.status != 404 });
+        check(resp, { "HttpStatusCode is NOT 405 Method Not Allowed": (r) => r.status != 405 });
+        check(resp, { "HttpStatusCode is NOT 406 Not Acceptable": (r) => r.status != 406 });
+        check(resp, { "HttpStatusCode is NOT 407 Proxy Authentication Required": (r) => r.status != 407 });
+        check(resp, { "HttpStatusCode is NOT 408 Request Timeout": (r) => r.status != 408 });
+        check(resp, { "HttpStatusCode is NOT 4xx Client Error": (r) => !(r.status >= 409 && r.status <= 499) });
+        check(resp, { "HttpStatusCode is NOT 5xx Server Error": (r) => !(r.status >= 500 && r.status <= 598) });
+        check(resp, { "HttpStatusCode is NOT 0 (Timeout Error)": (r) => r.status != 0 });
+        return;
+    }
+}
+
 export let rpsRampingOptions = {
     discardResponseBodies: false,
     scenarios: {
         ramping: {
             executor: 'ramping-arrival-rate',
             startRate: 60,
-            timeUnit:'1m',
+            timeUnit: '1m',
             preAllocatedVUs: 100,
             maxVUs: 200,
             stages: [
-                { target: 120, duration: '30s'},
-                { target: 240, duration: '1m'},
-                { target: 480, duration: '2m'},
-                { target: 960, duration: '30s'},
-                { target: 1100, duration: '3m'},
-                { target: 960, duration: '1m'},
-                { target: 240, duration: '1m'},
-                { target: 0, duration: '30s'}
+                { target: 120, duration: '30s' },
+                { target: 240, duration: '1m' },
+                { target: 480, duration: '2m' },
+                { target: 960, duration: '30s' },
+                { target: 1100, duration: '3m' },
+                { target: 960, duration: '1m' },
+                { target: 240, duration: '1m' },
+                { target: 0, duration: '30s' }
             ],
         },
     },
@@ -63,7 +89,7 @@ export let onceOptions = {
         constant: {
             executor: 'constant-arrival-rate',
             rate: 1,
-            timeUnit:'1s',
+            timeUnit: '1s',
             duration: '10s',
             preAllocatedVUs: 10,
             maxVUs: 100
@@ -78,14 +104,14 @@ export let loadOptions = {
         { duration: "1m", target: 150 },
         { duration: "2m", target: 200 },
         { duration: "2m", target: 300 },
-        { duration: "3m", target: 400 }, 
+        { duration: "3m", target: 400 },
         { duration: "30s", target: 100 },
         { duration: "20s", target: 50 },
         { duration: "10s", target: 1 },
     ],
     thresholds: {
         http_req_duration: ['p(95)<29000'], // 95% of requests must complete below 10s
-      },
+    },
 };
 
 export let load2Options = {
@@ -134,8 +160,7 @@ export let options = {
     iterations: 5
 }
 
-switch (testType)
-{
+switch (testType) {
     case 'load':
         options = loadOptions;
         break;
@@ -169,7 +194,7 @@ export let currentUser = 0;
 // not using SharedArray here will mean that the code in the function call (that is what loads and
 // parses the csv) will be executed per each VU which also means that there will be a complete copy
 // per each VU
-const csvData = new SharedArray("another data name", function() {
+const csvData = new SharedArray("another data name", function () {
     // Load CSV file and parse it using Papa Parse
     return PapaParse.parse(open('./data.csv'), { header: true }).data;
 });
@@ -181,109 +206,79 @@ export default function () {
     //console.log('Random user: ', JSON.stringify(randomUser));
 
     let headers = {
-        'User-Agent': 'k6', 
-        'X-API-KEY' : specialHeaderKey,
+        'User-Agent': 'k6',
+        'X-API-KEY': specialHeaderKey,
     }
     let success = false;
 
-    group('get SPA assets', function() {
+    group('get SPA assets', function () {
 
-    let responses = http.batch([
-        ['GET', entryPageUrl, null, { headers: headers, tags: { ctype: 'html' } }],
-        ['GET', cssUrl, null, { headers: headers, tags: { ctype: 'css' } }],
-        ['GET', cssVendorsUrl, null, { headers: headers, tags: { ctype: 'js' } }],
-        ['GET', vendorChunkJsUrl, null, { headers: headers, tags: { ctype: 'js' } }],
-        ['GET', siteChunkJsUrl, null, { headers: headers, tags: { ctype: 'js' } }],
-      ] );
-      check(responses[0], {
-        'Webpage status 200': (res) => res.status === 200,
-      });
-
-    success = check(responses[0], {
-        'Reached VaccineCard Page; Not Queue-IT': (r) => r.status === 200 && r.body.search('queue-it.net') === -1,
-        'VaccineCard Page Title Correct': (r) => r.status === 200 && r.html('title').text() == 'Health Gateway',
-      });
+        let responses = http.batch([
+            ['GET', entryPageUrl, null, { headers: headers, tags: { ctype: 'html' } }],
+            ['GET', cssUrl, null, { headers: headers, tags: { ctype: 'css' } }],
+            ['GET', cssVendorsUrl, null, { headers: headers, tags: { ctype: 'js' } }],
+            ['GET', vendorChunkJsUrl, null, { headers: headers, tags: { ctype: 'js' } }],
+            ['GET', siteChunkJsUrl, null, { headers: headers, tags: { ctype: 'js' } }],
+        ]);
+        check(responses[0], {
+            'Webpage status 200': (res) => res.status === 200,
+        });
+        checkResponse(responses[0]);
+        success = check(responses[0], {
+            'Reached VaccineCard Page; Not Queue-IT': (r) => r.status === 200 && r.body.search('queue-it.net') === -1,
+            'VaccineCard Page Title Correct': (r) => r.status === 200 && r.html('title').text() == 'Health Gateway',
+        });
     });
 
-    group('get vaccinecard with QR', function() {
+    group('get vaccinecard with QR', function () {
 
-    if (success)
-    {
-        sleep(5);  // the min time we think it would take someone to enter their information
+        if (success) {
+            sleep(5);  // the min time we think it would take someone to enter their information
 
-        let params = {
-            headers:  { 'User-Agent': 'k6', 
-            'X-API-KEY' : specialHeaderKey,
-            'phn': randomUser.phn, 
-            'dateOfBirth': randomUser.dateOfBirth, 
-            'dateOfVaccine': randomUser.dateOfVaccine }
-        }
-        let res2 = http.get(cardUrl, params);
+            let params = {
+                headers: {
+                    'User-Agent': 'k6',
+                    'X-API-KEY': specialHeaderKey,
+                    'phn': randomUser.phn,
+                    'dateOfBirth': randomUser.dateOfBirth,
+                    'dateOfVaccine': randomUser.dateOfVaccine
+                }
+            }
+            let res2 = http.get(cardUrl, params);
 
-        check(res2, {"VaccineStatus API Status 200": (r) => r.status === 200});
-        check(res2, {
-            'Reached API Endpoint; Not Queue-IT': (r) => r.status === 200 && r.body.search('queue-it.net') === -1,
-            'API Response Content-Type is JSON': (r) => r.status === 200 && r.headers['Content-Type'].search('application/json') >= 0, 
+            checkResponse(res2);
+            check(res2, {
+                'Reached API Endpoint; Not Queue-IT': (r) => r.status === 200 && r.body.search('queue-it.net') === -1,
+                'API Response Content-Type is JSON': (r) => r.status === 200 && r.headers['Content-Type'].search('application/json') >= 0,
             });
-    }
-    else {
-        console.log("Skipping API Call; in Waiting Room!")
-    }
-    sleep(1);
+        }
+        else {
+            console.log("Skipping API Call; in Waiting Room!")
+        }
+        sleep(1);
     });
 
-    group('Get Fed/Prov Combined PDF', function() {
+    group('Get Fed/Prov Combined PDF', function () {
         let requestParams = {
-            headers:  { 'User-Agent': 'k6', 
-            'X-API-KEY' : specialHeaderKey,
-            'phn': randomUser.phn, 
-            'dateOfBirth': randomUser.dateOfBirth, 
-            'dateOfVaccine': randomUser.dateOfVaccine,
-            'proofTemplate': 'Federal' } 
+            headers: {
+                'User-Agent': 'k6',
+                'X-API-KEY': specialHeaderKey,
+                'phn': randomUser.phn,
+                'dateOfBirth': randomUser.dateOfBirth,
+                'dateOfVaccine': randomUser.dateOfVaccine,
+                'proofTemplate': 'Federal'
+            }
         }
         sleep(1); // the think-time before user chooses to download the pdfUrl.
         let res3 = http.get(pdfUrl, requestParams);
 
-        checkResponses(res3);
-        check(res3, {"VaccineStatus/pdf API Status 200": (r) => r.status === 200});
+        checkResponse(res3);
         check(res3, {
             'Reached VaccineStatus/pdf API Endpoint; Not Queue-IT': (r) => r.status === 200 && r.body.search('queue-it.net') === -1,
             'Response Content-Type is application/json': (r) => r.status === 200 && r.headers['Content-Type'].search('application/json') >= 0,
-            'Response contains the Base64 PDF': (r) => r.status === 200 && r.body.includes("resourcePayload") && r.body.includes("VaccineProof.pdf") && r.body.includes("data")
+            'Response contains the Base64 PDF': (r) => r.status === 200 && r.body.includes("resourcePayload") && r.body.includes("VaccineProof.pdf") && r.body.includes("data"),
+            'Response does not contain actionCode=REFRESH': (r) => r.status === 200 && !(r.body.includes("actionCode") && r.body.includes("REFRESH"))
         });
 
     });
-
-}
-
-export function checkResponse(response) {
-    if (isObject(response)) {
-        var ok =
-            check(response, {
-                "HttpStatusCode is 200": (r) => r.status === 200,
-                "HttpStatusCode is NOT 3xx Redirection": (r) =>
-                    !(r.status >= 300 && r.status <= 306),
-                "HttpStatusCode is NOT 401 Unauthorized": (r) =>
-                    r.status != 401,
-                "HttpStatusCode is NOT 402 Payment Required": (r) =>
-                    r.status != 402,
-                "HttpStatusCode is NOT 403 Forbidden": (r) => r.status != 403,
-                "HttpStatusCode is NOT 404 Not Found": (r) => r.status != 404,
-                "HttpStatusCode is NOT 405 Method Not Allowed": (r) =>
-                    r.status != 405,
-                "HttpStatusCode is NOT 406 Not Acceptable": (r) =>
-                    r.status != 406,
-                "HttpStatusCode is NOT 407 Proxy Authentication Required": (
-                    r
-                ) => r.status != 407,
-                "HttpStatusCode is NOT 408 Request Timeout": (r) =>
-                    r.status != 408,
-                "HttpStatusCode is NOT 4xx Client Error": (r) =>
-                    !(r.status >= 409 && r.status <= 499),
-                "HttpStatusCode is NOT 5xx Server Error": (r) =>
-                    !(r.status >= 500 && r.status <= 598),
-                "HttpStatusCode is NOT 0 (Timeout Error)": (r) => r.status != 0,
-            }) || errorRate.add(1);
-        return;
-    }
 }
