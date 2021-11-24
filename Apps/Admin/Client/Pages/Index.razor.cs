8550145a216bbfba452b78250653f3b4694f7bc1
Stephen Laws <stephen.laws@nomostech.com>
2021-11-24 12:15:06 -0800

    Fix namespace issue in Client for AB#11779

--- Apps/Admin/Client/Pages/Index.razor.cs
+++ Apps/Admin/Client/Pages/Index.razor.cs
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //-------------------------------------------------------------------------
-namespace HealthGateway.HGAdmin.Client.Pages
+namespace HealthGateway.Admin.Client.Pages
 {
     using System.Security.Claims;
     using System.Threading.Tasks;
