c67f7371ee9b80baef989115c44fee71c06d064f
Stephen Laws <51342761+sslaws@users.noreply.github.com>
2021-11-24 14:17:19 -0800

    Remove references to HG and minor cleanup for AB#11846 (#3067)

--- Apps/Admin/Client/Program.cs
+++ Apps/Admin/Client/Program.cs
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //-------------------------------------------------------------------------
-namespace HealthGateway.HGAdmin.Client
+namespace HealthGateway.Admin.Client
 {
     using System;
     using System.Diagnostics.CodeAnalysis;
