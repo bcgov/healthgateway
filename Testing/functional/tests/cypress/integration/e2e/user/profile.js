4dce747933ca5bb13205709db1bbc02931b6e8e2
pagustinqt20 <92187402+pagustinqt20@users.noreply.github.com>
2021-11-26 08:54:35 -0800

    Fix Verify SMS Number AB#11821 (#3065)

--- Testing/functional/tests/cypress/integration/e2e/user/profile.js
+++ Testing/functional/tests/cypress/integration/e2e/user/profile.js
@@ -45,7 +45,104 @@ describe("User Profile Login History", () => {
             });
     });
 });
-describe("User Profile", () => {
+
+describe("User Profile - SMS", () => {
+    before(() => {
+        cy.enableModules("Medication");
+        cy.login(
+            Cypress.env("keycloak.username"),
+            Cypress.env("keycloak.password"),
+            AuthMethod.KeyCloak,
+            "/profile"
+        );
+    });
+
+    it("Verify SMS number", () => {
+        cy.intercept("GET", `**/v1/api/UserProfile/${HDID}`, (req) => {
+            req.reply({
+                fixture: "UserProfileService/userProfile.json",
+            });
+        });
+
+        cy.get("[data-testid=smsStatusNotVerified]").should("be.visible");
+        cy.get("[data-testid=verifySMSBtn]")
+            .should("be.visible")
+            .should("be.enabled")
+            .click();
+        cy.get('[data-testid="countdownText"]').contains(/\d{1,2}s$/); // has 1 or 2 digits before the last 's' character
+
+        cy.get("[data-testid=verifySMSModal] button.close").click();
+        cy.get("[data-testid=verifySMSBtn]")
+            .should("be.visible")
+            .should("be.enabled")
+            .click();
+        cy.get("[data-testid=verifySMSModalCodeInput]").should("be.visible");
+    });
+
+    it("SMS number status Verified", () => {
+        cy.enableModules("Medication");
+        cy.login(
+            Cypress.env("keycloak.username"),
+            Cypress.env("keycloak.password"),
+            AuthMethod.KeyCloak,
+            "/profile"
+        );
+        cy.intercept("GET", `**/v1/api/UserProfile/${HDID}`, (req) => {
+            req.reply({
+                fixture: "UserProfileService/userProfileSMSVerified.json",
+            });
+        });
+
+        cy.get("[data-testid=smsStatusNotVerified]").should("not.exist");
+        cy.get("[data-testid=verifySMSBtn]").should("not.exist");
+        cy.get("[data-testid=smsStatusVerified]").should("be.visible");
+    });
+
+    it("Edit sms number", () => {
+        cy.intercept("PUT", `**/v1/api/UserProfile/${HDID}/sms`, {
+            statusCode: 200,
+            body: true,
+        });
+        cy.get("[data-testid=editSMSBtn]").click();
+        cy.get("[data-testid=smsInvalidNewEqualsOld]").should("be.visible");
+        cy.get("[data-testid=smsNumberInput]").clear().type(fakeSMSNumber);
+        cy.get("[data-testid=saveSMSEditBtn]").click();
+        cy.get('[data-testid="countdownText"]').contains(/\d{1,2}s$/); // has 1 or 2 digits before the last 's' character
+
+        cy.get("[data-testid=verifySMSModal] button.close").click();
+        cy.get("[data-testid=smsStatusNotVerified]").should("be.visible");
+        cy.get("[data-testid=verifySMSBtn]")
+            .should("be.visible")
+            .should("be.enabled");
+    });
+
+    it("Invalid sms number", () => {
+        cy.get("[data-testid=editSMSBtn]").click();
+        cy.get("[data-testid=saveSMSEditBtn]").should("be.disabled");
+        cy.get("[data-testid=smsInvalidNewEqualsOld]").should("be.visible");
+        cy.get("[data-testid=cancelSMSEditBtn]").click();
+        cy.get("[data-testid=smsNumberInput]")
+            .should("be.disabled")
+            .invoke("val")
+            .then((value) =>
+                expect(value.replace(/\D+/g, "")).to.eq(fakeSMSNumber)
+            );
+    });
+
+    it("Clear/OptOut sms number", () => {
+        cy.intercept("PUT", `**/v1/api/UserProfile/${HDID}/sms`, {
+            statusCode: 200,
+            body: true,
+        });
+        cy.get("[data-testid=editSMSBtn]").click();
+        cy.get("[data-testid=smsNumberInput]").clear();
+        cy.get("[data-testid=smsOptOutMessage]").should("be.visible");
+        cy.get("[data-testid=saveSMSEditBtn]").click();
+        cy.get("[data-testid=smsStatusOptedOut]").should("be.visible");
+    });
+});
+
+describe("User Profile - Email Address", () => {
     const emailAddress =
         "healthgateway@mailinator" +
         Math.random().toString().substr(2, 5) +
@@ -100,69 +197,5 @@ describe("User Profile", () => {
         cy.get("[data-testid=emailOptOutMessage]").should("be.visible");
         cy.get("[data-testid=editEmailSaveBtn]").click();
         cy.get("[data-testid=emailStatusOptedOut]").should("be.visible");
-    });
-
-    it("Verify SMS number", () => {
-        cy.get("[data-testid=smsStatusNotVerified]").should("be.visible");
-        cy.get("[data-testid=verifySMSBtn]")
-            .should("be.visible")
-            .should("be.enabled")
-            .click();
-        cy.get('[data-testid="countdownText"]').contains(/\d{1,2}s$/); // has 1 or 2 digits before the last 's' character
-
-        cy.get("[data-testid=verifySMSModal] button.close").click();
-        cy.get("[data-testid=verifySMSBtn]")
-            .should("be.visible")
-            .should("be.enabled")
-            .click();
-        cy.get("[data-testid=verifySMSModalCodeInput]")
-            .should("be.visible")
-            .should("have.focus")
-            .type("123456"); //seed data
-
-        cy.get("[data-testid=smsStatusVerified]").should("be.visible");
-    });
-
-    it("Edit sms number", () => {
-        cy.intercept("PUT", `**/v1/api/UserProfile/${HDID}/sms`, {
-            statusCode: 200,
-            body: true,
-        });
-        cy.get("[data-testid=editSMSBtn]").click();
-        cy.get("[data-testid=smsInvalidNewEqualsOld]").should("be.visible");
-        cy.get("[data-testid=smsNumberInput]").clear().type(fakeSMSNumber);
-        cy.get("[data-testid=saveSMSEditBtn]").click();
-        cy.get('[data-testid="countdownText"]').contains(/\d{1,2}s$/); // has 1 or 2 digits before the last 's' character
-
-        cy.get("[data-testid=verifySMSModal] button.close").click();
-        cy.get("[data-testid=smsStatusNotVerified]").should("be.visible");
-        cy.get("[data-testid=verifySMSBtn]")
-            .should("be.visible")
-            .should("be.enabled");
-    });
-
-    it("Invalid sms number", () => {
-        cy.get("[data-testid=editSMSBtn]").click();
-        cy.get("[data-testid=saveSMSEditBtn]").should("be.disabled");
-        cy.get("[data-testid=smsInvalidNewEqualsOld]").should("be.visible");
-        cy.get("[data-testid=cancelSMSEditBtn]").click();
-        cy.get("[data-testid=smsNumberInput]")
-            .should("be.disabled")
-            .invoke("val")
-            .then((value) =>
-                expect(value.replace(/\D+/g, "")).to.eq(fakeSMSNumber)
-            );
-    });
-
-    it("Clear/OptOut sms number", () => {
-        cy.intercept("PUT", `**/v1/api/UserProfile/${HDID}/sms`, {
-            statusCode: 200,
-            body: true,
-        });
-        cy.get("[data-testid=editSMSBtn]").click();
-        cy.get("[data-testid=smsNumberInput]").clear();
-        cy.get("[data-testid=smsOptOutMessage]").should("be.visible");
-        cy.get("[data-testid=saveSMSEditBtn]").click();
-        cy.get("[data-testid=smsStatusOptedOut]").should("be.visible");
     });
 });
