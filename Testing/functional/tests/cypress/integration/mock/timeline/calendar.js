48481c283464591a0ab3a734d2ac565057dc3b68
pagustinqt20 <92187402+pagustinqt20@users.noreply.github.com>
2021-11-29 14:34:25 -0800

    Rearrange functional tests AB#10996 #10995 #10994 (#3068)
    * Rearrange functional tests part 2 AB#10995
    
    * Refactor Functional Tests task 10994 AB#10995
    
    * Update Functional Test 3 AB#10995
    
    * Functional Tests 3 Tasks 10996 AB#10995
    
    * Functional Tests part 3 AB#10995
    
    * Add functional Tests in mock folder AB#10995

---
+++ Testing/functional/tests/cypress/integration/mock/timeline/calendar.js
@@ -0,0 +1,68 @@
+const { AuthMethod } = require("../../../support/constants");
+
+describe("Calendar View", () => {
+    beforeEach(() => {
+        cy.enableModules("Medication");
+        cy.login(
+            Cypress.env("keycloak.username"),
+            Cypress.env("keycloak.password"),
+            AuthMethod.KeyCloak
+        );
+        cy.checkTimelineHasLoaded();
+    });
+
+    it("Navigate to Month View", () => {
+        cy.get("[data-testid=filterDropdown]").click();
+        cy.get("[data-testid=monthViewToggle]").first().click();
+        cy.get("[data-testid=btnFilterApply]").click();
+
+        cy.get("#currentDate").should("be.visible");
+        cy.location("hash").should("eq", "#calendar");
+    });
+
+    it("Validate Year Selector", () => {
+        cy.get("[data-testid=filterDropdown]").click();
+        cy.get("[data-testid=monthViewToggle]").first().click();
+        cy.get("[data-testid=btnFilterApply]").click();
+
+        cy.get("#currentDate").click();
+        cy.get("#currentDate").click();
+        cy.get(".years-wrapper").children().should("be.visible");
+        cy.get("[data-testid=yearBtn2014]").click();
+        cy.get("[data-testid=monthBtnDec]").should("not.be.enabled");
+        cy.get("[data-testid=monthBtnFeb]").click();
+        cy.get("#currentDate").should("have.text", " February 2014 ");
+        cy.get("[data-testid=monthBtnFeb]").should("have.class", "selected");
+
+        cy.get("#currentDate").click();
+        cy.get("[data-testid=monthBtnMar]").click();
+        cy.get("#currentDate").should("have.text", " March 2014 ");
+        cy.get("[data-testid=monthBtnMar]").should("have.class", "selected");
+        cy.get("[data-testid=monthBtnFeb]").should(
+            "not.have.class",
+            "selected"
+        );
+    });
+
+    it("Click on Calendar item to Linear Timeline", () => {
+        cy.get("[data-testid=filterDropdown]").click();
+        cy.get("[data-testid=monthViewToggle]").first().click();
+        cy.get("[data-testid=btnFilterApply]").click();
+
+        cy.location("hash").should("eq", "#calendar");
+        cy.get("#currentDate").click();
+        cy.get("#currentDate").click();
+        cy.get(".years-wrapper").children().should("be.visible");
+        cy.get("[data-testid=yearBtn2019]").click();
+        cy.get("[data-testid=monthBtnApr]").click();
+        cy.get("#currentDate").should("have.text", " April 2019 ");
+        cy.get("[data-testid=event-monthday-9]").click();
+        cy.location("hash").should("eq", "#linear");
+        cy.get("[data-testid=entryCardDetailsTitle]")
+            .first()
+            .should("have.text", "Methadone (Maintenance) 1mg/Ml");
+        cy.contains("[data-testid=entryCardDate]", "2019-May-01").should(
+            "be.visible"
+        );
+    });
+});
