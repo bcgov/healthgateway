536ca1f21f4e489ccf2ab82a896f012eda22f043
Mike Lyttle <mikelyttle@gmail.com>
2021-12-20 16:34:54 -0800

    Refactor functional tests for vaccine cards and COVID-19 tests AB#11871

---
+++ Testing/functional/tests/cypress/integration/e2e/covid19/federalProofOfVaccination.js
@@ -0,0 +1,28 @@
+import { deleteDownloadsFolder } from "../../../support/utils";
+const { AuthMethod } = require("../../../support/constants");
+const homeUrl = "/home";
+
+describe("Federal Proof of Vaccination", () => {
+    it("Save Federal Proof of Vaccination with Retry", () => {
+        deleteDownloadsFolder();
+
+        cy.enableModules(["Immunization", "FederalCardButton"]);
+
+        cy.login(
+            Cypress.env("keycloak.username"),
+            Cypress.env("keycloak.password"),
+            AuthMethod.KeyCloak,
+            homeUrl
+        );
+
+        cy.get("[data-testid=proof-vaccination-card-btn]")
+            .should("be.visible", "be.enabled")
+            .click();
+
+        cy.get("[data-testid=genericMessageModal]").should("be.visible");
+        cy.get("[data-testid=genericMessageSubmitBtn]").click();
+        cy.get("[data-testid=loadingSpinner]").should("be.visible");
+        cy.wait(1000);
+        cy.verifyDownload("VaccineProof.pdf");
+    });
+});
