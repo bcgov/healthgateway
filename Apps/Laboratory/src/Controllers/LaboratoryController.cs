bdcb1bbe24a282aff8e3102f3eb0f2e02d23b24a
pagustinqt20 <princess.agustin@quartech.com>
2022-01-20 16:09:56 -0800

    Revert "Add Route for Covid19Orders AB#12210"
    This reverts commit 606dbbd12692357a4716149ccd1549a4b81e5704.

--- Apps/Laboratory/src/Controllers/LaboratoryController.cs
+++ Apps/Laboratory/src/Controllers/LaboratoryController.cs
@@ -67,9 +67,8 @@ namespace HealthGateway.Laboratory.Controllers
         /// <response code="401">The client must authenticate itself to get the requested response.</response>
         /// <response code="403">The client does not have access rights to the content; that is, it is unauthorized, so the server is refusing to give the requested resource. Unlike 401, the client's identity is known to the server.</response>
         /// <response code="503">The service is unavailable for use.</response>
-        [HttpGet]
+        [HttpGet("Covid19")]
         [Produces("application/json")]
-        [Route("Covid19Orders")]
         [Authorize(Policy = LaboratoryPolicy.Read)]
         public async Task<RequestResult<IEnumerable<PhsaCovid19Order>>> GetCovid19Orders([FromQuery] string hdid)
         {
