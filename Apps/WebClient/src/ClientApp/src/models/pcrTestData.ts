b51566c77c538c8528cc60a27de08b4a550b2f3d
Sam Warren <samwarren4@gmail.com>
2022-01-27 01:41:42 -0800

    add PCRTestData interface

---
+++ Apps/WebClient/src/ClientApp/src/models/pcrTestData.ts
@@ -0,0 +1,14 @@
+export default interface PCRTestData {
+    firstName?: string;
+    lastName?: string;
+    phn?: string;
+    dob?: string;
+    contactPhoneNumber?: string;
+    streetAddress?: string;
+    city?: string;
+    postalCode?: string;
+    dateOfTest?: string;
+    timeOfTest?: string;
+    hdid?: string;
+    testKitCid: string;
+}
