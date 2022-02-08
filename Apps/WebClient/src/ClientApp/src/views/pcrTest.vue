8e63f3fbe52ff9d3ddfff9f1405742d6a6ad894f
BrianMaki <brian.jang.developer@outlook.com>
2022-02-08 11:49:54 -0800

    Button name change for AB#12381.

--- Apps/WebClient/src/ClientApp/src/views/pcrTest.vue
+++ Apps/WebClient/src/ClientApp/src/views/pcrTest.vue
@@ -1074,13 +1074,13 @@ export default class PcrTestView extends Vue {
                             </b-col>
                             <b-col cols="8">
                                 <hg-button
-                                    id="btn-submit"
+                                    id="btn-register-kit"
                                     block
                                     type="submit"
                                     variant="primary"
-                                    data-testid="btn-submit"
+                                    data-testid="btn-register-kit"
                                 >
-                                    Submit
+                                    Register Kit
                                 </hg-button>
                             </b-col>
                         </b-row>
