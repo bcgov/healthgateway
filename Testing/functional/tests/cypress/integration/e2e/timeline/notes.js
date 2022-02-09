4d7157b73f3f432d3e9c3abed1a488239f7a000d
BrianMaki <brian.jang.developer@outlook.com>
2022-02-09 00:36:43 -0800

    Re-ordered confirmation of add, edit and delete note for AB#12323.

--- Testing/functional/tests/cypress/integration/e2e/timeline/notes.js
+++ Testing/functional/tests/cypress/integration/e2e/timeline/notes.js
@@ -22,12 +22,6 @@ describe("Notes", () => {
             .type("1950-Jan-01");
         cy.get("[data-testid=noteTextInput]").type("Test");
         cy.get("[data-testid=saveNoteBtn]").click();
-        cy.get("[data-testid=entryCardDate]")
-            .last()
-            .should("have.text", "1950-Jan-01");
-        cy.get("[data-testid=noteTitle]")
-            .last()
-            .should("have.text", "Note Title!");
 
         // Edit Note
         cy.log("Editing Note.");
@@ -35,9 +29,14 @@ describe("Notes", () => {
         cy.get("[data-testid=editNoteMenuBtn]").first().click();
         cy.get("[data-testid=noteTitleInput]").clear().type("Test Edit");
         cy.get("[data-testid=saveNoteBtn]").click();
+
+        // Confirm added note - notes are sorted by date in descending order
+        cy.get("[data-testid=entryCardDate]")
+            .last()
+            .should("have.text", "1950-Jan-01");
         cy.get("[data-testid=noteTitle]")
-            .first()
-            .should("have.text", "Test Edit");
+            .last()
+            .should("have.text", "Note Title!");
 
         // Delete Note
         cy.log("Deleting Note.");
@@ -46,6 +45,13 @@ describe("Notes", () => {
             expect(str).to.eq("Are you sure you want to delete this note?");
         });
         cy.get("[data-testid=deleteNoteMenuBtn]").last().click();
+
+        // Confirm edited note
+        cy.get("[data-testid=noteTitle]")
+            .first()
+            .should("have.text", "Test Edit");
+
+        // Confirm deleted note
         cy.get("[data-testid=entryCardDetailsTitle]")
             .last()
             .contains("Note Title!")
