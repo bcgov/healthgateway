697cbbec1f79f606bed09926eb8d89e4aaf7d671
pagustinqt20 <92187402+pagustinqt20@users.noreply.github.com>
2022-02-11 16:40:39 -0800

    Add model and service in front end AB#12435 (#3319)

---
+++ Apps/AdminWebClient/src/ClientApp/src/models/covidTherapyAssessmentRequest.ts
@@ -0,0 +1,29 @@
+import { CovidTherapyAssessmentOption } from "@/constants/covidTherapyAssessmentOption";
+
+import { StringISODateTime } from "./dateWrapper";
+
+export default interface CovidTherapyAssessmentRequest {
+    phn?: string;
+    firstName?: string;
+    lastName?: string;
+    phoneNumber?: string;
+    identifiesIndigenous: CovidTherapyAssessmentOption;
+    hasAFamilyDoctorOrNp: CovidTherapyAssessmentOption;
+    confirmsOver12: boolean;
+    testedPositiveInPast7Days: CovidTherapyAssessmentOption;
+    hasSevereCovid19Symptoms: CovidTherapyAssessmentOption;
+    hasMildOrModerateCovid19Symptoms: boolean;
+    symptomOnSetDate: StringISODateTime;
+    hasImmunityCompromisingMedicalConditionAntiViralTri: CovidTherapyAssessmentOption;
+    reports3DosesC19Vaccine: CovidTherapyAssessmentOption;
+    agentComments?: string;
+    streetAddress?: string;
+    provOrState?: string;
+    postalCode?: string;
+    country?: string;
+    changeAddressFlag: boolean;
+    positiveCovidLabData?: string;
+    covidVaccinationHistory?: string;
+    cevGroupDetails?: string;
+    submitted?: StringISODateTime;
+}
