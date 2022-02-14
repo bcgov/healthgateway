b14845db7dc0aa3d955972ea82857f0940354607
pagustinqt20 <92187402+pagustinqt20@users.noreply.github.com>
2022-02-14 15:59:05 -0800

    UI Assessment Form  AB#12437 (#3323)
    * Covid Theray Assessment Form AB#12437
    
    * Fix bind-value issue AB#12437
    
    * Add validation and Phone number mask AB#12437
    
    * Add radio button validation AB#12437
    
    * Check validation pass during submit AB#12437
    
    * Add route call AB#12437
    
    * Rename component AB#12437
    
    * Removed the route and some enhancement AB#12437

--- Apps/AdminWebClient/src/ClientApp/src/models/CovidTreatmentAssessmentDetails.ts
+++ Apps/AdminWebClient/src/ClientApp/src/models/CovidTreatmentAssessmentDetails.ts
@@ -1,6 +1,6 @@
 import PreviousAssessmentDetailsList from "./previousAssessmentDetailsList";
 
-export default interface CovidTherapyAssessmentDetails {
+export default interface CovidTreatmentAssessmentDetails {
     hasKnownPositiveC19Past7Days: boolean;
     citizenIsConsideredImmunoCompromised: boolean;
     has3DoseMoreThan14Days: boolean;
