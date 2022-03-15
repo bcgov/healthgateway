f6dbe5bd17febd3938442b07aa6658c8e4e24eeb
pagustinqt20 <92187402+pagustinqt20@users.noreply.github.com>
2022-03-15 18:34:05 +0000

    Blazor System Analytics AB#12683 (#3449) (#3450)
    * Add new blazor page for Stats Admin AB#12683
    
    * Create one generic state AB#12683
    
    * Fixed build warnings AB#12683
    
    * Change Page file name and disabled button AB#12683
    
    * Added KeycloakAdmin configs AB#12683
    
    * Refactor based on PR  review feedback AB#12683

---
+++ Apps/Admin/Client/Services/IAnalyticsApi.cs
@@ -0,0 +1,72 @@
+//-------------------------------------------------------------------------
+// Copyright © 2019 Province of British Columbia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-------------------------------------------------------------------------
+namespace HealthGateway.Admin.Client.Services;
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Refit;
+
+/// <summary>
+/// API to fetch export data from the server.
+/// </summary>
+public interface IAnalyticsApi
+{
+    /// <summary>
+    /// Retrieves a list of User Profiles created inclusively between UTC dates if provided.
+    /// </summary>
+    /// <returns>HttpResponseMessage.</returns>
+    /// <param name="startDate">The optional start date for the data.</param>
+    /// <param name="endDate">The optional end date for the data.</param>
+    [Get("/GetUserProfiles")]
+    Task<HttpResponseMessage> GetUserProfiles(DateTime? startDate = null, DateTime? endDate = null);
+
+    /// <summary>
+    /// Retrieves a list of Comments inclusively between UTC dates if provided.
+    /// </summary>
+    /// <returns>HttpResponseMessage.</returns>
+    /// <param name="startDate">The optional start date for the data.</param>
+    /// <param name="endDate">The optional end date for the data.</param>
+    [Get("/GetComments")]
+    Task<HttpResponseMessage> GetComments(DateTime? startDate = null, DateTime? endDate = null);
+
+    /// <summary>
+    /// Retrieves a list of Notes inclusively between UTC dates if provided.
+    /// </summary>
+    /// <returns>HttpResponseMessage.</returns>
+    /// <param name="startDate">The optional start date for the data.</param>
+    /// <param name="endDate">The optional end date for the data.</param>
+    [Get("/GetNotes")]
+    Task<HttpResponseMessage> GetNotes(DateTime? startDate = null, DateTime? endDate = null);
+
+    /// <summary>
+    /// Retrieves a list of Ratings inclusively between UTC dates if provided.
+    /// </summary>
+    /// <returns>HttpResponseMessage.</returns>
+    /// <param name="startDate">The optional start date for the data.</param>
+    /// <param name="endDate">The optional end date for the data.</param>
+    [Get("/GetRatings")]
+    Task<HttpResponseMessage> GetRatings(DateTime? startDate = null, DateTime? endDate = null);
+
+    /// <summary>
+    /// Retrieves a list of inactive users created exclusive of the days inactive.
+    /// </summary>
+    /// <returns>HttpResponseMessage.</returns>
+    /// <param name="inactiveDays">The days inactive to filter the users last login.</param>
+    /// <param name="timeOffset">The offset from the client browser to UTC.</param>
+    [Get("/GetInactiveUsers?inactiveDays={inactiveDays}&timeOffset={timeOffset}")]
+    Task<HttpResponseMessage> GetInactiveUsers(int inactiveDays, int timeOffset);
+}
