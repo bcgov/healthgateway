fb6f6fc0398ed99ddfb9c223a14aef6b181f086e
ninosamson <33761012+ninosamson@users.noreply.github.com>
2022-03-18 10:09:37 -0700

    add question to vue

--- Apps/AdminWebClient/src/ClientApp/src/components/covidTreatmentAssessment/CovidTreatmentAssessment.vue
+++ Apps/AdminWebClient/src/ClientApp/src/components/covidTreatmentAssessment/CovidTreatmentAssessment.vue
@@ -515,6 +515,24 @@ export default class CovidTreatmentAssessmentComponent extends Vue {
                                 :has-not-sure-option="true"
                             />
                         </Card>
+                        <Card
+                            title="10. Do you agree to the information being added to your CareConnect electronic 
+                            health record as part of the process to obtain COVID-19 treatment?*"
+                        >
+                            <ValidationProvider
+                                v-slot="{ errors }"
+                                rules="oneOf:Yes,No"
+                            >
+                                <OptionDetails
+                                    :value.sync="
+                                        covidTreatmentAssessmentRequest.consentToSendCC
+                                    "
+                                />
+                                <div class="error-message">
+                                    {{ errors[0] }}
+                                </div>
+                            </ValidationProvider>
+                        </Card>
                         <Card title="Notes">
                             <div class="pt-2">
                                 <v-textarea
