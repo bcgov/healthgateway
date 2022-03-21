02b9c0f3a5b13b3fbf912e1225441ed53f4da4bc
pagustinqt20 <princess.agustin@quartech.com>
2022-03-21 07:04:42 -0700

    Change the return property to IList AB#12669

--- Apps/Admin/Client/Components/HgRatingDetails.razor.cs
+++ Apps/Admin/Client/Components/HgRatingDetails.razor.cs
@@ -86,8 +86,6 @@ public partial class HgRatingDetails : HgComponentBase
     /// </summary>
     [Parameter]
 #pragma warning disable CA2227 // Collection properties should be read only
-#pragma warning disable CA1002 // Do not expose generic lists
-    public List<Tuple<string, int, int>>? RatingSummary { get; set; }
-#pragma warning restore CA1002 // Do not expose generic lists
+    public IList<Tuple<string, int, int>>? RatingSummary { get; set; }
 #pragma warning restore CA2227 // Collection properties should be read only
 }
