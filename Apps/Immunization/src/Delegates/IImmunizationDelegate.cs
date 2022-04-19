3361c4732e63476536ce926d70397512ece77ce0
Brian Jang <58790456+BrianMaki@users.noreply.github.com>
2022-04-19 10:33:23 -0700

    Refactor Immunization Delegate to use ReFit for AB#12853 and AB#12857 (#3579)
    * Refactor immunization delegate to use ReFit for AB#12853.
    
    * Removed no longer used GetVaccineHistory delegate call for AB#12853.
    
    * Refactor unit tests to use ReFit for AB#12857.

--- Apps/Immunization/src/Delegates/IImmunizationDelegate.cs
+++ Apps/Immunization/src/Delegates/IImmunizationDelegate.cs
@@ -39,13 +39,5 @@ namespace HealthGateway.Immunization.Delegates
         /// <param name="pageIndex">The page index to return.</param>
         /// <returns>The PhsaResult including the load state and the list of Immunizations available for the user identified by the bearerToken.</returns>
         Task<RequestResult<PhsaResult<ImmunizationResponse>>> GetImmunizations(int pageIndex = 0);
-
-        /// <summary>
-        /// Gets the immunization card for the supplied HDID and Immunization Disease.
-        /// </summary>
-        /// <param name="hdid">The hdid to query.</param>
-        /// <param name="immunizationDisease">The associated disease to query.</param>
-        /// <returns>A Immunization Card.</returns>
-        Task<RequestResult<PhsaResult<ImmunizationCard>>> GetVaccineHistory(string hdid, string immunizationDisease);
     }
 }
