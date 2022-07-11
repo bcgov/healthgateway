2721618c8c0dab2b28e7e8c4af636e8485c98f46
rayking29 <ray.king@quartech.com>
2022-07-11 14:17:40 -0700

    Added tooManyRequestsWarning and tooManyRequestsError to errors store for AB#13498

--- Apps/WebClient/src/ClientApp/src/store/modules/error/getters.ts
+++ Apps/WebClient/src/ClientApp/src/store/modules/error/getters.ts
@@ -4,9 +4,15 @@ import { ErrorBannerGetters, ErrorBannerState } from "./types";
 
 export const getters: ErrorBannerGetters = {
     isShowing(state: ErrorBannerState): boolean {
-        return state.isShowing;
+        return state.genericErrorBanner.isShowing;
     },
     errors(state: ErrorBannerState): BannerError[] {
-        return state.errors;
+        return state.genericErrorBanner.errors;
+    },
+    tooManyRequestsWarning(state: ErrorBannerState): boolean {
+        return state.tooManyRequestsWarning;
+    },
+    tooManyRequestsError(state: ErrorBannerState): string | undefined {
+        return state.tooManyRequestsError;
     },
 };
