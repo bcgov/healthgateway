9da23ab01d524a2b6125035f240fc12bd7d3873d
Brad Head <38706615+bradhead@users.noreply.github.com>
2022-08-08 09:49:18 -0700

    removed older perf tests on services

--- Apps/Immunization/test/performance/k6_run.sh
+++
@@ -1,5 +0,0 @@
-##
-## How to run this on command-line:
-## export HG_USER_PASSWORD=password ; ./k6_run.sh
-##
-docker run -a STDOUT -a STDERR -i grafana/k6 run -e USER_PASSWORD=$HG_USER_PASSWORD -d 120s --vus 10 --http-debug -  <immunization_load_k6.js
