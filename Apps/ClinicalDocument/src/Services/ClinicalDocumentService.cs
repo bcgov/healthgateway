63f3776e49824ce478555bb4c4822c883ed66a00
rayking29 <ray.king@quartech.com>
2022-09-09 14:05:40 -0700

    Create clinical document service and refit API for AB#13792

--- Apps/ClinicalDocument/src/Services/ClinicalDocumentService.cs
+++ Apps/ClinicalDocument/src/Services/ClinicalDocumentService.cs
@@ -17,29 +17,39 @@ namespace HealthGateway.ClinicalDocument.Services
 {
     using System.Collections.Generic;
     using System.Diagnostics;
+    using System.Linq;
     using System.Threading.Tasks;
+    using HealthGateway.ClinicalDocument.Api;
     using HealthGateway.ClinicalDocument.Models;
     using HealthGateway.Common.Data.Constants;
     using HealthGateway.Common.Data.ViewModels;
+    using HealthGateway.Common.ErrorHandling;
     using HealthGateway.Common.Models.PHSA;
     using HealthGateway.Common.Services;
     using Microsoft.Extensions.Logging;
+    using Refit;
 
     /// <inheritdoc/>
     public class ClinicalDocumentService : IClinicalDocumentService
     {
         private readonly ILogger<ClinicalDocumentService> logger;
         private readonly IPersonalAccountsService personalAccountsService;
+        private readonly IClinicalDocumentsApi clinicalDocumentsApi;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ClinicalDocumentService"/> class.
         /// </summary>
         /// <param name="logger">Injected logger.</param>
         /// <param name="personalAccountsService">The injected personal accounts service.</param>
-        public ClinicalDocumentService(ILogger<ClinicalDocumentService> logger, IPersonalAccountsService personalAccountsService)
+        /// <param name="clinicalDocumentsApi">The injected clinical documents api.</param>
+        public ClinicalDocumentService(
+            ILogger<ClinicalDocumentService> logger,
+            IPersonalAccountsService personalAccountsService,
+            IClinicalDocumentsApi clinicalDocumentsApi)
         {
             this.logger = logger;
             this.personalAccountsService = personalAccountsService;
+            this.clinicalDocumentsApi = clinicalDocumentsApi;
         }
 
         private static ActivitySource Source { get; } = new(nameof(ClinicalDocumentService));
@@ -57,6 +67,23 @@ namespace HealthGateway.ClinicalDocument.Services
             if (response.ResultStatus == ResultType.Success)
             {
                 this.logger.LogDebug("PID Fetched: {Pid}", response.ResourcePayload?.PatientIdentity?.Pid);
+                IApiResponse<IEnumerable<ClinicalDocumentRecord>> apiResponse =
+                    await this.clinicalDocumentsApi.GetClinicalDocumentRecordsByPid(response.ResourcePayload?.PatientIdentity?.Pid.ToString()).ConfigureAwait(true);
+                if (apiResponse.IsSuccessStatusCode)
+                {
+                    requestResult.ResultStatus = ResultType.Success;
+                    requestResult.ResourcePayload = apiResponse.Content;
+                    requestResult.TotalResultCount = apiResponse.Content.Count();
+                }
+                else
+                {
+                    this.logger.LogCritical("API Exception {Error}", apiResponse.Error?.ToString());
+                    requestResult.ResultError = new()
+                    {
+                        ResultMessage = $"API Exception {apiResponse.Error}",
+                        ErrorCode = ErrorTranslator.ServiceError(ErrorType.CommunicationExternal, ServiceType.PHSA),
+                    };
+                }
             }
             else
             {
