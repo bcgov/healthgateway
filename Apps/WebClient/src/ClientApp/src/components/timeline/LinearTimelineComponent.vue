a8ca3fcec6b9c40fe127d68e2d65237b93878e04
Brian Jang <58790456+BrianMaki@users.noreply.github.com>
2022-09-27 09:34:14 -0700

    Add disclaimer to timeline filter for AB #13975 (#4181)
    * Add disclaimer to timeline filter for AB #13975

--- Apps/WebClient/src/ClientApp/src/components/timeline/LinearTimelineComponent.vue
+++ Apps/WebClient/src/ClientApp/src/components/timeline/LinearTimelineComponent.vue
@@ -171,6 +171,10 @@ export default class LinearTimelineComponent extends Vue {
         return filterLoading;
     }
 
+    private get isImmunization(): boolean {
+        return this.isFilterApplied(EntryType.Immunization);
+    }
+
     private get numberOfPages(): number {
         let pageCount = 1;
         if (this.timelineEntries.length > this.pageSize) {
@@ -322,6 +326,24 @@ export default class LinearTimelineComponent extends Vue {
                 {{ timelineEntryCount }} records
             </b-col>
         </b-row>
+        <div id="linear-timeline-immunization-disclaimer" class="containter">
+            <b-alert
+                :show="isImmunization"
+                variant="info"
+                class="mt-0 mb-1"
+                data-testid="linear-timeline-immunization-disclaimer-alert"
+            >
+                <span>
+                    You can add or update immunizations by visiting
+                    <a
+                        href="https://www.immunizationrecord.gov.bc.ca"
+                        target="_blank"
+                        rel="noopener"
+                        >immunizationrecord.gov.bc.ca</a
+                    >.
+                </span>
+            </b-alert>
+        </div>
         <div id="timeData" data-testid="linearTimelineData">
             <div
                 v-for="dateGroup in dateGroups"
