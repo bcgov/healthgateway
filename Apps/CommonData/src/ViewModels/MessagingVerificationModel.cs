b2c2fa9fbbf6d0fd5cde20e888cdef18009b2815
BrianMaki <brian.jang.developer@outlook.com>
2022-10-12 14:53:07 -0700

    Fixed unused using statement for admin server code for AB#13978

--- Apps/CommonData/src/ViewModels/MessagingVerificationModel.cs
+++ Apps/CommonData/src/ViewModels/MessagingVerificationModel.cs
@@ -18,7 +18,6 @@ namespace HealthGateway.Common.Data.ViewModels
     using System;
     using System.Diagnostics.CodeAnalysis;
     using HealthGateway.Common.Data.Constants;
-    using HealthGateway.Common.Data.Models;
 
 #pragma warning disable CS1591 // self explanatory simple model
 #pragma warning disable SA1600 // self explanatory simple model
