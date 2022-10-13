40c5722e5eddeaa3a77501a3a121ab788f7dc011
Mike Lyttle <mikelyttle@gmail.com>
2022-10-12 19:02:01 -0700

    Update and port error utilities to AdminWebClient

--- Apps/AdminWebClient/src/ClientApp/src/models/errors.ts
+++ Apps/AdminWebClient/src/ClientApp/src/models/errors.ts
@@ -1,32 +1,8 @@
 import { ActionType } from "@/constants/actionType";
 
-export interface BannerError {
-    title: string;
-    traceId?: string;
-    source: string;
-}
-
-export interface CustomBannerError {
-    title: string;
-    description: string;
-    detail?: string;
-}
-
 export interface HttpError {
     message: string;
     statusCode?: number;
-}
-
-export class PageError {
-    public code: string;
-    public name: string;
-    public message: string;
-
-    constructor(code: string, name: string, message: string) {
-        this.code = code;
-        this.name = name;
-        this.message = message;
-    }
 }
 
 export interface ResultError {
