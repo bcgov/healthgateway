757467b5081a336baee6af2d3586921116c16e7d
rayking29 <ray.king@quartech.com>
2022-10-26 10:42:37 -0700

    Added functional tests for hospital visits for AB#14171

--- Testing/functional/tests/cypress/integration/e2e/timeline/encounter.js
+++ Testing/functional/tests/cypress/integration/e2e/timeline/encounter.js
@@ -11,10 +11,33 @@ describe("MSP Visits", () => {
         cy.checkTimelineHasLoaded();
     });
 
-    it("Validate Card Details", () => {
+    it("Validate Encounter Card Details", () => {
         cy.get("[data-testid=encounterTitle]").should("be.visible");
         cy.get("[data-testid=entryCardDetailsTitle").first().click();
         cy.get("[data-testid=encounterClinicLabel").should("be.visible");
         cy.get("[data-testid=encounterClinicName").should("be.visible");
+    });
+});
+
+describe("Hospital Visits", () => {
+    beforeEach(() => {
+        cy.enableModules("HospitalVisit");
+        cy.login(
+            Cypress.env("keycloak.username"),
+            Cypress.env("keycloak.password"),
+            AuthMethod.KeyCloak
+        );
+        cy.checkTimelineHasLoaded();
+    });
+
+    it("Validate Hospital Visit Card Details", () => {
+        cy.get("[data-testid=hospitalvisitTitle]").should("be.visible");
+        cy.get("[data-testid=entryCardDetailsTitle").first().click();
+        cy.get("[data-testid=hospital-visit-location").should("be.visible");
+        cy.get("[data-testid=hospital-visit-provider").should("be.visible");
+        cy.get("[data-testid=hospital-visit-date").should("be.visible");
+        cy.get("[data-testid=hospital-visit-discharge-date").should(
+            "be.visible"
+        );
     });
 });
