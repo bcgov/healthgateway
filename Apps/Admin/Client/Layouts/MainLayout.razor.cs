2d51eb33d28fb140c05f73d5a1c5e51b32c6472b
Stephen Laws <stephen.laws@nomostech.com>
2022-12-05 13:00:21 -0800

    PR Fixes for AB#14344

--- Apps/Admin/Client/Layouts/MainLayout.razor.cs
+++ Apps/Admin/Client/Layouts/MainLayout.razor.cs
@@ -13,7 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //-------------------------------------------------------------------------
-#pragma warning disable CS0618
 namespace HealthGateway.Admin.Client.Layouts
 {
     using System;
