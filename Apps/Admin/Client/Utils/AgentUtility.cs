6f4f9748b502bd3b67bc58ca15a1fa163076e318
Mike Lyttle <mikelyttle@gmail.com>
2023-01-09 17:49:27 -0800

    Create page and table for agent provisioning AB#14635

---
+++ Apps/Admin/Client/Utils/AgentUtility.cs
@@ -0,0 +1,25 @@
+﻿namespace HealthGateway.Admin.Client.Utils
+{
+    using HealthGateway.Admin.Common.Constants;
+
+    /// <summary>
+    /// Utilities for interacting with admin agents.
+    /// </summary>
+    public static class AgentUtility
+    {
+        /// <summary>
+        /// Returns the formatted representation of an agent's identity provider.
+        /// </summary>
+        /// <param name="identityProvider">The agent's identity provider.</param>
+        /// <returns>A string containing the formatted representation of a agent's identity provider.</returns>
+        public static string FormatKeycloakIdentityProvider(KeycloakIdentityProvider identityProvider)
+        {
+            return identityProvider switch
+            {
+                KeycloakIdentityProvider.Idir => "IDIR",
+                KeycloakIdentityProvider.PhsaAzure => "PHSA",
+                _ => identityProvider.ToString(),
+            };
+        }
+    }
+}
