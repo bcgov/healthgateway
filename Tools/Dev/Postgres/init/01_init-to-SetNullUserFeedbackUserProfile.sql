35e1ee66e7f61cfe6012113c5bfad125f196e867
Stephen Laws <stephen.laws@nomostech.com>
2023-01-11 15:52:54 -0800

    Add missing connection and role settings for AB#14693

--- Tools/Dev/Postgres/init/01_init-to-SetNullUserFeedbackUserProfile.sql
+++ Tools/Dev/Postgres/init/01_init-to-SetNullUserFeedbackUserProfile.sql
@@ -1,4 +1,7 @@
-﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+﻿\c hglocal
+SET ROLE hglocal;
+
+CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
     "MigrationId" character varying(150) NOT NULL,
     "ProductVersion" character varying(32) NOT NULL,
     CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY ("MigrationId")
