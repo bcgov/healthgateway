9a8ec91fafebeaa2f22266f89711beb0317c402b
BrianMaki <brian.jang.developer@outlook.com>
2023-01-12 13:19:54 -0800

    Update namespace for admin unit tests for AB#14521.

--- Apps/Admin/Tests/Converters/DateOutputConverterTests.cs
+++ Apps/Admin/Tests/Converters/DateOutputConverterTests.cs
@@ -13,7 +13,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 // -------------------------------------------------------------------------
-namespace HealthGateway.Admin.Client.Converters
+namespace HealthGateway.Admin.Tests.Converters
 {
     using System;
     using HealthGateway.Admin.Server.Converters;
