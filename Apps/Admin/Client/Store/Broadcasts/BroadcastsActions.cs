f3f6c8c8e26610b6a86401ee1a9be96d90f4e07c
Mike Lyttle <mikelyttle@gmail.com>
2023-01-17 13:33:57 -0800

    Fix broadcast dialog error alerts persisting after reopening dialog

--- Apps/Admin/Client/Store/Broadcasts/BroadcastsActions.cs
+++ Apps/Admin/Client/Store/Broadcasts/BroadcastsActions.cs
@@ -221,6 +221,20 @@ public static class BroadcastsActions
     }
 
     /// <summary>
+    /// The action that clears any error encountered during an add.
+    /// </summary>
+    public class ClearAddErrorAction
+    {
+    }
+
+    /// <summary>
+    /// The action that clears any error encountered during an update.
+    /// </summary>
+    public class ClearUpdateErrorAction
+    {
+    }
+
+    /// <summary>
     /// The action that clears the state.
     /// </summary>
     public class ResetStateAction
