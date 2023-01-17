3a9b6e3313cf3b18b15af9a7d20a09e115581615
Brian Jang <58790456+BrianMaki@users.noreply.github.com>
2023-01-17 10:16:52 -0800

    Fix Blazor Admin logout/login for AB#14712 and obsolete SignOutSessionStateManager for AB#14344 (#4636)
    * Fix logout and login issue for AB#14712.
    
    * Remove obsolete SignOutSessionStateManager for AB#14344.

--- Apps/Admin/Client/Pages/AuthenticationPage.razor.cs
+++ Apps/Admin/Client/Pages/AuthenticationPage.razor.cs
@@ -16,7 +16,9 @@
 
 namespace HealthGateway.Admin.Client.Pages;
 
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.Extensions.Options;
 
@@ -32,10 +34,24 @@ public partial class AuthenticationPage : ComponentBase
     public string? Action { get; set; }
 
     [Inject]
+    private AuthenticationStateProvider AuthenticationStateProvider { get; set; } = default!;
+
+    [Inject]
     private NavigationManager NavigationManager { get; set; } = default!;
 
     [Inject]
     private IOptionsSnapshot<RemoteAuthenticationOptions<ApiAuthorizationProviderOptions>> OptionsSnapshot { get; set; } = default!;
+
+    /// <inheritdoc/>
+    protected override async Task OnParametersSetAsync()
+    {
+        AuthenticationState authState = await this.AuthenticationStateProvider.GetAuthenticationStateAsync().ConfigureAwait(true);
+        if (this.Action == "logout-callback" && authState.User.Identity is { IsAuthenticated: true })
+        {
+            string loginPath = this.OptionsSnapshot.Get(Options.DefaultName).AuthenticationPaths.LogInPath;
+            this.NavigationManager.NavigateTo(loginPath ?? "/", replace: true);
+        }
+    }
 
     private void RedirectToLoginPage()
     {
