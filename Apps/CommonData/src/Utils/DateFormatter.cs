fd6b5b4dfec15bf5daf275715ac8ef544b1bfa9c
ytqsl <yossi.tamari@quartech.com>
2023-01-22 12:06:08 -0800

    formatting and support for nullable datetime in ToShortDateAndTime

--- Apps/CommonData/src/Utils/DateFormatter.cs
+++ Apps/CommonData/src/Utils/DateFormatter.cs
@@ -13,128 +13,131 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 // -------------------------------------------------------------------------
-namespace HealthGateway.Common.Data.Utils;
-
-using System;
-using System.Globalization;
-using System.Runtime.InteropServices;
-using Microsoft.Extensions.Configuration;
-
-/// <summary>
-/// Formats dates and times for display.
-/// </summary>
-public static class DateFormatter
+namespace HealthGateway.Common.Data.Utils
 {
-    private const string UnixTzKey = "TimeZone:UnixTimeZoneId";
-    private const string WindowsTzKey = "TimeZone:WindowsTimeZoneId";
+    using System;
+    using System.Globalization;
+    using System.Runtime.InteropServices;
+    using Microsoft.Extensions.Configuration;
 
     /// <summary>
-    /// Converts the supplied date to a string formatted as YYYY-MM-DD (2022-01-01).
+    /// Formats dates and times for display.
     /// </summary>
-    /// <param name="date">The date to format.</param>
-    /// <returns>The date in YYYY-MM-DD format.</returns>
-    public static string ToShortDate(DateOnly date)
+    public static class DateFormatter
     {
-        return date.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
-    }
+        private const string UnixTzKey = "TimeZone:UnixTimeZoneId";
+        private const string WindowsTzKey = "TimeZone:WindowsTimeZoneId";
 
-    /// <summary>
-    /// Converts the supplied dateTime to a string formatted as YYYY-MM-DD (2022-01-01).
-    /// </summary>
-    /// <param name="dateTime">The dateTime to format.</param>
-    /// <returns>The dateTime in YYYY-MM-DD format.</returns>
-    public static string ToShortDate(DateTime dateTime)
-    {
-        return dateTime.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
-    }
+        /// <summary>
+        /// Converts the supplied date to a string formatted as YYYY-MM-DD (2022-01-01).
+        /// </summary>
+        /// <param name="date">The date to format.</param>
+        /// <returns>The date in YYYY-MM-DD format.</returns>
+        public static string ToShortDate(DateOnly date)
+        {
+            return date.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+        }
 
-    /// <summary>
-    /// Converts the supplied date to a string formatted as MMMM dd, YYYY (January 1, 2022).
-    /// </summary>
-    /// <param name="date">The date to format.</param>
-    /// <returns>The date in MMMM dd, YYYY format.</returns>
-    public static string ToLongDate(DateOnly date)
-    {
-        return date.ToString("MMMM dd, yyyy", CultureInfo.InvariantCulture);
-    }
+        /// <summary>
+        /// Converts the supplied dateTime to a string formatted as YYYY-MM-DD (2022-01-01).
+        /// </summary>
+        /// <param name="dateTime">The dateTime to format.</param>
+        /// <returns>The dateTime in YYYY-MM-DD format.</returns>
+        public static string ToShortDate(DateTime dateTime)
+        {
+            return dateTime.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+        }
 
-    /// <summary>
-    /// Converts the supplied dateTime to a string formatted as MMMM dd, YYYY (January 1, 2022).
-    /// </summary>
-    /// <param name="dateTime">The dateTime to format.</param>
-    /// <returns>The dateTime in MMMM dd, YYYY format.</returns>
-    public static string ToLongDate(DateTime dateTime)
-    {
-        return dateTime.ToString("MMMM dd, yyyy", CultureInfo.InvariantCulture);
-    }
+        /// <summary>
+        /// Converts the supplied date to a string formatted as MMMM dd, YYYY (January 1, 2022).
+        /// </summary>
+        /// <param name="date">The date to format.</param>
+        /// <returns>The date in MMMM dd, YYYY format.</returns>
+        public static string ToLongDate(DateOnly date)
+        {
+            return date.ToString("MMMM dd, yyyy", CultureInfo.InvariantCulture);
+        }
 
-    /// <summary>
-    /// Converts the supplied time to a string formatted as HH:mm (5:00 PM).
-    /// </summary>
-    /// <param name="time">The time to format.</param>
-    /// <returns>The time in HH:mm format.</returns>
-    public static string ToShortTime(TimeOnly time)
-    {
-        return time.ToString("h:mm tt", CultureInfo.InvariantCulture);
-    }
+        /// <summary>
+        /// Converts the supplied dateTime to a string formatted as MMMM dd, YYYY (January 1, 2022).
+        /// </summary>
+        /// <param name="dateTime">The dateTime to format.</param>
+        /// <returns>The dateTime in MMMM dd, YYYY format.</returns>
+        public static string ToLongDate(DateTime dateTime)
+        {
+            return dateTime.ToString("MMMM dd, yyyy", CultureInfo.InvariantCulture);
+        }
 
-    /// <summary>
-    /// Converts the supplied dateTime to a string formatted as HH:mm (5:00 PM).
-    /// </summary>
-    /// <param name="dateTime">The dateTime to format.</param>
-    /// <returns>The dateTime in HH:mm format.</returns>
-    public static string ToShortTime(DateTime dateTime)
-    {
-        return dateTime.ToString("h:mm tt", CultureInfo.InvariantCulture);
-    }
+        /// <summary>
+        /// Converts the supplied time to a string formatted as HH:mm (5:00 PM).
+        /// </summary>
+        /// <param name="time">The time to format.</param>
+        /// <returns>The time in HH:mm format.</returns>
+        public static string ToShortTime(TimeOnly time)
+        {
+            return time.ToString("h:mm tt", CultureInfo.InvariantCulture);
+        }
 
-    /// <summary>
-    /// Converts the supplied dateTime to a string formatted as YYYY-MM-DD HH:mm (2022-01-01 5:00 PM).
-    /// </summary>
-    /// <param name="dateTime">The dateTime to format.</param>
-    /// <returns>The dateTime in HH:mm format.</returns>
-    public static string ToShortDateAndTime(DateTime dateTime)
-    {
-        return dateTime.ToString("yyyy-MM-dd h:mm tt", CultureInfo.InvariantCulture);
-    }
+        /// <summary>
+        /// Converts the supplied dateTime to a string formatted as HH:mm (5:00 PM).
+        /// </summary>
+        /// <param name="dateTime">The dateTime to format.</param>
+        /// <returns>The dateTime in HH:mm format.</returns>
+        public static string ToShortTime(DateTime dateTime)
+        {
+            return dateTime.ToString("h:mm tt", CultureInfo.InvariantCulture);
+        }
 
-    /// <summary>
-    /// Converts the supplied dateTime to a string formatted as MMMM dd, YYYY HH:mm (January 1, 2022 5:00 PM).
-    /// </summary>
-    /// <param name="dateTime">The dateTime to format.</param>
-    /// <returns>The dateTime in HH:mm format.</returns>
-    public static string ToLongDateAndTime(DateTime dateTime)
-    {
-        return dateTime.ToString("MMMM dd, yyyy h:mm tt", CultureInfo.InvariantCulture);
-    }
+        /// <summary>
+        /// Converts the supplied dateTime to a string formatted as YYYY-MM-DD HH:mm (2022-01-01 5:00 PM).
+        /// </summary>
+        /// <param name="dateTime">The dateTime to format.</param>
+        /// <returns>The dateTime in HH:mm format.</returns>
+        public static string ToShortDateAndTime(DateTime? dateTime)
+        {
+            return dateTime == null
+                ? string.Empty
+                : dateTime.Value.ToString("yyyy-MM-dd h:mm tt", CultureInfo.InvariantCulture);
+        }
 
-    /// <summary>
-    /// Tries to parse a string to a DateTime using the format supplied.
-    /// </summary>
-    /// <param name="dateTime">string to parse.</param>
-    /// <param name="format">format to parse into.</param>
-    /// <param name="parsedDateTime">the parsed DateTime is successful.</param>
-    /// <returns>false if failed to parse, true is succeeded.</returns>
-    public static bool TryParse(string dateTime, string format, out DateTime parsedDateTime)
-    {
-        return DateTime.TryParseExact(dateTime, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedDateTime);
-    }
+        /// <summary>
+        /// Converts the supplied dateTime to a string formatted as MMMM dd, YYYY HH:mm (January 1, 2022 5:00 PM).
+        /// </summary>
+        /// <param name="dateTime">The dateTime to format.</param>
+        /// <returns>The dateTime in HH:mm format.</returns>
+        public static string ToLongDateAndTime(DateTime dateTime)
+        {
+            return dateTime.ToString("MMMM dd, yyyy h:mm tt", CultureInfo.InvariantCulture);
+        }
 
-    /// <summary>
-    /// Gets local timezone.
-    /// </summary>
-    /// <param name="configuration">The configuration to use.</param>
-    /// <returns>TimeZoneInfo object representing local timezone.</returns>
-    public static TimeZoneInfo GetLocalTimeZone(IConfiguration configuration)
-    {
-        return TimeZoneInfo.FindSystemTimeZoneById(
-            RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
-                ? GetConfigurationValue<string>(configuration, WindowsTzKey)
-                : GetConfigurationValue<string>(configuration, UnixTzKey));
-    }
+        /// <summary>
+        /// Tries to parse a string to a DateTime using the format supplied.
+        /// </summary>
+        /// <param name="dateTime">string to parse.</param>
+        /// <param name="format">format to parse into.</param>
+        /// <param name="parsedDateTime">the parsed DateTime is successful.</param>
+        /// <returns>false if failed to parse, true is succeeded.</returns>
+        public static bool TryParse(string dateTime, string format, out DateTime parsedDateTime)
+        {
+            return DateTime.TryParseExact(dateTime, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedDateTime);
+        }
 
-    private static T GetConfigurationValue<T>(IConfiguration cfg, string key)
-    {
-        return cfg.GetValue<T>(key)!;
+        /// <summary>
+        /// Gets local timezone.
+        /// </summary>
+        /// <param name="configuration">The configuration to use.</param>
+        /// <returns>TimeZoneInfo object representing local timezone.</returns>
+        public static TimeZoneInfo GetLocalTimeZone(IConfiguration configuration)
+        {
+            return TimeZoneInfo.FindSystemTimeZoneById(
+                RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                    ? GetConfigurationValue<string>(configuration, WindowsTzKey)
+                    : GetConfigurationValue<string>(configuration, UnixTzKey));
+        }
+
+        private static T GetConfigurationValue<T>(IConfiguration cfg, string key)
+        {
+            return cfg.GetValue<T>(key)!;
+        }
     }
 }
