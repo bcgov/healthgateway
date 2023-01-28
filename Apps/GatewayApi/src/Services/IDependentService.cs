bea1f9ed60ca4ea5bfc36bba86478df171a477e3
Ray King <ray.king@quartech.com>
2023-01-27 16:10:12 -0800

    Merge the Dependent Endpoint for PHSA feature branch to dev (#4679)
    * Added new controller and updated service for AB#14739 (#4658)
    
    * Added new controller and updated service for AB#14739
    
    * Changes from PR review for AB#14739
    
    * Changes from review for AB#14739
    
    * Changes from review for AB#14739
    
    * Changes from review for AB#14739
    
    * Updated delegate, refactored page and pageSize to defaults on controller and not nullable in service, renamed pageNumber to page, fixed paging bug on GetDependents(hdid, page, pageSize) method. (#4673)
    
    * Added PhsaControllerTests.cs and new test to DependentServiceTests.cs for AB#14741 (#4677)
    
    * Updated route to protect endpoints in PhsaController for AB#14740 (#4678)

--- Apps/GatewayApi/src/Services/IDependentService.cs
+++ Apps/GatewayApi/src/Services/IDependentService.cs
@@ -15,6 +15,7 @@
 // -------------------------------------------------------------------------
 namespace HealthGateway.GatewayApi.Services
 {
+    using System;
     using System.Collections.Generic;
     using HealthGateway.Common.Data.ViewModels;
     using HealthGateway.GatewayApi.Models;
@@ -34,7 +35,17 @@ namespace HealthGateway.GatewayApi.Services
         RequestResult<IEnumerable<DependentModel>> GetDependents(string hdId, int page = 0, int pageSize = 500);
 
         /// <summary>
-        /// Add a dependent to the given hdId of the delegate (parent or guardiant).
+        /// Gets all the dependents for the given date range.
+        /// </summary>
+        /// <param name="fromDateUtc">The from date time in Utc.</param>
+        /// <param name="toDateUtc">The to date time in Utc.</param>
+        /// <param name="page">The page of data to fetch indexed from 0.</param>
+        /// <param name="pageSize">The amount of records per page.</param>
+        /// <returns>A List of dependents wrapped in a RequestResult.</returns>
+        RequestResult<IEnumerable<GetDependentResponse>> GetDependents(DateTime fromDateUtc, DateTime? toDateUtc, int page, int pageSize);
+
+        /// <summary>
+        /// Add a dependent to the given hdId of the delegate (parent or guardian).
         /// </summary>
         /// <param name="delegateHdId">The HdId of the Delegate (parent or guardian).</param>
         /// <param name="addDependentRequest">The request to create a User Delegate model.</param>
