2ffc9ab24a7778abfe2eece41332cf457b31aa46
Brian Jang <58790456+BrianMaki@users.noreply.github.com>
2023-02-28 09:04:48 -0800

    Dismiss notification centre badge for AB#15007 (#4791)
    * Dismiss notification badge on click and fix last login bug  for AB#15007
    
    * Removed logger message and fixed warning for AB#15007.
    
    * Add functional test for notification badge for AB#15013.
    
    * Fixed bug UTC bug for scheduledDateTimeUtc fom PHSA from AB#15007
    
    * Added additional checks for last login date times array for AB#15007.
    
    * Add comments and modified notifications fixture for AB#15013.
    
    * Revert changes for seed data script for AB#15007.
    
    * Should return undefined if no login returned from history for AB#15007.
    
    * Reverting Visual Studio Code warning private to public fix for AB#15007.

--- Testing/functional/tests/cypress/integration/ui/user/notificationCentre.js
+++ Testing/functional/tests/cypress/integration/ui/user/notificationCentre.js
@@ -123,3 +123,59 @@ describe("Notification Centre", () => {
         );
     });
 });
+
+describe("Notification Badge", () => {
+    beforeEach(() => {
+        cy.configureSettings({
+            notificationCentre: {
+                enabled: true,
+            },
+        });
+
+        cy.intercept("GET", `**/UserProfile/${HDID}`, {
+            fixture: "UserProfileService/userProfile.json",
+        });
+
+        // The scheduledDateTimeUtc must be after user profile's last login in lastLoginDateTimes, which is the second entry
+        // not the first entry. The first entry is the current login.
+        cy.intercept("GET", `**/Notification/${HDID}`, {
+            fixture: "NotificationService/notifications.json",
+        });
+
+        cy.login(
+            Cypress.env("keycloak.username"),
+            Cypress.env("keycloak.password"),
+            AuthMethod.KeyCloak,
+            "/home"
+        );
+    });
+
+    it("Verify notification badge", () => {
+        cy.get("[data-testid=notification-centre-button]")
+            .get("span")
+            .should("have.class", "b-avatar-badge badge-danger")
+            .contains("2");
+
+        cy.get("[data-testid=notification-centre-button]")
+            .should("be.visible")
+            .should("be.enabled")
+            .click();
+
+        cy.get("[data-testid=notification-centre-close-button]")
+            .should("be.visible")
+            .should("be.enabled")
+            .click();
+
+        cy.get("[data-testid=notification-centre-button]").should(
+            "not.have.class",
+            "b-avatar-badge badge-danger"
+        );
+
+        cy.reload();
+
+        cy.get("[data-testid=notification-centre-button]")
+            .get("span")
+            .should("have.class", "b-avatar-badge badge-danger")
+            .contains("2");
+    });
+});
