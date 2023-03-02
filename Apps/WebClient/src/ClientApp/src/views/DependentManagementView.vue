ec0b1461bb6b394c4dd9f0fa59aae74ba2063d17
Mike Lyttle <mikelyttle@gmail.com>
2023-03-01 16:44:42 -0800

    Update new dependent view and card component

--- Apps/WebClient/src/ClientApp/src/views/DependentManagementView.vue
+++ Apps/WebClient/src/ClientApp/src/views/DependentManagementView.vue
@@ -1,11 +1,14 @@
 <script lang="ts">
 import Vue from "vue";
-import { Component } from "vue-property-decorator";
+import { Component, Ref } from "vue-property-decorator";
 import { Action, Getter } from "vuex-class";
 
 import DependentCardComponent from "@/components/dependent/DependentCardComponent.vue";
 import LoadingComponent from "@/components/LoadingComponent.vue";
+import NewDependentComponent from "@/components/modal/NewDependentComponent.vue";
 import BreadcrumbComponent from "@/components/navmenu/BreadcrumbComponent.vue";
+import TutorialComponent from "@/components/shared/TutorialComponent.vue";
+import UserPreferenceType from "@/constants/userPreferenceType";
 import BreadcrumbItem from "@/models/breadcrumbItem";
 import { Dependent } from "@/models/dependent";
 import User from "@/models/user";
@@ -16,6 +19,8 @@ const options: any = {
         BreadcrumbComponent,
         DependentCardComponent,
         LoadingComponent,
+        NewDependentComponent,
+        TutorialComponent,
     },
 };
 
@@ -36,6 +41,9 @@ export default class DependentManagementView extends Vue {
     @Getter("user", { namespace: "user" })
     private user!: User;
 
+    @Ref("newDependentModal")
+    readonly newDependentModal!: NewDependentComponent;
+
     private breadcrumbItems: BreadcrumbItem[] = [
         {
             text: "Dependents",
@@ -49,11 +57,23 @@ export default class DependentManagementView extends Vue {
         return this.dependentsAreLoading;
     }
 
+    private get addDependentTutorialPreference(): string {
+        return UserPreferenceType.TutorialAddDependent;
+    }
+
     private async created(): Promise<void> {
         await this.retrieveDependents({
             hdid: this.user.hdid,
             bypassCache: false,
         });
+    }
+
+    private refreshDependents(): void {
+        this.retrieveDependents({ hdid: this.user.hdid, bypassCache: true });
+    }
+
+    private showModal(): void {
+        this.newDependentModal.showModal();
     }
 }
 </script>
@@ -61,11 +81,37 @@ export default class DependentManagementView extends Vue {
     <div>
         <BreadcrumbComponent :items="breadcrumbItems" />
         <LoadingComponent :is-loading="isLoading" />
-        <page-title title="Dependents" />
+        <page-title title="Dependents">
+            <hg-button
+                id="add-dependent-button"
+                data-testid="addNewDependentBtn"
+                class="float-right"
+                variant="secondary"
+                @click="showModal()"
+            >
+                <hg-icon icon="user-plus" size="medium" class="mr-2" />
+                <span>Add</span>
+            </hg-button>
+            <TutorialComponent
+                :preference-type="addDependentTutorialPreference"
+                target="add-dependent-button"
+            >
+                <div data-testid="add-dependent-tutorial-popover">
+                    Add a dependent under 12 years old to get their health
+                    records.
+                </div>
+            </TutorialComponent>
+        </page-title>
         <DependentCardComponent
             v-for="dependent in dependents"
             :key="dependent.ownerId"
             :dependent="dependent"
+            class="my-3"
+        />
+        <NewDependentComponent
+            ref="newDependentModal"
+            @show="showModal"
+            @handle-submit="refreshDependents"
         />
     </div>
 </template>
