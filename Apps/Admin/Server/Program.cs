a22db544912f2335ddb60561496eb05b0bc34e18
Brian Jang <58790456+BrianMaki@users.noreply.github.com>
2023-03-14 18:37:55 -0700

    Create database tables and delegate for AB15116 (#4876)
    * Create database tables and delegate code for AB#15116
    
    * Code review changes for AB#15116.

--- Apps/Admin/Server/Program.cs
+++ Apps/Admin/Server/Program.cs
@@ -132,6 +132,7 @@ namespace HealthGateway.Admin.Server
 
         private static void AddDelegates(IServiceCollection services)
         {
+            services.AddTransient<IDelegationDelegate, DbDelegationDelegate>();
             services.AddTransient<IMessagingVerificationDelegate, DbMessagingVerificationDelegate>();
             services.AddTransient<IFeedbackDelegate, DbFeedbackDelegate>();
             services.AddTransient<IRatingDelegate, DbRatingDelegate>();
---
+++ Apps/Database/src/Delegates/DbDelegationDelegate.cs
@@ -0,0 +1,171 @@
+// -------------------------------------------------------------------------
+//  Copyright © 2019 Province of British Columbia
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------
+namespace HealthGateway.Database.Delegates
+{
+    using System.Linq;
+    using HealthGateway.Database.Constants;
+    using HealthGateway.Database.Context;
+    using HealthGateway.Database.Models;
+    using HealthGateway.Database.Wrapper;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Logging;
+
+    /// <inheritdoc/>
+    public class DbDelegationDelegate : IDelegationDelegate
+    {
+        private readonly ILogger logger;
+        private readonly GatewayDbContext dbContext;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DbDelegationDelegate"/> class.
+        /// </summary>
+        /// <param name="logger">The injected logger provider.</param>
+        /// <param name="dbContext">The context to be used when accessing the database.</param>
+        public DbDelegationDelegate(ILogger<DbDelegationDelegate> logger, GatewayDbContext dbContext)
+        {
+            this.logger = logger;
+            this.dbContext = dbContext;
+        }
+
+        /// <inheritdoc/>
+        public DbResult<AllowedDelegation> DeleteAllowedDelegation(AllowedDelegation allowedDelegation, bool commit = true)
+        {
+            DbResult<AllowedDelegation> result = new()
+            {
+                Payload = allowedDelegation,
+                Status = DbStatusCode.Deferred,
+            };
+            this.dbContext.AllowedDelegation.Remove(allowedDelegation);
+
+            if (commit)
+            {
+                try
+                {
+                    this.dbContext.SaveChanges();
+                    result.Status = DbStatusCode.Deleted;
+                }
+                catch (DbUpdateException e)
+                {
+                    result.Status = DbStatusCode.Error;
+                    result.Message = e.Message;
+                }
+            }
+
+            return result;
+        }
+
+        /// <inheritdoc/>
+        public DbResult<Dependent> GetDependent(string hdid, bool includeAllowedDelegation = false)
+        {
+            this.logger.LogTrace("Getting dependent - includeAllowedDelegation : {IncludeAllowedDelegation}", includeAllowedDelegation);
+
+            IQueryable<Dependent> query = this.dbContext.Dependent
+                .Where(d => d.HdId == hdid);
+
+            if (includeAllowedDelegation)
+            {
+                query = query.Include(d => d.AllowedDelegations);
+            }
+
+            Dependent? dependent = query.SingleOrDefault();
+
+            if (dependent == null)
+            {
+                return new()
+                {
+                    Status = DbStatusCode.NotFound,
+                    Message = $"Unable to find dependent by hdid: {hdid}",
+                };
+            }
+
+            return new()
+            {
+                Payload = dependent,
+                Status = DbStatusCode.Read,
+            };
+        }
+
+        /// <inheritdoc/>
+        public DbResult<Dependent> InsertDependent(Dependent dependent, bool commit = true)
+        {
+            DbResult<Dependent> result = new();
+            this.dbContext.Dependent.Add(dependent);
+
+            if (commit)
+            {
+                try
+                {
+                    this.dbContext.SaveChanges();
+                    result.Status = DbStatusCode.Created;
+                }
+                catch (DbUpdateException e)
+                {
+                    result.Status = DbStatusCode.Error;
+                    result.Message = e.Message;
+                }
+            }
+
+            return result;
+        }
+
+        /// <inheritdoc/>
+        public DbResult<AllowedDelegation> InsertAllowedDelegation(AllowedDelegation allowedDelegation, bool commit = true)
+        {
+            DbResult<AllowedDelegation> result = new();
+            this.dbContext.AllowedDelegation.Add(allowedDelegation);
+
+            if (commit)
+            {
+                try
+                {
+                    this.dbContext.SaveChanges();
+                    result.Status = DbStatusCode.Created;
+                }
+                catch (DbUpdateException e)
+                {
+                    result.Status = DbStatusCode.Error;
+                    result.Message = e.Message;
+                }
+            }
+
+            return result;
+        }
+
+        /// <inheritdoc/>
+        public DbResult<Dependent> UpdateDependent(Dependent dependent, bool commit = true)
+        {
+            this.dbContext.Dependent.Update(dependent);
+            DbResult<Dependent> result = new();
+
+            if (commit)
+            {
+                try
+                {
+                    this.dbContext.SaveChanges();
+                    result.Status = DbStatusCode.Updated;
+                    result.Payload = dependent;
+                }
+                catch (DbUpdateException e)
+                {
+                    result.Status = DbStatusCode.Error;
+                    result.Message = e.Message;
+                }
+            }
+
+            return result;
+        }
+    }
+}
