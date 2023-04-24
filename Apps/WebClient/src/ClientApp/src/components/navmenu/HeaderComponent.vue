169be61a23ec46345482c8a7b6d8659fc3430b24
Declan Coughlan <deccough@gmail.com>
2023-04-24 16:33:20 -0700

    Update Apps/WebClient/src/ClientApp/src/components/navmenu/HeaderComponent.vue
    Co-authored-by: Mike Lyttle <mikelyttle@gmail.com>

--- Apps/WebClient/src/ClientApp/src/components/navmenu/HeaderComponent.vue
+++ Apps/WebClient/src/ClientApp/src/components/navmenu/HeaderComponent.vue
@@ -419,7 +419,7 @@ export default class HeaderComponent extends Vue {
         </b-navbar>
 
         <RatingComponent ref="ratingComponent" @on-close="processLogout()" />
-        <app-tour-component ref="appTourComponent" />
+        <AppTourComponent ref="appTourComponent" />
     </header>
 </template>
 
