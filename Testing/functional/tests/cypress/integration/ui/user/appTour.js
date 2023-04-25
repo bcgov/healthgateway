2d57c368e29d83fce016df9d2c0304597749194d
Declan Coughlan <deccough@gmail.com>
2023-04-25 12:13:52 -0700

    Refine AppTour testing

--- Testing/functional/tests/cypress/integration/ui/user/appTour.js
+++ Testing/functional/tests/cypress/integration/ui/user/appTour.js
@@ -31,18 +31,24 @@ describe("App Tour Authenticated", () => {
             .should("not.exist");
     });
 
-    function keepClickNext() {
+    function keepClickingNext(resolve) {
         const nextButtonId = "[data-testid=app-tour-next]";
-        cy.get("[data-testid=app-tour-modal]").then((modal) => {
-            if (modal.find(nextButtonId).length) {
-                cy.get(nextButtonId).should("be.visible").click();
-                keepClickNext();
-            }
-        });
+
+        cy.get("[data-testid=app-tour-modal]")
+            .wait(1000) // Ensure all animations are complete
+            .then((modal) => {
+                if (modal.find(nextButtonId).length) {
+                    cy.get(nextButtonId).as("nextBtn").should("be.visible");
+                    cy.get("@nextBtn").click();
+                    keepClickingNext(resolve);
+                } else {
+                    resolve();
+                }
+            });
     }
 
-    it("Modal controls should be visible and work (no back)", () => {
-        cy.get("[data-testid=app-tour-button]").click();
+    it("User should be able to move through all slides and use done to close modal", () => {
+        cy.get("[data-testid=app-tour-button]").should("be.visible").click();
         cy.get("[data-testid=app-tour-modal]").should("be.visible");
 
         cy.get("[data-testid=app-tour-back]")
@@ -54,34 +60,42 @@ describe("App Tour Authenticated", () => {
 
         cy.get("[data-testid=app-tour-button]").click();
 
-        keepClickNext();
+        new Cypress.Promise((resolve) => {
+            keepClickingNext(resolve);
+        }).then(() => {
+            // last slide app-tour-done should be visible
+            cy.get("[data-testid=app-tour-done]").should("be.visible").click();
 
-        // last slide app-tour-done should be visible
-        cy.get("[data-testid=app-tour-done]").should("be.visible").click();
-
-        // modal should be closed
-        cy.get("[data-testid=app-tour-modal]").should("not.exist");
+            // modal should be closed
+            cy.get("[data-testid=app-tour-modal]").should("not.exist");
+        });
     });
 
     // This greatly depends on the tour content, and isn't a great test but will test if the content is there
-    it("Modal controls should be visible and work forwards and backwards", () => {
-        cy.get("[data-testid=app-tour-button]").click();
+    it("User should be able to navigate forwards and backwards if ", () => {
+        cy.get("[data-testid=app-tour-button]").should("be.visible").click();
         cy.get("[data-testid=app-tour-modal]").should("be.visible");
 
-        // first slide back should be disabled
-        cy.get("[data-testid=app-tour-back]")
-            .should("be.visible")
-            .should("be.disabled");
+        // first slide: back should be disabled
+        cy.get("[data-testid=app-tour-back]").should(
+            "be.visible",
+            "be.disabled"
+        );
 
         cy.get("[data-testid=app-tour-next]").should("be.visible").click();
 
-        cy.get("[data-testid=app-tour-modal]").then((modal) => {
-            if (modal.find("[data-testid=app-tour-back]").length) {
-                cy.get("[data-testid=app-tour-back]")
-                    .should("be.visible")
-                    .click();
-            }
-            cy.log("No back button found");
-        });
+        // second slide: either back should be enabled or done should be visible
+        cy.get("[data-testid=app-tour-modal]")
+            .wait(1000) // Wait for animations to complete
+            .then((modal) => {
+                if (modal.find("[data-testid=app-tour-back]").length) {
+                    cy.get("[data-testid=app-tour-back]")
+                        .should("be.visible", "be.enabled")
+                        .click()
+                        .should("be.visible", "be.disabled");
+                } else {
+                    cy.get("[data-testid=app-tour-done]").should("be.visible");
+                }
+            });
     });
 });
