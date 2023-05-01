b80b95f985536d9ffed6e95688bfb470ee3c80ee
Declan Coughlan <deccough@gmail.com>
2023-05-01 13:35:41 -0700

    Remove required from InviteKey

--- Apps/Database/src/Migrations/20230501202904_RemoveRequiredInviteKey.Designer.cs
+++ Apps/Database/src/Migrations/20230501202904_RemoveRequiredInviteKey.Designer.cs
@@ -5,6 +5,7 @@ using System.Text.Json;
 using HealthGateway.Database.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -13,9 +14,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace HealthGateway.Database.Migrations
 {
     [DbContext(typeof(GatewayDbContext))]
-    partial class GatewayDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230501202904_RemoveRequiredInviteKey")]
+    partial class RemoveRequiredInviteKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
