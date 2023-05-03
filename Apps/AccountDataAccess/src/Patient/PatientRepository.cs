41c88c025bae6f07f9adde3713e1be4f03c91596
BrianMaki <brian.jang.developer@outlook.com>
2023-05-03 15:12:09 -0700

    Add patient to cache after calling phsa patient-identity for AB#15500.

--- Apps/AccountDataAccess/src/Patient/PatientRepository.cs
+++ Apps/AccountDataAccess/src/Patient/PatientRepository.cs
@@ -197,7 +197,9 @@ namespace HealthGateway.AccountDataAccess.Patient
             try
             {
                 PatientIdentity result = await this.patientIdentityApi.GetPatientIdentityAsync(hdid).ConfigureAwait(true);
-                return this.mapper.Map<PatientModel>(result);
+                PatientModel patient = this.mapper.Map<PatientModel>(result);
+                this.CachePatient(patient);
+                return patient;
             }
             catch (ApiException e) when (e.StatusCode == HttpStatusCode.NotFound)
             {
