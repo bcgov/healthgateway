7ab2761e9535211a23855f09785c646ef13edfc1
ytqsl <yossi.tamari@quartech.com>
2023-05-09 09:39:23 -0700

    added test containers support to integration tests

--- Testing/integration/tests/ScenarioBase.cs
+++ Testing/integration/tests/ScenarioBase.cs
@@ -16,40 +16,78 @@
 
 namespace HealthGateway.IntegrationTests;
 
-using System.Reflection;
 using Alba;
+using DotNet.Testcontainers.Containers;
+using HealthGateway.Database.Context;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Testcontainers.PostgreSql;
+using Testcontainers.Redis;
 using Xunit.Abstractions;
 
-#pragma warning disable S3220 // Method calls should not resolve ambiguously to overloads with "params"
-
-public class WebAppFixture
+public class WebAppFixture : IAsyncLifetime
 {
     private readonly ServiceCollection testRelatedServices = new();
+    private readonly PostgreSqlContainer postgreSqlContainer = new PostgreSqlBuilder().Build();
+    private readonly RedisContainer redisContainer = new RedisBuilder().WithImage("docker.io/redis:7.0").Build();
 
     public IServiceCollection Services => this.testRelatedServices;
 
-    public async Task<IAlbaHost> CreateHost<TStartup>(ITestOutputHelper output, IConfiguration configuration)
+    public virtual async Task<IAlbaHost> CreateHost<TStartup>(ITestOutputHelper output, params KeyValuePair<string, string?>[] configurationSettings)
         where TStartup : class
     {
+#pragma warning disable S3220 // Method calls should not resolve ambiguously to overloads with "params"
         IAlbaHost host = await AlbaHost.For<TStartup>(
             builder =>
             {
                 builder.ConfigureServices(
-                    services =>
+                    (_, services) =>
                     {
+                        // add test specific dependencies
                         foreach (ServiceDescriptor service in this.testRelatedServices)
                         {
                             services.Add(service);
                         }
 
+                        // set logging to xunit output
                         services.AddLogging(loggingBuilder => loggingBuilder.AddXUnit(output));
                     });
+
+                // override the db connection string
+                var configOverrides = new Dictionary<string, string?>(configurationSettings)
+                {
+                    { "ConnectionStrings:GatewayConnection", this.postgreSqlContainer.GetConnectionString() },
+                    { "RedisConnection", this.redisContainer.GetConnectionString() },
+                };
+                // set or override configuration settings
+                builder.ConfigureAppConfiguration((context, configBuilder) =>
+                {
+                    configBuilder.AddInMemoryCollection(configOverrides);
+                });
             });
+#pragma warning restore S3220 // Method calls should not resolve ambiguously to overloads with "params"
 
         return host;
+    }
+
+    public async Task InitializeAsync()
+    {
+        if (this.postgreSqlContainer.State != TestcontainersStates.Running)
+        {
+            await this.postgreSqlContainer.StartAsync();
+        }
+        if (this.redisContainer.State != TestcontainersStates.Running)
+        {
+            await this.redisContainer.StartAsync();
+        }
+    }
+
+    public async Task DisposeAsync()
+    {
+        await this.postgreSqlContainer.DisposeAsync();
+        await this.redisContainer.DisposeAsync();
     }
 }
 
@@ -63,7 +101,6 @@ public abstract class ScenarioContextBase<TStartup> : IAsyncLifetime, IClassFixt
     where TStartup : class
 {
     private readonly WebAppFixture fixture;
-    private readonly IConfiguration configuration;
 
     protected IAlbaHost Host { get; private set; } = null!;
 
@@ -73,12 +110,19 @@ public abstract class ScenarioContextBase<TStartup> : IAsyncLifetime, IClassFixt
     {
         this.Output = output;
         this.fixture = fixture;
-        this.configuration = new ConfigurationBuilder().AddUserSecrets(Assembly.GetExecutingAssembly(), false).Build();
     }
 
     public virtual async Task InitializeAsync()
     {
-        this.Host = await this.fixture.CreateHost<TStartup>(this.Output, this.configuration);
+        this.Host = await this.fixture.CreateHost<TStartup>(this.Output);
+
+        using var initScope = this.Host.Services.CreateScope();
+        await MigrateDatabase(initScope.ServiceProvider.GetRequiredService<GatewayDbContext>());
+    }
+
+    private static async Task MigrateDatabase(GatewayDbContext ctx)
+    {
+        await ctx.Database.MigrateAsync();
     }
 
     public async Task DisposeAsync()
