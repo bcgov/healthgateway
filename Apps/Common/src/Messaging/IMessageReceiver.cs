e0a41fde36473639ffc23899103a89ba00e1fedc
Yossi T <48697347+ytqsl@users.noreply.github.com>
2023-05-09 14:02:08 -0700

    PHSA Change Feed technical work (#5094)
    * refactored serialization helpers to own util and changed default discriminator to start with $
    
    * message sending and receiving + integration tests project
    
    * trying to sneak in some relaxed rules :)
    
    * fixed DI error
    
    * hangfire outbox support and integration tests
    
    * added messaging config to jobscheduler
    
    * added transactional test and fixed broken tests
    
    * cleanup
    
    * more cleanup
    
    * more cleanup
    
    * added created-on timestamp to messages
    
    * added created on metadata
    
    * added dots, commas and some insignificat required documenation
    
    * updated interface documentation
    
    * Apply suggestions from code review
    
    Co-authored-by: Declan Coughlan <deccough@gmail.com>
    
    * introduced messageenvelope as a wrapper
    
    * Apply automated code reformatting and cleanup
    
    * better integration tests with scopes to test parallelism
    
    * Added dedicated outbox table
    
    * fixed warnings
    
    * cleanup
    
    * added test containers support to integration tests
    
    * review adjustments
    
    ---------
    
    Co-authored-by: Declan Coughlan <deccough@gmail.com>
    Co-authored-by: Mike Lyttle <mikelyttle@gmail.com>

---
+++ Apps/Common/src/Messaging/IMessageReceiver.cs
@@ -0,0 +1,40 @@
+// -------------------------------------------------------------------------
+//  Copyright © 2019 Province of British Columbia
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------
+
+namespace HealthGateway.Common.Messaging;
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Receive messages.
+/// </summary>
+public interface IMessageReceiver
+{
+    /// <summary>
+    /// Subscribes handlers to receive one-way messages.
+    /// </summary>
+    /// <param name="receiveHandler">
+    /// Message receive handler that receives a session id and array of messages,
+    /// it can return false to reject the messages or true when processed successfully.
+    /// </param>
+    /// <param name="errorHandler">Error handler that receives the exception when an error occurred.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>Awaitable task.</returns>
+    Task Subscribe(Func<string, IEnumerable<MessageEnvelope>, Task<bool>> receiveHandler, Func<Exception, Task> errorHandler, CancellationToken ct = default);
+}
