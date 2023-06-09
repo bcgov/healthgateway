c5b91dbc392fcdfba672bb516ab60b0da2a071ab
Mike Lyttle <mikelyttle@gmail.com>
2023-06-09 11:28:56 -0700

    Initialize Vue 3 ClientApp with Vuetify and Pinia
    - removes project-specific .gitignore and .editorconfig
    - adds and enables prettier and @vue/eslint-config-prettier

---
+++ Apps/Gateway/src/ClientApp/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        "plugin:vue/vue3-essential",
+        "eslint:recommended",
+        "@vue/eslint-config-typescript",
+        "@vue/eslint-config-prettier",
+    ],
+    rules: {
+        "vue/multi-word-component-names": "off",
+    },
+};
