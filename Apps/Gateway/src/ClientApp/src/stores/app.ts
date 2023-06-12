37dc925fdeea084dcc36c5ffc3fbf3039fb78fcd
Mike Lyttle <mikelyttle@gmail.com>
2023-06-12 16:49:32 -0700

    Migrate app store AB#15694

--- Apps/Gateway/src/ClientApp/src/stores/app.ts
+++ Apps/Gateway/src/ClientApp/src/stores/app.ts
@@ -1,8 +1,30 @@
-// Utilities
+import { ref } from "vue";
 import { defineStore } from "pinia";
+import { AppErrorType } from "@/constants/errorType";
 
-export const useAppStore = defineStore("app", {
-    state: () => ({
-        //
-    }),
+export const useAppStore = defineStore("app", () => {
+    const appError = ref<AppErrorType>();
+    const isMobile = ref(false);
+    const isIdle = ref(false);
+
+    function setAppError(errorType: AppErrorType): void {
+        appError.value = errorType;
+    }
+
+    function setIsMobile(value: boolean) {
+        isMobile.value = value;
+    }
+
+    function setIsIdle(value: boolean) {
+        isIdle.value = value;
+    }
+
+    return {
+        appError,
+        isMobile,
+        isIdle,
+        setAppError,
+        setIsMobile,
+        setIsIdle,
+    };
 });
