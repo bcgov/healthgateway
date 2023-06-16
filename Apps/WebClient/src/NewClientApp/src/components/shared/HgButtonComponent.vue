9a64bc7020910269503c38feacc546200f520e20
Mike Lyttle <mikelyttle@gmail.com>
2023-06-16 11:33:09 -0700

    Remove classes from HgButtonComponent

--- Apps/WebClient/src/NewClientApp/src/components/shared/HgButtonComponent.vue
+++ Apps/WebClient/src/NewClientApp/src/components/shared/HgButtonComponent.vue
@@ -15,29 +15,24 @@ const props = withDefaults(defineProps<Props>(), {
 type HgButtonVariant = "primary" | "secondary" | "link";
 
 interface HgButtonVariantDetails {
-    className: string;
     vuetifyVariant: "flat" | "outlined" | "text";
     color: string;
 }
 
 const variants = new Map<HgButtonVariant, HgButtonVariantDetails>();
 variants.set("primary", {
-    className: "hg-primary",
     vuetifyVariant: "flat",
     color: "primary",
 });
 variants.set("secondary", {
-    className: "hg-secondary",
     vuetifyVariant: "outlined",
     color: "primary",
 });
 variants.set("link", {
-    className: "hg-link",
     vuetifyVariant: "text",
     color: "link",
 });
 
-const className = computed(() => variants.get(props.variant)?.className);
 const vuetifyVariant = computed(
     () => variants.get(props.variant)?.vuetifyVariant
 );
@@ -45,13 +40,7 @@ const color = computed(() => variants.get(props.variant)?.color);
 </script>
 
 <template>
-    <v-btn
-        :variant="vuetifyVariant"
-        class="hg-button"
-        :class="className"
-        :color="color"
-        :ripple="false"
-    >
+    <v-btn :variant="vuetifyVariant" :color="color" :ripple="false">
         <slot />
     </v-btn>
 </template>
