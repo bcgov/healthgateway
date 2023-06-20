ee276bbd79ac3e260157aea41545a2635468a822
Mike Lyttle <mikelyttle@gmail.com>
2023-06-20 15:26:12 -0700

    Update HgButtonComponent with alternate appearances on dark backgrounds

--- Apps/WebClient/src/NewClientApp/src/components/shared/HgButtonComponent.vue
+++ Apps/WebClient/src/NewClientApp/src/components/shared/HgButtonComponent.vue
@@ -15,41 +15,58 @@ const props = withDefaults(defineProps<Props>(), {
 type HgButtonVariant = "primary" | "secondary" | "link";
 
 interface HgButtonVariantDetails {
+    inverseVariant: HgButtonVariant;
     vuetifyVariant: "flat" | "outlined" | "text";
     color: string;
+    bgColor?: string;
 }
 
 const variants = new Map<HgButtonVariant, HgButtonVariantDetails>();
 variants.set("primary", {
+    inverseVariant: "secondary",
     vuetifyVariant: "flat",
     color: "primary",
 });
 variants.set("secondary", {
+    inverseVariant: "primary",
     vuetifyVariant: "outlined",
     color: "primary",
+    bgColor: "white",
 });
 variants.set("link", {
+    inverseVariant: "link",
     vuetifyVariant: "text",
     color: "link",
 });
 
 const slots = useSlots();
 
-const vuetifyVariant = computed(
-    () => variants.get(props.variant)?.vuetifyVariant
-);
-const color = computed(() => variants.get(props.variant)?.color);
 const hasSlot = computed(() => slots.default !== undefined);
+const variantDetails = computed(() => {
+    const details = variants.get(props.variant)!;
+    return props.inverse ? variants.get(details.inverseVariant)! : details;
+});
+const bgColor = computed(() => variantDetails.value.bgColor);
+const classes = computed(() => ({
+    [`bg-${bgColor.value}`]: Boolean(bgColor.value),
+}));
 </script>
 
 <template>
     <v-btn
         v-if="hasSlot"
-        :variant="vuetifyVariant"
-        :color="color"
+        :variant="variantDetails.vuetifyVariant"
+        :color="variantDetails.color"
+        :class="classes"
         :ripple="false"
     >
         <slot />
     </v-btn>
-    <v-btn v-else :variant="vuetifyVariant" :color="color" :ripple="false" />
+    <v-btn
+        v-else
+        :variant="variantDetails.vuetifyVariant"
+        :color="variantDetails.color"
+        :class="classes"
+        :ripple="false"
+    />
 </template>
