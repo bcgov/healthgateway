c5e09bf326705cdec464e4e9483c5672cc102332
Mike Lyttle <mikelyttle@gmail.com>
2023-06-20 13:15:32 -0700

    Migrate PageTitleComponent to vuetify AB#15736

---
+++ Apps/WebClient/src/NewClientApp/src/components/shared/PageTitleComponent.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { computed, useSlots } from "vue";
+
+interface Props {
+    title: string;
+}
+defineProps<Props>();
+
+const slots = useSlots();
+
+const hasPrependSlot = computed(() => slots.prepend !== undefined);
+const hasAppendSlot = computed(() => slots.append !== undefined);
+</script>
+
+<template>
+    <div id="pageTitle">
+        <v-row dense justify="end" align="center">
+            <v-col v-if="hasPrependSlot" class="flex-grow-0">
+                <slot name="prepend" />
+            </v-col>
+            <v-col>
+                <h1 id="subject" class="text-primary">
+                    {{ title }}
+                </h1>
+            </v-col>
+            <v-col v-if="hasAppendSlot" class="flex-grow-0">
+                <slot name="append" />
+            </v-col>
+        </v-row>
+        <v-divider
+            class="border-opacity-100 my-2"
+            color="primary"
+            :thickness="2"
+        />
+    </div>
+</template>
