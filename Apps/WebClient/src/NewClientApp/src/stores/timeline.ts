b28825e60be51d2b73d38f2d31680a1b942a2189
BrianMaki <brian.jang.developer@outlook.com>
2023-06-22 13:27:03 -0700

    Implement timeline store for Vue 3.0 for AB#15708

---
+++ Apps/WebClient/src/NewClientApp/src/stores/timeline.ts
@@ -0,0 +1,106 @@
+import { DateWrapper } from "./../models/dateWrapper";
+import { container } from "@/ioc/container";
+import { SERVICE_IDENTIFIER } from "@/ioc/identifier";
+import { StringISODate } from "@/models/dateWrapper";
+import TimelineFilter, { TimelineFilterBuilder } from "@/models/timelineFilter";
+import { ILogger } from "@/services/interfaces";
+import { defineStore } from "pinia";
+import { computed, ref } from "vue";
+import { EntryType } from "@/constants/entryType";
+
+export const useNoteStore = defineStore("timeline", () => {
+    const logger = container.get<ILogger>(SERVICE_IDENTIFIER.Logger);
+
+    // Refs
+    const keyword = ref("");
+    const timeLineFilter = ref(TimelineFilterBuilder.buildEmpty());
+    const timeLineLinearDate = ref(new DateWrapper().toISO());
+    const timeLineSelectedDate = ref<StringISODate>();
+
+    // Computed
+    const filter = computed(() => {
+        if (timeLineFilter.value instanceof TimelineFilter) {
+            return timeLineFilter.value;
+        } else {
+            return Object.assign(
+                TimelineFilterBuilder.buildEmpty(),
+                timeLineFilter.value
+            );
+        }
+    });
+
+    const hasActiveFilter = computed(() => {
+        if (timeLineFilter.value instanceof TimelineFilter) {
+            return (
+                timeLineFilter.value.hasActiveFilter() || keyword.value !== ""
+            );
+        }
+
+        const timelineFilter: TimelineFilter = Object.assign(
+            TimelineFilterBuilder.buildEmpty(),
+            timeLineFilter.value
+        );
+
+        return timelineFilter.hasActiveFilter() || keyword.value !== "";
+    });
+
+    const linearDate = computed(() => {
+        return new DateWrapper(timeLineLinearDate.value);
+    });
+
+    const selectedDate = computed(() => {
+        if (timeLineSelectedDate.value === undefined) {
+            return undefined;
+        }
+
+        return new DateWrapper(timeLineLinearDate.value);
+    });
+
+    const selectedEntryTypes = computed(() => {
+        if (
+            timeLineFilter.value instanceof TimelineFilter &&
+            timeLineFilter.value.entryTypes instanceof Set<EntryType>
+        ) {
+            return timeLineFilter.value.entryTypes;
+        }
+
+        return Object.assign(
+            new Set<EntryType>(),
+            timeLineFilter.value.entryTypes
+        );
+    });
+
+    // Actions
+    function setFilter(filterBuilder: TimelineFilterBuilder) {
+        logger.verbose(`Setting filter to time line store.`);
+        timeLineFilter.value = filterBuilder.build();
+    }
+
+    function clearFilter() {
+        logger.verbose(`Clearing filter to time line store.`);
+        timeLineFilter.value = TimelineFilterBuilder.buildEmpty();
+    }
+
+    function setLinearDate(linearDate: DateWrapper) {
+        logger.verbose(`Setting linear date to time line store.`);
+        timeLineLinearDate.value = linearDate.toISO();
+    }
+
+    function setSelectedDate(selectedDate?: DateWrapper) {
+        logger.verbose(`Setting selected date to time line store.`);
+        timeLineSelectedDate.value = selectedDate?.toISO();
+    }
+
+    return {
+        filter,
+        hasActiveFilter,
+        keyword,
+        linearDate,
+        selectedDate,
+        selectedEntryTypes,
+        setFilter,
+        clearFilter,
+        setLinearDate,
+        setSelectedDate,
+    };
+});
