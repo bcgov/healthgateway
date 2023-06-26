8b233bc94b336373665b85a8eb97d21c6d7bc9c0
Brian Jang <58790456+BrianMaki@users.noreply.github.com>
2023-06-26 16:47:44 -0700

    Convert TooManyRequestsComponent to Vue 3 for AB#15781 (#5279)
    * Convert TooManyRequestsComponent to Vue 3 for AB#15781.
    
    * Clean up delete unused imports in timeline store.
    
    * Code review change for AB#15781

---
+++ Apps/WebClient/src/NewClientApp/src/components/TooManyRequestsComponent.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { useErrorStore } from "@/stores/error";
+import { computed } from "vue";
+
+interface Props {
+    location?: string;
+}
+const props = withDefaults(defineProps<Props>(), {
+    location: "page",
+});
+
+const errorStore = useErrorStore();
+
+const showWarning = computed(
+    () => errorStore.tooManyRequestsWarning == props.location
+);
+
+const showError = computed(
+    () => errorStore.tooManyRequestsError == props.location
+);
+
+function clearTooManyRequestsWarning(): void {
+    errorStore.clearTooManyRequestsWarning();
+}
+
+function clearTooManyRequestsError(): void {
+    errorStore.clearTooManyRequestsError();
+}
+</script>
+
+<template>
+    <div>
+        <v-alert
+            :show="showWarning"
+            data-testid="too-many-requests-warning"
+            color="warning"
+            dismissible
+            class="no-print"
+            @dismissed="clearTooManyRequestsWarning"
+        >
+            We are unable to complete all actions because the site is too busy.
+            Please try again later.
+        </v-alert>
+        <v-alert
+            :show="showError"
+            data-testid="too-many-requests-error"
+            color="error"
+            dismissible
+            class="no-print"
+            @dismissed="clearTooManyRequestsError"
+        >
+            Unable to complete action as the site is too busy. Please try again
+            later.
+        </v-alert>
+    </div>
+</template>
