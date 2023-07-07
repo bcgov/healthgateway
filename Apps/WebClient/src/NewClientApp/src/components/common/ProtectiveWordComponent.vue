38e71ce028ef414b9d15d62dca20fe0df1126779
Declan Coughlan <deccough@gmail.com>
2023-07-07 10:45:24 -0700

    Run Linting

--- Apps/WebClient/src/NewClientApp/src/components/common/ProtectiveWordComponent.vue
+++ Apps/WebClient/src/NewClientApp/src/components/common/ProtectiveWordComponent.vue
@@ -1,13 +1,13 @@
 ﻿<script setup lang="ts">
 import { computed, ref } from "vue";
 
-import { ResultError } from "@/models/errors";
-import { ILogger } from "@/services/interfaces";
-import {container} from "@/ioc/container";
-import {SERVICE_IDENTIFIER} from "@/ioc/identifier";
-import {useMedicationStore} from "@/stores/medication";
 import HgButtonComponent from "@/components/shared/HgButtonComponent.vue";
 import HgIconButtonComponent from "@/components/shared/HgIconButtonComponent.vue";
+import { container } from "@/ioc/container";
+import { SERVICE_IDENTIFIER } from "@/ioc/identifier";
+import { ResultError } from "@/models/errors";
+import { ILogger } from "@/services/interfaces";
+import { useMedicationStore } from "@/stores/medication";
 
 interface Props {
     hdid: string;
@@ -37,10 +37,17 @@ const protectiveWordAttempts = computed(() =>
 );
 
 const errorMessages = computed(() =>
-    protectiveWordAttempts.value > 1 ? ["Invalid protective word. Try again."] : []
+    protectiveWordAttempts.value > 1
+        ? ["Invalid protective word. Try again."]
+        : []
 );
 
-const isVisible = computed(() => !forceClosed.value && medicationsAreProtected.value && !medicationsAreLoading.value);
+const isVisible = computed(
+    () =>
+        !forceClosed.value &&
+        medicationsAreProtected.value &&
+        !medicationsAreLoading.value
+);
 
 function handleOk(): void {
     fetchMedications();
@@ -52,12 +59,11 @@ function handleClose(): void {
 }
 
 function fetchMedications(): void {
-    medicationStore.retrieveMedications(
-        props.hdid,
-        protectiveWord.value,
-    ).catch((err: ResultError) =>
-        logger.error("Error retrieving medications: " + JSON.stringify(err))
-    );
+    medicationStore
+        .retrieveMedications(props.hdid, protectiveWord.value)
+        .catch((err: ResultError) =>
+            logger.error("Error retrieving medications: " + JSON.stringify(err))
+        );
 }
 </script>
 
@@ -72,7 +78,11 @@ function fetchMedications(): void {
         <div class="d-flex justify-center">
             <v-card max-width="700px">
                 <v-card-title class="bg-primary px-0">
-                    <v-toolbar title="Restricted PharmaNet Records" density="compact" color="primary">
+                    <v-toolbar
+                        title="Restricted PharmaNet Records"
+                        density="compact"
+                        color="primary"
+                    >
                         <HgIconButtonComponent
                             data-testid="protectiveWordCloseButton"
                             icon="close"
@@ -82,30 +92,32 @@ function fetchMedications(): void {
                 </v-card-title>
                 <v-card-text class="text-body-1 pa-3">
                     <v-form @submit.stop.prevent="handleOk">
-                                <v-text-field
-                                    id="protectiveWord-input"
-                                    v-model="protectiveWord"
-                                    label="Protective Word"
-                                    data-testid="protectiveWordInput"
-                                    type="password"
-                                    :error-messages="errorMessages"
-                                    required
-                                />
+                        <v-text-field
+                            id="protectiveWord-input"
+                            v-model="protectiveWord"
+                            label="Protective Word"
+                            data-testid="protectiveWordInput"
+                            type="password"
+                            :error-messages="errorMessages"
+                            required
+                        />
                     </v-form>
                 </v-card-text>
-                <v-card-actions class="d-flex flex-column align-start border-t-sm pa-4">
+                <v-card-actions
+                    class="d-flex flex-column align-start border-t-sm pa-4"
+                >
                     <HgButtonComponent
                         data-testid="protectiveWordContinueBtn"
                         variant="primary"
                         :disabled="!protectiveWord"
-                        @click.prevent="handleOk()"
                         text="Continue"
                         class="mb-3"
+                        @click.prevent="handleOk()"
                     />
                     <p>
                         <small data-testid="protectiveWordModalText">
-                            Please enter the protective word required to
-                            access these restricted PharmaNet records.
+                            Please enter the protective word required to access
+                            these restricted PharmaNet records.
                         </small>
                     </p>
                     <p>
