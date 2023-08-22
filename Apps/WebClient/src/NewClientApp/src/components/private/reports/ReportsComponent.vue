54caf47c992927d08aca85d4cf5fc47d21855ea5
Mike Lyttle <mikelyttle@gmail.com>
2023-08-21 17:12:50 -0700

    Prevent long medications escaping chips AB#15978

--- Apps/WebClient/src/NewClientApp/src/components/private/reports/ReportsComponent.vue
+++ Apps/WebClient/src/NewClientApp/src/components/private/reports/ReportsComponent.vue
@@ -404,6 +404,7 @@ for (const [entryType] of reportComponentMap) {
                     <v-chip
                         v-for="item in reportFilter.medications"
                         :key="item"
+                        class="excluded-medication"
                         closable
                         :title="item"
                         :data-testid="`${replaceSpaceWithDash(
@@ -527,3 +528,9 @@ for (const [entryType] of reportComponentMap) {
         />
     </div>
 </template>
+
+<style lang="scss">
+.excluded-medication .v-chip__content {
+    overflow: hidden !important;
+}
+</style>
