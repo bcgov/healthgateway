ca59eb3f5f576158a9f211276e75340b9ebd6a00
Stephen Laws <stephen.laws@nomostech.com>
2023-09-14 14:56:19 -0700

    Remove quotes

--- Tools/Pipelines/scripts/azp_runAdminFunctionalTests.sh
+++ Tools/Pipelines/scripts/azp_runAdminFunctionalTests.sh
@@ -37,9 +37,9 @@ npm ci
 
 echo "Running Cypress Functional Tests"
 TZ=America/Vancouver npx cypress run \
-  --env "keycloak_password=$KEYCLOAK_PASSWORD,\
+  --env keycloak_password=$KEYCLOAK_PASSWORD,\
   idir_password=$IDIR_PASSWORD,\
-  keycloak_admin_secret=$KEYCLOAK_ADMIN_SECRET" \
+  keycloak_admin_secret=$KEYCLOAK_ADMIN_SECRET \
   --record \
   --key $CYPRESS_ADMIN_KEY \
   --parallel \
