c039d24b4b30318355bc9477a2695c05bab8871d
BrianMaki <brian.jang.developer@outlook.com>
2023-09-26 13:14:10 -0700

    Code review change for AB#16173

--- Apps/Admin/Tests/Functional/cypress/integration/ui/support.cy.js
+++ Apps/Admin/Tests/Functional/cypress/integration/ui/support.cy.js
@@ -108,7 +108,7 @@ describe("Support", () => {
         cy.get("[data-testid=user-banner-feedback-warning-message]").should(
             "be.visible"
         );
-        verifySingleSupportResult(String(), phnDuplicate);
+        verifySingleSupportResult("", phnDuplicate);
         cy.get("[data-testid=user-banner-feedback-warning-message]").within(
             () => {
                 cy.get("button").parent(".mud-alert-close").click();
