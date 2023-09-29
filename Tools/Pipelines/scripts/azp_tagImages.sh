bba13b221e8278e65848004e3d73a451e67cb4d4
Stephen Laws <stephen.laws@nomostech.com>
2023-09-29 11:59:48 -0700

    Update image tagging to have optional repo

--- Tools/Pipelines/scripts/azp_tagImages.sh
+++ Tools/Pipelines/scripts/azp_tagImages.sh
@@ -7,16 +7,24 @@ fi
 
 tag=$1
 
-oc tag $(ImageRepo)/webclient:${RELEASE_ARTIFACTS_WEBCLIENTIMAGE_BUILDNUMBER} webclient:$tag
-oc tag $(ImageRepo)/admin:${RELEASE_ARTIFACTS_ADMINBLAZORIMAGE_BUILDNUMBER} admin:$tag
-oc tag $(ImageRepo)/adminwebclient:${RELEASE_ARTIFACTS_ADMINWEBCLIENTIMAGE_BUILDNUMBER} webadmin:$tag
-oc tag $(ImageRepo)/clinicaldocument:${RELEASE_ARTIFACTS_CLINICALDOCUMENTSIMAGE_BUILDNUMBER} clinicaldoc:$tag
-oc tag $(ImageRepo)/encounter:${RELEASE_ARTIFACTS_ENCOUNTERSERVICEIMAGE_BUILDNUMBER} encounter:$tag
-oc tag $(ImageRepo)/gatewayapi:${RELEASE_ARTIFACTS_GATEWAYAPIIMAGE_BUILDNUMBER} gatewayapi:$tag
-oc tag $(ImageRepo)/hangfire:${RELEASE_ARTIFACTS_HANGFIREIMAGE_BUILDNUMBER} jobscheduler:$tag
-oc tag $(ImageRepo)/hgcdogs:${RELEASE_ARTIFACTS_HGCDOGSIMAGE_BUILDNUMBER} hgcdogs:$tag
-oc tag $(ImageRepo)/immunization:${RELEASE_ARTIFACTS_IMMSSERVICEIMAGE_BUILDNUMBER} immunization:$tag
-oc tag $(ImageRepo)/laboratory:${RELEASE_ARTIFACTS_LABORATORYSERVICEIMAGE_BUILDNUMBER} laboratory:$tag
-oc tag $(ImageRepo)/medication:${RELEASE_ARTIFACTS_MEDICATIONSERVICEIMAGE_BUILDNUMBER} medication:$tag
-oc tag $(ImageRepo)/offline:${RELEASE_ARTIFACTS_OFFLINEIMAGE_BUILDNUMBER} offline:$tag
-oc tag $(ImageRepo)/patient:${RELEASE_ARTIFACTS_PATIENTSERVICEIMAGE_BUILDNUMBER} patient:$tag
+if [ -z "$IMAGE_REPO" ]; then
+  echo "Image Repo is not specified tagging locally"
+  ImageRepo=""
+else
+  echo "Image Repo is specified tagging to $IMAGE_REPO"
+  ImageRepo="${IMAGE_REPO}/"
+fi
+
+oc tag ${ImageRepo}webclient:${RELEASE_ARTIFACTS_WEBCLIENTIMAGE_BUILDNUMBER} webclient:$tag
+oc tag ${ImageRepo}admin:${RELEASE_ARTIFACTS_ADMINBLAZORIMAGE_BUILDNUMBER} admin:$tag
+oc tag ${ImageRepo}adminwebclient:${RELEASE_ARTIFACTS_ADMINWEBCLIENTIMAGE_BUILDNUMBER} webadmin:$tag
+oc tag ${ImageRepo}clinicaldocument:${RELEASE_ARTIFACTS_CLINICALDOCUMENTSIMAGE_BUILDNUMBER} clinicaldoc:$tag
+oc tag ${ImageRepo}encounter:${RELEASE_ARTIFACTS_ENCOUNTERSERVICEIMAGE_BUILDNUMBER} encounter:$tag
+oc tag ${ImageRepo}gatewayapi:${RELEASE_ARTIFACTS_GATEWAYAPIIMAGE_BUILDNUMBER} gatewayapi:$tag
+oc tag ${ImageRepo}hangfire:${RELEASE_ARTIFACTS_HANGFIREIMAGE_BUILDNUMBER} jobscheduler:$tag
+oc tag ${ImageRepo}hgcdogs:${RELEASE_ARTIFACTS_HGCDOGSIMAGE_BUILDNUMBER} hgcdogs:$tag
+oc tag ${ImageRepo}immunization:${RELEASE_ARTIFACTS_IMMSSERVICEIMAGE_BUILDNUMBER} immunization:$tag
+oc tag ${ImageRepo}laboratory:${RELEASE_ARTIFACTS_LABORATORYSERVICEIMAGE_BUILDNUMBER} laboratory:$tag
+oc tag ${ImageRepo}medication:${RELEASE_ARTIFACTS_MEDICATIONSERVICEIMAGE_BUILDNUMBER} medication:$tag
+oc tag ${ImageRepo}offline:${RELEASE_ARTIFACTS_OFFLINEIMAGE_BUILDNUMBER} offline:$tag
+oc tag ${ImageRepo}patient:${RELEASE_ARTIFACTS_PATIENTSERVICEIMAGE_BUILDNUMBER} patient:$tag
