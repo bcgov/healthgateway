7feea8ae9cc0a7bb42998dea8991529387abb475
Stephen Laws <stephen.laws@nomostech.com>
2023-09-29 12:08:07 -0700

    Fix image tag names

--- Tools/Pipelines/scripts/azp_tagImages.sh
+++ Tools/Pipelines/scripts/azp_tagImages.sh
@@ -17,11 +17,11 @@ fi
 
 oc tag ${ImageRepo}webclient:${RELEASE_ARTIFACTS_WEBCLIENTIMAGE_BUILDNUMBER} webclient:$tag
 oc tag ${ImageRepo}admin:${RELEASE_ARTIFACTS_ADMINBLAZORIMAGE_BUILDNUMBER} admin:$tag
-oc tag ${ImageRepo}adminwebclient:${RELEASE_ARTIFACTS_ADMINWEBCLIENTIMAGE_BUILDNUMBER} webadmin:$tag
-oc tag ${ImageRepo}clinicaldocument:${RELEASE_ARTIFACTS_CLINICALDOCUMENTSIMAGE_BUILDNUMBER} clinicaldoc:$tag
+oc tag ${ImageRepo}adminwebclient:${RELEASE_ARTIFACTS_ADMINWEBCLIENTIMAGE_BUILDNUMBER} adminwebclient:$tag
+oc tag ${ImageRepo}clinicaldocument:${RELEASE_ARTIFACTS_CLINICALDOCUMENTSIMAGE_BUILDNUMBER} clinicaldocument:$tag
 oc tag ${ImageRepo}encounter:${RELEASE_ARTIFACTS_ENCOUNTERSERVICEIMAGE_BUILDNUMBER} encounter:$tag
 oc tag ${ImageRepo}gatewayapi:${RELEASE_ARTIFACTS_GATEWAYAPIIMAGE_BUILDNUMBER} gatewayapi:$tag
-oc tag ${ImageRepo}hangfire:${RELEASE_ARTIFACTS_HANGFIREIMAGE_BUILDNUMBER} jobscheduler:$tag
+oc tag ${ImageRepo}hangfire:${RELEASE_ARTIFACTS_HANGFIREIMAGE_BUILDNUMBER} hangfire:$tag
 oc tag ${ImageRepo}hgcdogs:${RELEASE_ARTIFACTS_HGCDOGSIMAGE_BUILDNUMBER} hgcdogs:$tag
 oc tag ${ImageRepo}immunization:${RELEASE_ARTIFACTS_IMMSSERVICEIMAGE_BUILDNUMBER} immunization:$tag
 oc tag ${ImageRepo}laboratory:${RELEASE_ARTIFACTS_LABORATORYSERVICEIMAGE_BUILDNUMBER} laboratory:$tag
