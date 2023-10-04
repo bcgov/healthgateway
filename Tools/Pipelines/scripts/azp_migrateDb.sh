e406c9dcb7700792fae8fe6ba1ce0fd939436cbe
Stephen Laws <stephen.laws@nomostech.com>
2023-10-03 17:09:05 -0700

    Use lack of additional parameters to trigger local Patroni lookup

--- Tools/Pipelines/scripts/azp_migrateDb.sh
+++ Tools/Pipelines/scripts/azp_migrateDb.sh
@@ -3,8 +3,11 @@
 if [ -z "$$1" ]; then
   echo "No Additional Parameters supplied for DB Connection"
   echo "If you're connecting to an Azure DB you'll want to add: sslmode=VerifyFull"
+  dbHost=${PATRONI_POSTGRES_MASTER_SERVICE_HOST}
   xtraParms=""
 else
+  echo "Will connect to Azure DB with additional parameters"
+  dbHost=${DB_HOST}  
   xtraParms=$1
   echo "Additional Parameters supplied for DB Connection ${xtraParms}"
 fi
@@ -13,5 +16,5 @@ pushd _Database/drop
 migrations=${RELEASE_ARTIFACTS__DATABASE_BUILDNUMBER}.efbundle
 echo "Running migrations from ${migrations}"
 chmod u+x $migrations
-./$migrations --connection "Host=${DB_HOST};Database=${DB_NAME};User ID=${DB_USER};Password=${DB_PASSWORD};Command Timeout=300;"
+./$migrations --connection "Host=${dbHost};Database=${DB_NAME};User ID=${DB_USER};Password=${DB_PASSWORD};Command Timeout=300;$xtraParms"
 popd
