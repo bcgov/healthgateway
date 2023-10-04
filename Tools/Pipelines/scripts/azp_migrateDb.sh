06b0acb5b3a86ac76d56d151a7d9d49f4e0e45dd
Stephen Laws <stephen.laws@nomostech.com>
2023-10-03 17:25:00 -0700

    Use environment to pass in local db

--- Tools/Pipelines/scripts/azp_migrateDb.sh
+++ Tools/Pipelines/scripts/azp_migrateDb.sh
@@ -3,11 +3,8 @@
 if [ -z "$$1" ]; then
   echo "No Additional Parameters supplied for DB Connection"
   echo "If you're connecting to an Azure DB you'll want to add: sslmode=VerifyFull"
-  dbHost=${PATRONI_POSTGRES_MASTER_SERVICE_HOST}
   xtraParms=""
 else
-  echo "Will connect to Azure DB with additional parameters"
-  dbHost=${DB_HOST}  
   xtraParms=$1
   echo "Additional Parameters supplied for DB Connection ${xtraParms}"
 fi
