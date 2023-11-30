6aa9d2bf0a85d71e1830ac7e8ca29f88e042bfad
BrianMaki <brian.jang.developer@outlook.com>
2023-11-29 18:52:55 -0800

    Unit tests for associate delegation for AB#16273.

--- Apps/GatewayApi/test/unit/Services.Test/DelegationServiceTests.cs
+++ Apps/GatewayApi/test/unit/Services.Test/DelegationServiceTests.cs
@@ -46,18 +46,33 @@ namespace HealthGateway.GatewayApiTests.Services.Test
     /// </summary>
     public class DelegationServiceTests
     {
+        private const string DelegationId = "a7673b82-16ee-4ce7-9467-a08bbd3217f7";
+        private const string DelegateHdid = "DEV4FPEGCXG2NB5K2USBL52S66SC3GOUHWRP3GTXR2BTY5HEC4YA";
+        private const string EncryptedDelegationId =
+            "CfDJ8LLY0gwTcqdJoMgQs9OOAXd7dzOHlGh2YP-gemIPfyfqR4_7Igrj4B85s2bvgYSnrCgOPFHP8C0pG2oMTfrU2qxyT0BdMMET_MbEii514O4HtalNmPCVHl1dDLFyruuBA3RwRyC70uoVxZtpavV4tRRGd9lAq5VNwVWNez4kuvfC";
+        private const string ProfileHdid = "GO4DOSMRJ7MFKPPADDZ3FK2MOJ45SFKONJWR67XNLMZQFNEHDKDA";
+        private const string ResourceOwnerHdid = "P6FFO433A5WPMVTGM7T4ZVWBKCSVNAYGTWTU3J2LWMGUMERKI72A";
+
         private const string Hdid = "P6FFO433A5WPMVTGM7T4ZVWBKCSVNAYGTWTU3J2LWMGUMERKI72A";
         private const string Phn = "9735353315";
         private const string InviteKey = "ABCDEF";
+
         private const string InvalidEmailErrorMessage = "Email address is not valid";
         private const string InvalidDataSourceErrorMessage = "DataSources must have at least one item.";
         private const string InvalidNicknameErrorMessage = "Maximum length for Nickname has been exceeded.";
         private const string InvalidExpiryDateErrorMesage = "Invalid date";
+        private const string EncryptedDelegationIdErrorMessage = "Encrypted Delegation Id must not be empty";
+        private const string DelegationAlreadyAssociatedErrorMessage = "Delegation has already been associated with another profile.";
+        private const string DelegationSelfAssociationErrorMessage = "The delegation cannot be associated with oneself.";
+        private const string SharingLinkExpirationErrorMesage = "Sharing link has expired.";
 
         private const string ValidEmail = "delegator@gateway.ca";
         private const string InvalidEmail = "delegator@gateway";
         private const string ValidNickname = "12345678901234567890"; // 20 characters
         private const string InvalidNickname = "123456789012345678901"; // 21 characters
+
+        private const string ExpiryHours = "48";
+
         private static readonly HashSet<DataSource> ValidDataSources =
         [
             DataSource.Immunization,
@@ -81,6 +96,91 @@ namespace HealthGateway.GatewayApiTests.Services.Test
         }
 
         /// <summary>
+        /// Should associate a delegation.
+        /// </summary>
+        /// <param name="resourceOwnerHdid">The delegation's resource owner hdid.</param>
+        /// <param name="delegateHdid">The delegate's hdid.</param>
+        /// <param name="profileHdid">The profile hdid stored in Delegation.</param>
+        /// <param name="secondsToExpiration">The number of seconds to expiration.</param>
+        /// <param name="success">The value indicates whether the test should succeed or not.</param>
+        /// <param name="errorMessage">The error message to display when success is false.</param>
+        [Theory]
+        [InlineData(ResourceOwnerHdid, DelegateHdid, null, 0, true, null)] // 0 Hours
+        [InlineData(ResourceOwnerHdid, DelegateHdid, null, -172800, false, SharingLinkExpirationErrorMesage)] // 48 hours
+        [InlineData(ResourceOwnerHdid, DelegateHdid, null, -172801, false, SharingLinkExpirationErrorMesage)] // 48 hours plus 1 second
+        [InlineData(ResourceOwnerHdid, ResourceOwnerHdid, null, 0, false, DelegationSelfAssociationErrorMessage)]
+        [InlineData(ResourceOwnerHdid, DelegateHdid, ProfileHdid, 0, false, DelegationAlreadyAssociatedErrorMessage)]
+        public async Task AssociateDelegation(string resourceOwnerHdid, string delegateHdid, string? profileHdid, int secondsToExpiration, bool success, string? errorMessage)
+        {
+            // Arrange
+            TimeZoneInfo localTimezone = DateFormatter.GetLocalTimeZone(GetConfiguration());
+            Mock<IDelegationDelegate> delegationDelegate = new();
+            PatientDetails patient = GetPatient();
+
+            int expiryHours = int.Parse(ExpiryHours);
+            DateTime createdDateTime = DateTime.UtcNow.AddSeconds(secondsToExpiration);
+
+            Delegation delegation = new()
+            {
+                Id = Guid.NewGuid(),
+                ResourceOwnerHdid = resourceOwnerHdid,
+                ProfileHdid = profileHdid,
+                CreatedDateTime = createdDateTime,
+
+                ResourceOwnerIdentifier = $"{patient.PreferredName.GivenName} {patient.PreferredName.Surname[0]}",
+                Nickname = ValidNickname,
+                ExpiryDate = DateOnly.FromDateTime(TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, localTimezone)).AddDays(30),
+                DataSources = ValidDataSources,
+            };
+
+            IDelegationService delegationService = GetDelegationService(delegationDelegate, delegation);
+
+            if (success)
+            {
+                // Act
+                await delegationService.AssociateDelegationAsync(delegateHdid, EncryptedDelegationId);
+
+                // Verify
+                delegationDelegate.Verify(
+                    d => d.UpdateDelegationAsync(
+                        It.Is<Delegation>(di => AssertDelegation(delegation, di)),
+                        true));
+            }
+            else
+            {
+                // Act
+                async Task Actual()
+                {
+                    await delegationService.AssociateDelegationAsync(delegateHdid, EncryptedDelegationId);
+                }
+
+                // Verify
+                ProblemDetailsException exception = await Assert.ThrowsAsync<ProblemDetailsException>(Actual);
+                Assert.Equal(errorMessage, exception.ProblemDetails!.Detail);
+            }
+        }
+
+        /// <summary>
+        /// Should throw problem details exception when no delegation is found.
+        /// </summary>
+        [Fact]
+        public async Task AssociateDelegationThrowsDelegationNotFound()
+        {
+            // Arrange
+            IDelegationService delegationService = GetDelegationService(); // will mock null delegation response
+
+            // Act
+            async Task Actual()
+            {
+                await delegationService.AssociateDelegationAsync(DelegateHdid, EncryptedDelegationId);
+            }
+
+            // Verify
+            ProblemDetailsException exception = await Assert.ThrowsAsync<ProblemDetailsException>(Actual);
+            Assert.Equal(ErrorMessages.DelegationNotFound, exception.ProblemDetails!.Detail);
+        }
+
+        /// <summary>
         /// Should create delegation.
         /// </summary>
         /// <param name="email">Email to validate.</param>
@@ -94,11 +194,10 @@ namespace HealthGateway.GatewayApiTests.Services.Test
         [MemberData(nameof(TestCases))]
         public async Task CreateDelegation(string email, string nickname, int daysToAdd, HashSet<DataSource> dataSources, bool success, string errorMessage)
         {
+            // Arrange
             TimeZoneInfo localTimezone = DateFormatter.GetLocalTimeZone(GetConfiguration());
             Mock<IDelegationDelegate> delegationDelegate = new();
-
-            // Setup
-            PatientDetails patient = GetPatient(localTimezone);
+            PatientDetails patient = GetPatient();
 
             Delegation expectedDelegation = new()
             {
@@ -117,7 +216,7 @@ namespace HealthGateway.GatewayApiTests.Services.Test
                 DataSources = dataSources,
             };
 
-            IDelegationService delegationService = GetDelegateService(patient, GetHash(), delegationDelegate);
+            IDelegationService delegationService = GetDelegationService(patient, GetHash(), delegationDelegate);
 
             if (success)
             {
@@ -154,7 +253,28 @@ namespace HealthGateway.GatewayApiTests.Services.Test
             return true;
         }
 
-        private static IDelegationService GetDelegateService(PatientDetails patient, IHash hash, IMock<IDelegationDelegate> delegationDelegate)
+        private static IDelegationService GetDelegationService(Mock<IDelegationDelegate> delegationDelegate = null, Delegation delegation = null)
+        {
+            delegationDelegate = delegationDelegate ?? new Mock<IDelegationDelegate>();
+            delegationDelegate.Setup(d => d.GetDelegationAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>())).ReturnsAsync(delegation);
+
+            Mock<IDataProtectionProvider> dataProtectionProvider = new();
+            Mock<IDataProtector> dataProtector = new();
+            dataProtector.Setup(p => p.Unprotect(It.IsAny<byte[]>())).Returns(Encoding.UTF8.GetBytes(DelegationId));
+            dataProtectionProvider.Setup(p => p.CreateProtector(It.IsAny<string>())).Returns(dataProtector.Object);
+
+            return new DelegationService(
+                GetConfiguration(),
+                Mapper,
+                new Mock<ILogger<DelegationService>>().Object,
+                dataProtectionProvider.Object,
+                delegationDelegate.Object,
+                new Mock<IHashDelegate>().Object,
+                new Mock<IEmailQueueService>().Object,
+                new Mock<IPatientDetailsService>().Object);
+        }
+
+        private static IDelegationService GetDelegationService(PatientDetails patient, IHash hash, Mock<IDelegationDelegate> delegationDelegate)
         {
             Mock<IHashDelegate> hashDelegate = new();
             hashDelegate.Setup(d => d.Hash(It.IsAny<string>())).Returns(hash);
@@ -188,7 +308,7 @@ namespace HealthGateway.GatewayApiTests.Services.Test
             {
                 { "TimeZone:UnixTimeZoneId", "America/Vancouver" },
                 { "TimeZone:WindowsTimeZoneId", "Pacific Standard Time" },
-                { "DelegationInvitation:ExpiryHours", "48" },
+                { "DelegationInvitation:ExpiryHours", ExpiryHours },
             };
 
             return new ConfigurationBuilder()
@@ -217,7 +337,7 @@ namespace HealthGateway.GatewayApiTests.Services.Test
             };
         }
 
-        private static PatientDetails GetPatient(TimeZoneInfo localTimezone)
+        private static PatientDetails GetPatient()
         {
             return new()
             {
@@ -225,7 +345,7 @@ namespace HealthGateway.GatewayApiTests.Services.Test
                 HdId = Hdid,
                 CommonName = new() { GivenName = "Bonny", Surname = "PROTERVITY" },
                 LegalName = new() { GivenName = "Bonny", Surname = "PROTERVITY" },
-                Birthdate = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, localTimezone).AddYears(-30),
+                Birthdate = DateTime.UtcNow.AddYears(-30),
                 Gender = "Female",
             };
         }
