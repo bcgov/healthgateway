794caa7750331d9233196599ea0e64acf8a051fc
Declan Coughlan <deccough@gmail.com>
2023-12-18 16:20:24 -0800

    Add coverage for error scenario for AB#16589

---
+++ Apps/AccountDataAccess/test/unit/Strategy/PatientQueryFactoryTests.cs
@@ -0,0 +1,19 @@
+﻿namespace AccountDataAccessTest.Strategy
+{
+    using HealthGateway.AccountDataAccess.Patient.Strategy;
+    using Moq;
+    using Xunit;
+
+    public class PatientQueryFactoryTests
+    {
+        [Fact]
+        public void ShouldThrowIfStrategyTypeIsNull()
+        {
+            PatientQueryFactory patientQueryFactory = new(new Mock<IServiceProvider>().Object);
+
+            string strategyType = "Not.A.Strategy.Type";
+
+            Assert.Throws<ArgumentException>(() => patientQueryFactory.GetPatientQueryStrategy(strategyType));
+        }
+    }
+}
