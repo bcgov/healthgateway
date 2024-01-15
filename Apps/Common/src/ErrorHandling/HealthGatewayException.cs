8e0dd8095fa13d7ba6b73534fcafa4d4adb6c666
Declan Coughlan <deccough@gmail.com>
2024-01-15 14:41:34 -0800

    Add Health Gateway exception for AB#16607

---
+++ Apps/Common/src/ErrorHandling/HealthGatewayException.cs
@@ -0,0 +1,58 @@
+﻿// -------------------------------------------------------------------------
+//  Copyright © 2019 Province of British Columbia
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------
+namespace HealthGateway.Common.ErrorHandling
+{
+    using System;
+    using System.Net;
+
+    /// <summary>
+    /// Exception class for Health Gateway.
+    /// </summary>
+    public class HealthGatewayException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HealthGatewayException"/> class.
+        /// </summary>
+        /// <param name="message">Error message detailing the failure in question.</param>
+        public HealthGatewayException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HealthGatewayException"/> class.
+        /// </summary>
+        /// <param name="message">Error message details the failure in question.</param>
+        /// <param name="statusCode">Http status code to return.</param>
+        /// <param name="errorCode">Concise code to indicate the cause of status code.</param>
+        public HealthGatewayException(string message, HttpStatusCode statusCode, string? errorCode)
+            : base(message)
+        {
+            this.StatusCode = statusCode;
+            this.ErrorCode = errorCode;
+        }
+
+        /// <summary>
+        /// Gets or sets the error codes.
+        /// </summary>
+        public string? ErrorCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the http status code.
+        /// </summary>
+        public HttpStatusCode StatusCode { get; set; } = HttpStatusCode.InternalServerError;
+    }
+}
