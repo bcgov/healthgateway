7385a772130ee814a1db05ec2d854b450d3a8d37
BrianMaki <brian.jang.developer@outlook.com>
2024-01-22 18:06:14 -0800

    Re-enable auth tests for AB#16603.

--- Tools/Pipelines/scripts/azp_runParallelFunctionalTests.sh
+++ Tools/Pipelines/scripts/azp_runParallelFunctionalTests.sh
@@ -37,8 +37,8 @@ TZ=America/Vancouver npx cypress run \
   --record \
   --key $CYPRESS_KEY \
   --parallel \
-  --ci-build-id "$buildId" \
-  --group "$buildId" \
+  --ci-build-id "$buildId-Parallel" \
+  --group "$buildId-Parallel" \
   --tag "$tags" \
   --spec "cypress/integration/ui/**/*,cypress/integration/e2e/**/!(auth.js)" \
   --headless \
