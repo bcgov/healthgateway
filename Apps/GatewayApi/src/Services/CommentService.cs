14ba74242d43da970fbca1d36ce22d39d7d33e19
Mike Lyttle <mikelyttle@gmail.com>
2024-03-28 10:59:09 -0700

    Add alert to access beta site AB#16691 AB#16692 AB#16693 (#6097)
    * Add beta URL to appsettings and config models AB#16691
    
    * Refactor outdated collection handling in UserProfileModel
    
    * Move user preference population from controller to service
    
    * Simplify GetUserPreferencesAsync
    
    * Move UserProfileModel to GatewayApi
    
    * Update DB delegate to return beta feature codes for profiles
    
    * Populate BetaFeatures in UserProfileModel
    
    * Fix profile data missing after accepting ToS update
    
    * Add alert on home page to access beta site AB#16693
    
    * Fix link with wrong styling

--- Apps/GatewayApi/src/Services/CommentService.cs
+++ Apps/GatewayApi/src/Services/CommentService.cs
@@ -68,7 +68,7 @@ namespace HealthGateway.GatewayApi.Services
                 return RequestResultFactory.Error<UserComment>(ErrorType.InvalidState, validationResult.Errors);
             }
 
-            UserProfile? profile = await this.profileDelegate.GetUserProfileAsync(userComment.UserProfileId, ct);
+            UserProfile? profile = await this.profileDelegate.GetUserProfileAsync(userComment.UserProfileId, ct: ct);
             string? key = profile?.EncryptionKey;
             if (key == null)
             {
@@ -90,7 +90,7 @@ namespace HealthGateway.GatewayApi.Services
         /// <inheritdoc/>
         public async Task<RequestResult<IEnumerable<UserComment>>> GetEntryCommentsAsync(string hdId, string parentEntryId, CancellationToken ct = default)
         {
-            UserProfile? profile = await this.profileDelegate.GetUserProfileAsync(hdId, ct);
+            UserProfile? profile = await this.profileDelegate.GetUserProfileAsync(hdId, ct: ct);
             string? key = profile?.EncryptionKey;
 
             // Check that the key has been set
@@ -117,7 +117,7 @@ namespace HealthGateway.GatewayApi.Services
         /// <inheritdoc/>
         public async Task<RequestResult<IDictionary<string, IEnumerable<UserComment>>>> GetProfileCommentsAsync(string hdId, CancellationToken ct = default)
         {
-            UserProfile? profile = await this.profileDelegate.GetUserProfileAsync(hdId, ct);
+            UserProfile? profile = await this.profileDelegate.GetUserProfileAsync(hdId, ct: ct);
             string? key = profile?.EncryptionKey;
 
             // Check that the key has been set
@@ -153,7 +153,7 @@ namespace HealthGateway.GatewayApi.Services
                 return RequestResultFactory.Error<UserComment>(ErrorType.InvalidState, validationResult.Errors);
             }
 
-            UserProfile? profile = await this.profileDelegate.GetUserProfileAsync(userComment.UserProfileId, ct);
+            UserProfile? profile = await this.profileDelegate.GetUserProfileAsync(userComment.UserProfileId, ct: ct);
             string? key = profile?.EncryptionKey;
             if (key == null)
             {
@@ -182,7 +182,7 @@ namespace HealthGateway.GatewayApi.Services
                 return RequestResultFactory.Error<UserComment>(ErrorType.InvalidState, validationResult.Errors);
             }
 
-            UserProfile? profile = await this.profileDelegate.GetUserProfileAsync(userComment.UserProfileId, ct);
+            UserProfile? profile = await this.profileDelegate.GetUserProfileAsync(userComment.UserProfileId, ct: ct);
             string? key = profile?.EncryptionKey;
             if (key == null)
             {
