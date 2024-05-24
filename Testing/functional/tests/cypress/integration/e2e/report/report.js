d4ac8b7e858d181e4c3a2e5a8df3c3546da88cc3
BrianMaki <brian.jang.developer@outlook.com>
2024-05-24 13:52:31 -0700

    Fix flakiness in report functional tests for AB#16740.

--- Testing/functional/tests/cypress/integration/e2e/report/report.js
+++ Testing/functional/tests/cypress/integration/e2e/report/report.js
@@ -1,9 +1,28 @@
-const { AuthMethod } = require("../../../support/constants");
+import { AuthMethod } from "../../../support/constants";
+
+const defaultTimeout = 60000;
+const hdid = "P6FFO433A5WPMVTGM7T4ZVWBKCSVNAYGTWTU3J2LWMGUMERKI72A";
+
+function setupReportAliases() {
+    cy.log("Setting up standard aliases.");
+
+    cy.intercept("GET", `**/Encounter/${hdid}`).as("getHealthVisits");
+    cy.intercept("GET", `**/MedicationStatement/*`).as("getMedications");
+    cy.intercept("GET", "**/Immunization*").as("getImmunizations");
+    cy.intercept("GET", "**/Laboratory/Covid19Orders*").as("getCovid19Tests");
+    cy.intercept("GET", "**/MedicationRequest/*").as(
+        "getSpecialAuthorityReport"
+    );
+    cy.intercept("GET", "**/Laboratory/LaboratoryOrders*").as("getLabResults");
+    cy.intercept("GET", "**/Encounter/HospitalVisit/*").as("getHospitalVisits");
+    cy.intercept("GET", "**/Note/*").as("getNotes");
+}
 
 describe("Reports", () => {
     let sensitiveDocText =
         "The file that you are downloading contains personal information. If you are on a public computer, please ensure that the file is deleted before you log off.";
     beforeEach(() => {
+        setupReportAliases();
         cy.setupDownloads();
         cy.configureSettings({
             datasets: [
@@ -63,6 +82,7 @@ describe("Reports", () => {
         cy.viewport(1440, 600);
 
         cy.vSelect("[data-testid=report-type]", "Medications");
+        cy.wait("@getMedications", { timeout: defaultTimeout });
 
         cy.get("[data-testid=export-record-btn]").should(
             "be.enabled",
@@ -72,6 +92,8 @@ describe("Reports", () => {
 
     it("Validate Medication Report", () => {
         cy.vSelect("[data-testid=report-type]", "Medications");
+        cy.wait("@getMedications", { timeout: defaultTimeout });
+
         cy.get("[data-testid=medication-history-report-table]").should(
             "be.visible"
         );
@@ -100,6 +122,7 @@ describe("Reports", () => {
 
     it("Validate MSP Visits Report", () => {
         cy.vSelect("[data-testid=report-type]", "Health Visits");
+        cy.wait("@getHealthVisits", { timeout: defaultTimeout });
 
         cy.get("[data-testid=msp-visits-report-table]").should("be.visible");
 
@@ -129,6 +152,7 @@ describe("Reports", () => {
 
     it("Validate COVID-19 Report", () => {
         cy.vSelect("[data-testid=report-type]", "COVID‑19 Tests");
+        cy.wait("@getCovid19Tests", { timeout: defaultTimeout });
 
         cy.get("[data-testid=covid19-report-table]").should("be.visible");
         cy.get("[data-testid=covid19DateItem]", { timeout: 60000 })
@@ -159,6 +183,7 @@ describe("Reports", () => {
 
     it("Validate Immunization Report", () => {
         cy.vSelect("[data-testid=report-type]", "Immunizations");
+        cy.wait("@getImmunizations", { timeout: defaultTimeout });
 
         cy.get("[data-testid=immunization-history-report-table]").should(
             "be.visible"
@@ -193,6 +218,7 @@ describe("Reports", () => {
 
     it("Validate Special Authority Report", () => {
         cy.vSelect("[data-testid=report-type]", "Special Authority");
+        cy.wait("@getSpecialAuthorityReport", { timeout: defaultTimeout });
 
         cy.get("[data-testid=medication-request-report-table]").should(
             "be.visible"
@@ -224,6 +250,7 @@ describe("Reports", () => {
 
     it("Validate Laboratory Report", () => {
         cy.vSelect("[data-testid=report-type]", "Lab Results");
+        cy.wait("@getLabResults", { timeout: defaultTimeout });
 
         cy.get("[data-testid=laboratory-report-table]").should("be.visible");
         cy.get("[data-testid=labResultDateItem]", { timeout: 60000 })
@@ -256,6 +283,7 @@ describe("Reports", () => {
 
     it("Validate Hospital Visits Report", () => {
         cy.vSelect("[data-testid=report-type]", "Hospital Visits");
+        cy.wait("@getHospitalVisits", { timeout: defaultTimeout });
 
         cy.get("[data-testid=hospital-visit-report-table]").should(
             "be.visible"
@@ -293,6 +321,7 @@ describe("Reports - Notes", () => {
     let sensitiveDocText =
         "The file that you are downloading contains personal information. If you are on a public computer, please ensure that the file is deleted before you log off.";
     beforeEach(() => {
+        setupReportAliases();
         cy.setupDownloads();
         cy.configureSettings({
             datasets: [
@@ -312,6 +341,7 @@ describe("Reports - Notes", () => {
 
     it("Validate Notes Report", () => {
         cy.vSelect("[data-testid=report-type]", "My Notes");
+        cy.wait("@getNotes", { timeout: defaultTimeout });
 
         cy.get("[data-testid=report-sample]").should("be.visible");
 
