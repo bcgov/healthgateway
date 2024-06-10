a1201ec201dfdd10786e55a6ddfd6d2da2d74891
Brian Jang <58790456+BrianMaki@users.noreply.github.com>
2024-06-10 14:43:17 -0700

    Create Dockerfile and Pipeline for Landing for AB#16752 and AB16753 (#6195)
    * Create Landing docker image and update azure pipeline for AB#16752.
    
    * Add new image to hgtools for AB#16752
    
    * Add tag for landing for AB#16752
    
    * Enable Docker image to work with local APIs for AB#16752.
    
    * Add clean to npm development build for AB#16752
    
    * Modify vite config to work with IDE for AB#16752.
    
    * Update pipeline to support web clients in any relative folder
    
    * Move build context variable into hg.yaml
    
    * Replace pipeline references to "Landing" with more-generic "client"
    
    * Fix typos in pipeline task names
    
    ---------
    
    Co-authored-by: Stephen Laws <stephen.laws@nomostech.com>
    Co-authored-by: Mike Lyttle <mikelyttle@gmail.com>

--- Apps/Landing/src/vite.config.ts
+++ Apps/Landing/src/vite.config.ts
@@ -1,8 +1,27 @@
 import { fileURLToPath, URL } from "node:url";
 
 import vue from "@vitejs/plugin-vue";
+import dotenv from "dotenv";
 import { defineConfig } from "vite";
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
+// Load environment variables from the appropriate .env file
+dotenv.config({
+    path: fileURLToPath(
+        new URL(
+            process.env.NODE_ENV === "production"
+                ? "./.env.production"
+                : "./.env.development",
+            import.meta.url
+        )
+    ),
+});
+
+// Prepare environment variables for Vite's define option
+const envVariables = Object.keys(process.env).reduce((prev, curr) => {
+    prev[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+    return prev;
+}, {});
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,7 +37,7 @@ export default defineConfig({
     optimizeDeps: {
         exclude: ["vuetify"],
     },
-    define: { "process.env": {} },
+    define: envVariables, // Inject environment variables
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
