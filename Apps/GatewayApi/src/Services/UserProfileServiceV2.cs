ec4db660d512ecd015ce9a02e7676405b09b0369
BrianMaki <brian.jang.developer@outlook.com>
2024-08-01 11:44:35 -0700

    Use primary constructor for UserProfileServiceV2 for AB#16811

--- Apps/GatewayApi/src/Services/UserProfileServiceV2.cs
+++ Apps/GatewayApi/src/Services/UserProfileServiceV2.cs
@@ -35,75 +35,46 @@ namespace HealthGateway.GatewayApi.Services
     using Microsoft.Extensions.Logging;
 
     /// <inheritdoc/>
-    public class UserProfileServiceV2 : IUserProfileServiceV2
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UserProfileServiceV2"/> class.
+    /// </summary>
+    /// <param name="logger">The injected logger.</param>
+    /// <param name="patientDetailsService">The injected patient details service.</param>
+    /// <param name="emailQueueService">The injected service to queue emails.</param>
+    /// <param name="userProfileDelegate">The injected user profile database delegate.</param>
+    /// <param name="userPreferenceService">The injected user preference service.</param>
+    /// <param name="legalAgreementService">The injected legal agreement service.</param>
+    /// <param name="messageVerificationDelegate">The injected message verification delegate.</param>
+    /// <param name="configuration">The injected configuration.</param>
+    /// <param name="mappingService">The injected mapping service.</param>
+    /// <param name="authenticationDelegate">The injected authentication delegate.</param>
+    /// <param name="applicationSettingsService">The injected application settings service.</param>
+    /// <param name="patientRepository">The injected patient repository.</param>
+    public class UserProfileServiceV2(
+        ILogger<UserProfileServiceV2> logger,
+        IPatientDetailsService patientDetailsService,
+        IEmailQueueService emailQueueService,
+        IUserProfileDelegate userProfileDelegate,
+        IUserPreferenceServiceV2 userPreferenceService,
+        ILegalAgreementServiceV2 legalAgreementService,
+        IMessagingVerificationDelegate messageVerificationDelegate,
+        IConfiguration configuration,
+        IGatewayApiMappingService mappingService,
+        IAuthenticationDelegate authenticationDelegate,
+        IApplicationSettingsService applicationSettingsService,
+        IPatientRepository patientRepository) : IUserProfileServiceV2
     {
         private const string UserProfileHistoryRecordLimitKey = "UserProfileHistoryRecordLimit";
         private const string WebClientConfigSection = "WebClient";
-        private readonly EmailTemplateConfig emailTemplateConfig;
-        private readonly int userProfileHistoryRecordLimit;
-
-        private readonly ILogger<UserProfileServiceV2> logger;
-        private readonly IPatientDetailsService patientDetailsService;
-        private readonly IEmailQueueService emailQueueService;
-        private readonly IUserProfileDelegate userProfileDelegate;
-        private readonly IUserPreferenceServiceV2 userPreferenceService;
-        private readonly ILegalAgreementServiceV2 legalAgreementService;
-        private readonly IMessagingVerificationDelegate messageVerificationDelegate;
-        private readonly IGatewayApiMappingService mappingService;
-        private readonly IAuthenticationDelegate authenticationDelegate;
-        private readonly IApplicationSettingsService applicationSettingsService;
-        private readonly IPatientRepository patientRepository;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="UserProfileServiceV2"/> class.
-        /// </summary>
-        /// <param name="logger">The injected logger.</param>
-        /// <param name="patientDetailsService">The injected patient details service.</param>
-        /// <param name="emailQueueService">The injected service to queue emails.</param>
-        /// <param name="userProfileDelegate">The injected user profile database delegate.</param>
-        /// <param name="userPreferenceService">The injected user preference service.</param>
-        /// <param name="legalAgreementService">The injected legal agreement service.</param>
-        /// <param name="messageVerificationDelegate">The injected message verification delegate.</param>
-        /// <param name="configuration">The injected configuration.</param>
-        /// <param name="mappingService">The injected mapping service.</param>
-        /// <param name="authenticationDelegate">The injected authentication delegate.</param>
-        /// <param name="applicationSettingsService">The injected application settings service.</param>
-        /// <param name="patientRepository">The injected patient repository.</param>
-        public UserProfileServiceV2(
-            ILogger<UserProfileServiceV2> logger,
-            IPatientDetailsService patientDetailsService,
-            IEmailQueueService emailQueueService,
-            IUserProfileDelegate userProfileDelegate,
-            IUserPreferenceServiceV2 userPreferenceService,
-            ILegalAgreementServiceV2 legalAgreementService,
-            IMessagingVerificationDelegate messageVerificationDelegate,
-            IConfiguration configuration,
-            IGatewayApiMappingService mappingService,
-            IAuthenticationDelegate authenticationDelegate,
-            IApplicationSettingsService applicationSettingsService,
-            IPatientRepository patientRepository)
-        {
-            this.logger = logger;
-            this.patientDetailsService = patientDetailsService;
-            this.emailQueueService = emailQueueService;
-            this.userProfileDelegate = userProfileDelegate;
-            this.userPreferenceService = userPreferenceService;
-            this.legalAgreementService = legalAgreementService;
-            this.messageVerificationDelegate = messageVerificationDelegate;
-            this.mappingService = mappingService;
-            this.authenticationDelegate = authenticationDelegate;
-            this.applicationSettingsService = applicationSettingsService;
-            this.patientRepository = patientRepository;
-            this.emailTemplateConfig = configuration.GetSection(EmailTemplateConfig.ConfigurationSectionKey).Get<EmailTemplateConfig>() ?? new();
-            this.userProfileHistoryRecordLimit = configuration.GetSection(WebClientConfigSection).GetValue(UserProfileHistoryRecordLimitKey, 4);
-        }
+        private readonly EmailTemplateConfig emailTemplateConfig = configuration.GetSection(EmailTemplateConfig.ConfigurationSectionKey).Get<EmailTemplateConfig>() ?? new();
+        private readonly int userProfileHistoryRecordLimit = configuration.GetSection(WebClientConfigSection).GetValue(UserProfileHistoryRecordLimitKey, 4);
 
         /// <inheritdoc/>
         public async Task<UserProfileModel> GetUserProfileAsync(string hdid, DateTime jwtAuthTime, CancellationToken ct = default)
         {
-            this.logger.LogTrace("Getting user profile... {Hdid}", hdid);
-            UserProfile? userProfile = await this.userProfileDelegate.GetUserProfileAsync(hdid, true, ct);
-            this.logger.LogDebug("Finished getting user profile...{Hdid}", hdid);
+            logger.LogTrace("Getting user profile... {Hdid}", hdid);
+            UserProfile? userProfile = await userProfileDelegate.GetUserProfileAsync(hdid, true, ct);
+            logger.LogDebug("Finished getting user profile...{Hdid}", hdid);
 
             if (userProfile == null)
             {
@@ -113,29 +84,29 @@ namespace HealthGateway.GatewayApi.Services
             DateTime previousLastLogin = userProfile.LastLoginDateTime;
             if (DateTime.Compare(previousLastLogin, jwtAuthTime) != 0)
             {
-                this.logger.LogTrace("Updating user last login and year of birth... {Hdid}", hdid);
+                logger.LogTrace("Updating user last login and year of birth... {Hdid}", hdid);
                 userProfile.LastLoginDateTime = jwtAuthTime;
-                userProfile.LastLoginClientCode = this.authenticationDelegate.FetchAuthenticatedUserClientType();
+                userProfile.LastLoginClientCode = authenticationDelegate.FetchAuthenticatedUserClientType();
 
                 // Update user year of birth.
-                PatientDetails patient = await this.patientDetailsService.GetPatientAsync(hdid, ct: ct);
+                PatientDetails patient = await patientDetailsService.GetPatientAsync(hdid, ct: ct);
                 userProfile.YearOfBirth = patient.Birthdate.Year;
 
                 // Try to update user profile with last login time; ignore any failures
-                await this.userProfileDelegate.UpdateAsync(userProfile, ct: ct);
+                await userProfileDelegate.UpdateAsync(userProfile, ct: ct);
 
-                this.logger.LogDebug("Finished updating user last login and year of birth... {Hdid}", hdid);
+                logger.LogDebug("Finished updating user last login and year of birth... {Hdid}", hdid);
             }
 
-            IList<UserProfileHistory> userProfileHistoryList = await this.userProfileDelegate.GetUserProfileHistoryListAsync(hdid, this.userProfileHistoryRecordLimit, ct);
+            IList<UserProfileHistory> userProfileHistoryList = await userProfileDelegate.GetUserProfileHistoryListAsync(hdid, this.userProfileHistoryRecordLimit, ct);
 
             string? emailAddress = !string.IsNullOrEmpty(userProfile.Email)
                 ? userProfile.Email
-                : (await this.messageVerificationDelegate.GetLastForUserAsync(hdid, MessagingVerificationType.Email, ct))?.Email?.To;
+                : (await messageVerificationDelegate.GetLastForUserAsync(hdid, MessagingVerificationType.Email, ct))?.Email?.To;
 
             string? smsNumber = !string.IsNullOrEmpty(userProfile.SmsNumber)
                 ? userProfile.SmsNumber
-                : (await this.messageVerificationDelegate.GetLastForUserAsync(hdid, MessagingVerificationType.Sms, ct))?.SmsNumber;
+                : (await messageVerificationDelegate.GetLastForUserAsync(hdid, MessagingVerificationType.Sms, ct))?.SmsNumber;
 
             UserProfileModel userProfileModel = await this.BuildUserProfileModelAsync(userProfile, userProfileHistoryList, emailAddress, smsNumber, ct);
 
@@ -145,20 +116,20 @@ namespace HealthGateway.GatewayApi.Services
         /// <inheritdoc/>
         public async Task CloseUserProfileAsync(string hdid, CancellationToken ct = default)
         {
-            this.logger.LogTrace("Closing user profile... {Hdid}", hdid);
+            logger.LogTrace("Closing user profile... {Hdid}", hdid);
 
-            UserProfile userProfile = await this.userProfileDelegate.GetUserProfileAsync(hdid, ct: ct) ?? throw new NotFoundException(ErrorMessages.UserProfileNotFound);
+            UserProfile userProfile = await userProfileDelegate.GetUserProfileAsync(hdid, ct: ct) ?? throw new NotFoundException(ErrorMessages.UserProfileNotFound);
 
             if (userProfile.ClosedDateTime != null)
             {
-                this.logger.LogTrace("Profile already closed");
+                logger.LogTrace("Profile already closed");
                 return;
             }
 
             // Mark profile for deletion
             userProfile.ClosedDateTime = DateTime.UtcNow;
-            userProfile.IdentityManagementId = new(this.authenticationDelegate.FetchAuthenticatedUserId());
-            DbResult<UserProfile> dbResult = await this.userProfileDelegate.UpdateAsync(userProfile, ct: ct);
+            userProfile.IdentityManagementId = new(authenticationDelegate.FetchAuthenticatedUserId());
+            DbResult<UserProfile> dbResult = await userProfileDelegate.UpdateAsync(userProfile, ct: ct);
             if (dbResult.Status != DbStatusCode.Updated)
             {
                 throw new DatabaseException(dbResult.Message);
@@ -170,19 +141,19 @@ namespace HealthGateway.GatewayApi.Services
         /// <inheritdoc/>
         public async Task RecoverUserProfileAsync(string hdid, CancellationToken ct = default)
         {
-            this.logger.LogTrace("Recovering user profile... {Hdid}", hdid);
-            UserProfile userProfile = await this.userProfileDelegate.GetUserProfileAsync(hdid, ct: ct) ?? throw new NotFoundException(ErrorMessages.UserProfileNotFound);
+            logger.LogTrace("Recovering user profile... {Hdid}", hdid);
+            UserProfile userProfile = await userProfileDelegate.GetUserProfileAsync(hdid, ct: ct) ?? throw new NotFoundException(ErrorMessages.UserProfileNotFound);
 
             if (userProfile.ClosedDateTime == null)
             {
-                this.logger.LogTrace("Profile already is active, recover not needed");
+                logger.LogTrace("Profile already is active, recover not needed");
                 return;
             }
 
             // Unmark profile for deletion
             userProfile.ClosedDateTime = null;
             userProfile.IdentityManagementId = null;
-            DbResult<UserProfile> dbResult = await this.userProfileDelegate.UpdateAsync(userProfile, true, ct);
+            DbResult<UserProfile> dbResult = await userProfileDelegate.UpdateAsync(userProfile, true, ct);
             if (dbResult.Status != DbStatusCode.Updated)
             {
                 throw new DatabaseException(dbResult.Message);
@@ -194,10 +165,10 @@ namespace HealthGateway.GatewayApi.Services
         /// <inheritdoc/>
         public async Task UpdateAcceptedTermsAsync(string hdid, Guid termsOfServiceId, CancellationToken ct = default)
         {
-            UserProfile userProfile = await this.userProfileDelegate.GetUserProfileAsync(hdid, ct: ct) ?? throw new NotFoundException(ErrorMessages.UserProfileNotFound);
+            UserProfile userProfile = await userProfileDelegate.GetUserProfileAsync(hdid, ct: ct) ?? throw new NotFoundException(ErrorMessages.UserProfileNotFound);
             userProfile.TermsOfServiceId = termsOfServiceId;
 
-            DbResult<UserProfile> result = await this.userProfileDelegate.UpdateAsync(userProfile, ct: ct);
+            DbResult<UserProfile> result = await userProfileDelegate.UpdateAsync(userProfile, ct: ct);
             if (result.Status != DbStatusCode.Updated)
             {
                 throw new DatabaseException(result.Message);
@@ -211,18 +182,18 @@ namespace HealthGateway.GatewayApi.Services
             string smsNumber,
             CancellationToken ct = default)
         {
-            Guid? termsOfServiceId = await this.legalAgreementService.GetActiveLegalAgreementId(LegalAgreementType.TermsOfService, ct);
-            UserProfileModel userProfileModel = this.mappingService.MapToUserProfileModel(userProfile, termsOfServiceId);
+            Guid? termsOfServiceId = await legalAgreementService.GetActiveLegalAgreementId(LegalAgreementType.TermsOfService, ct);
+            UserProfileModel userProfileModel = mappingService.MapToUserProfileModel(userProfile, termsOfServiceId);
             userProfileModel.Email = emailAddress;
             userProfileModel.SmsNumber = smsNumber;
 
-            DateTime? latestTourChangeDateTime = await this.applicationSettingsService.GetLatestTourChangeDateTimeAsync(ct);
+            DateTime? latestTourChangeDateTime = await applicationSettingsService.GetLatestTourChangeDateTimeAsync(ct);
             userProfileModel.HasTourUpdated = historyCollection.Count != 0 &&
                                               latestTourChangeDateTime != null &&
                                               historyCollection.Max(x => x.LastLoginDateTime) < latestTourChangeDateTime;
 
-            userProfileModel.BlockedDataSources = await this.patientRepository.GetDataSourcesAsync(userProfile.HdId, ct);
-            userProfileModel.Preferences = await this.userPreferenceService.GetUserPreferencesAsync(userProfileModel.HdId, ct);
+            userProfileModel.BlockedDataSources = await patientRepository.GetDataSourcesAsync(userProfile.HdId, ct);
+            userProfileModel.Preferences = await userPreferenceService.GetUserPreferencesAsync(userProfileModel.HdId, ct);
             userProfileModel.LastLoginDateTimes = [userProfile.LastLoginDateTime, ..historyCollection.Select(h => h.LastLoginDateTime)];
 
             return userProfileModel;
@@ -233,7 +204,7 @@ namespace HealthGateway.GatewayApi.Services
             if (!string.IsNullOrWhiteSpace(emailAddress))
             {
                 Dictionary<string, string> keyValues = new() { [EmailTemplateVariable.Host] = this.emailTemplateConfig.Host };
-                await this.emailQueueService.QueueNewEmailAsync(emailAddress, emailTemplateName, keyValues, ct: ct);
+                await emailQueueService.QueueNewEmailAsync(emailAddress, emailTemplateName, keyValues, ct: ct);
             }
         }
     }
