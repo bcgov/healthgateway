98703abbd239100d61271eab8b453d0c8347232d
BrianMaki <brian.jang.developer@outlook.com>
2025-02-27 13:23:01 -0800

    Add My Health Apps overlay to main layout for AB#16893.

--- Tools/Dev/Auth/client/Layout/MainLayout.razor.cs
+++ Tools/Dev/Auth/client/Layout/MainLayout.razor.cs
@@ -6,18 +6,26 @@ namespace RegionalPortal.Layout;
 public partial class MainLayout
 {
     private bool DrawerOpen { get; set; } = true;
+    private bool ShowHealthAppsPopup { get; set; }
 
     [Inject]
-    private NavigationManager NavigationManager { get; set; } = default!;
-    
+    private NavigationManager NavigationManager { get; set; } = null!;
+
+    [Inject]
+    private IConfiguration Configuration { get; set; } = null!;
+
+    private string TimelineUrl => $"{Configuration["SFBaseURL"]}/timeline";
+
     private void DrawerToggle()
     {
         this.DrawerOpen = !this.DrawerOpen;
     }
-    private async Task ToggleThemeAsync()
+
+    private void ToggleMyHealthApps()
     {
+        this.ShowHealthAppsPopup = !this.ShowHealthAppsPopup;
     }
-    
+
     private void LogOut()
     {
         this.NavigationManager.NavigateToLogout("authentication/logout");
