ce4590d50c575a8960d7493a4b2e1439230efbae
Stephen Laws <stephen.laws@nomostech.com>
2025-12-18 16:07:09 -0800

    Add valid logout redirects for Icarus client

--- Tools/KeyCloak/Terraform/client_icarus.tf
+++ Tools/KeyCloak/Terraform/client_icarus.tf
@@ -1,17 +1,18 @@
 resource "keycloak_openid_client" "icarus_client" {
-  realm_id                     = data.keycloak_realm.hg_realm.id
-  client_id                    = var.client_icarus.id
-  name                         = "VPP Client - ${var.environment.name}"
-  description                  = "VPP Client"
-  enabled                      = true
-  access_type                  = "CONFIDENTIAL"
-  login_theme                  = "bcgov-no-brand"
-  standard_flow_enabled        = true
-  direct_access_grants_enabled = false
-  valid_redirect_uris          = var.client_icarus.valid_redirects
-  web_origins                  = var.client_icarus.web_origins
-  full_scope_allowed           = false
-  access_token_lifespan        = var.client_icarus.token_lifespan
+  realm_id                        = data.keycloak_realm.hg_realm.id
+  client_id                       = var.client_icarus.id
+  name                            = "VPP Client - ${var.environment.name}"
+  description                     = "VPP Client"
+  enabled                         = true
+  access_type                     = "CONFIDENTIAL"
+  login_theme                     = "bcgov-no-brand"
+  standard_flow_enabled           = true
+  direct_access_grants_enabled    = false
+  valid_redirect_uris             = var.client_icarus.valid_redirects
+  valid_post_logout_redirect_uris = var.client_icarus.valid_logout_redirects
+  web_origins                     = var.client_icarus.web_origins
+  full_scope_allowed              = false
+  access_token_lifespan           = var.client_icarus.token_lifespan
   authentication_flow_binding_overrides {
     browser_id = keycloak_authentication_flow.icarus_login.id
   }
