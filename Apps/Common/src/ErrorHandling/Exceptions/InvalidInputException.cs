c96a0f42e17d8354c80f2cb12a9e87726de2aeb1
BrianMaki <brian.jang.developer@outlook.com>
2026-03-10 16:48:30 -0700

    Add controller and service for drug lookup by DIN for AB#17031

---
+++ Apps/Common/src/ErrorHandling/Exceptions/InvalidInputException.cs
@@ -0,0 +1,42 @@
+// -------------------------------------------------------------------------
+//  Copyright © 2019 Province of British Columbia
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------
+namespace HealthGateway.Common.ErrorHandling.Exceptions
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Net;
+
+    /// <summary>
+    /// <see cref="InvalidInputException"/> is used when a request contains invalid or malformed input provided by the client.
+    /// The default problem type is <see cref="ProblemType.InvalidInput"/>. The associated status code is
+    /// <see cref="HttpStatusCode.BadRequest"/> (400).
+    /// </summary>
+    [SuppressMessage("Design", "CA1032:Implement standard exception constructors", Justification = "The constructors should be explicit")]
+    [ExcludeFromCodeCoverage]
+    public class InvalidInputException : HealthGatewayException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidInputException"/> class.
+        /// </summary>
+        /// <param name="message">Error message detailing the failure in question.</param>
+        /// <param name="innerException">An internal exception that results in a higher order failure.</param>
+        public InvalidInputException(string? message = null, Exception? innerException = null)
+            : base(message, innerException)
+        {
+            this.ProblemType = ProblemType.InvalidInput;
+        }
+    }
+}
