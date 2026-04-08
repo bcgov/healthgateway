06bea0ea1038e2920f5bf0ea7d6d93cbb0797aee
Mike Lyttle <mikelyttle@gmail.com>
2026-04-08 14:31:03 -0700

    Finalize removal of vaccine card from home page AB#16982

--- Testing/functional/tests/cypress/integration/e2e/user/quickLinks.js
+++ Testing/functional/tests/cypress/integration/e2e/user/quickLinks.js
@@ -420,64 +420,6 @@ describe("Disabling organ donor services", () => {
     });
 });
 
-// Vaccine Card Quick Link tests
-const vaccineCardQuickLinkCardSelector = "[data-testid=bc-vaccine-card-card]";
-const vaccineCardAddQuickLinkChipSelector =
-    "[data-testid=bc-vaccine-card-filter]";
-
-describe("Vaccine Card Quick Link", () => {
-    // AB#16941 - Skip test as Vaccine Card has been removed from Home
-    it.skip("Remove and Add Vaccine Card Quick Link", () => {
-        cy.configureSettings({});
-
-        cy.login(
-            Cypress.env("keycloak.username"),
-            Cypress.env("keycloak.password"),
-            AuthMethod.KeyCloak,
-            homePath
-        );
-
-        cy.log("Verifying vaccine quick link card is present");
-        cy.get(vaccineCardQuickLinkCardSelector).should(
-            "be.visible",
-            "be.enabled"
-        );
-
-        cy.log("Removing vaccine card quick link");
-        cy.get(vaccineCardQuickLinkCardSelector).within(() => {
-            cy.get(quickLinkMenuButtonSelector)
-                .should("be.visible", "be.enabled")
-                .click();
-        });
-        clickRemoveQuickLinkSelector(true);
-
-        cy.log("Verifying vaccine card quick link no longer exists");
-        cy.get(vaccineCardQuickLinkCardSelector).should("not.exist");
-
-        cy.log("Adding vaccine card quick link");
-        cy.get(addQuickLinkButtonSelector)
-            .should("be.visible")
-            .should("be.enabled")
-            .click();
-        cy.get(vaccineCardAddQuickLinkChipSelector).should("exist").click();
-        cy.get(addQuickLinkSubmitButtonSelector)
-            .should("be.visible")
-            .should("be.enabled")
-            .click();
-
-        cy.log("Verifying add quick link button is disabled");
-        cy.get(addQuickLinkButtonSelector)
-            .should("be.visible")
-            .should("not.be.enabled");
-
-        cy.log("Verifying vaccine quick link card is present");
-        cy.get(vaccineCardQuickLinkCardSelector).should(
-            "be.visible",
-            "be.enabled"
-        );
-    });
-});
-
 // Health Connect Registry Quick Link tests
 const healthConnectQuickLinkCardSelector =
     "[data-testid=health-connect-registry-card]";
