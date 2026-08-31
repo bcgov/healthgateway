2e8049537cbd92e844c74b5966b83fdc49b247b0
BrianMaki <brian.jang.developer@outlook.com>
2026-08-31 16:41:21 -0700

    Split ui and e2e functional test pipeline jobs AB#17107

--- Tools/Pipelines/scripts/azp_runE2eFunctionalTests.sh
+++ Tools/Pipelines/scripts/azp_runE2eFunctionalTests.sh
@@ -2,14 +2,6 @@
 set -e
 if [ "$#" -ne 3 ]; then
     echo "Usage: $0 <working_directory> <buildId> <tags>"
-    #Release should use:
-    #   workDir=_FunctionalTests/drop/gateway
-    #   BuildId=$(Release.ReleaseName)-$(Release.AttemptNumber)
-    #   tags=CICD,Release,Dev
-    #Pipeline should use:
-    #   workDir=Testing/functional/tests
-    #   BuildId=$(Build.BuildNumber)-$(System.JobAttempt)
-    #   tags="dev,$(Build.Reason)"
     exit 1
 fi
 
@@ -27,20 +19,20 @@ echo "Installing dependencies"
 npm ci
 
 if [ -z "$IDIR_PASSWORD" ]; then
-  echo "ERROR: The IDIR_PASSWORD variable is not set or is empty."
-  exit 1
+    echo "ERROR: The IDIR_PASSWORD variable is not set or is empty."
+    exit 1
 fi
 
-echo "Running Cypress Functional Tests"
+echo "Running Cypress E2E Functional Tests"
 TZ=America/Vancouver npx cypress run \
   --env "bcsc.password=$BCSC_PW,keycloak.password=$KEYCLOAK_PW,idir.password=$IDIR_PASSWORD,phoneNumber=$PHONENUMBER,keycloak.erebus.client=$KEYCLOAK_EREBUS_CLIENT,keycloak.erebus.secret=$KEYCLOAK_EREBUS_SECRET,keycloak.phsa.client=$KEYCLOAK_PHSA_CLIENT,keycloak.phsa.secret=$KEYCLOAK_PHSA_SECRET" \
   --record \
   --key $CYPRESS_KEY \
   --parallel \
-  --ci-build-id "$buildId" \
-  --group "$buildId" \
+  --ci-build-id "$buildId-e2e" \
+  --group "e2e" \
   --tag "$tags" \
-  --spec "cypress/integration/ui/**/*,cypress/integration/e2e/**/!(auth.js)" \
+  --spec "cypress/integration/e2e/**/!(auth.js)" \
   --headless \
   --browser chrome
 popd
---
+++ Tools/Pipelines/scripts/azp_runUiFunctionalTests.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -e
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <working_directory> <buildId> <tags>"
+    exit 1
+fi
+
+workDir="$1"
+buildId="$2"
+tags="$3"
+
+if [ ! -d "$workDir" ]; then
+    echo "Error: The directory '$workDir' does not exist."
+    exit 1
+fi
+
+pushd "$workDir"
+echo "Installing dependencies"
+npm ci
+
+if [ -z "$IDIR_PASSWORD" ]; then
+    echo "ERROR: The IDIR_PASSWORD variable is not set or is empty."
+    exit 1
+fi
+
+echo "Running Cypress UI Functional Tests"
+TZ=America/Vancouver npx cypress run \
+  --env "bcsc.password=$BCSC_PW,keycloak.password=$KEYCLOAK_PW,idir.password=$IDIR_PASSWORD,phoneNumber=$PHONENUMBER,keycloak.erebus.client=$KEYCLOAK_EREBUS_CLIENT,keycloak.erebus.secret=$KEYCLOAK_EREBUS_SECRET,keycloak.phsa.client=$KEYCLOAK_PHSA_CLIENT,keycloak.phsa.secret=$KEYCLOAK_PHSA_SECRET" \
+  --record \
+  --key $CYPRESS_KEY \
+  --parallel \
+  --ci-build-id "$buildId-ui" \
+  --group "ui" \
+  --tag "$tags" \
+  --spec "cypress/integration/ui/**/!(auth.js)" \
+  --headless \
+  --browser chrome
+popd
