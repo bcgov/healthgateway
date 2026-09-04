f66296b802b3dbf3ee7f91a70d7eebd0f6bbc0d6
BrianMaki <brian.jang.developer@outlook.com>
2026-09-04 12:30:21 -0700

    Stabilize Cypress functional test workflows AB#17111

--- Testing/functional/tests/cypress/integration/e2e/user/acceptTermsOfService.js
+++ Testing/functional/tests/cypress/integration/e2e/user/acceptTermsOfService.js
@@ -30,7 +30,25 @@ describe("Need to accept terms of service", () => {
         cy.get("[data-testid=accept-tos-checkbox] input")
             .should("be.enabled")
             .check({ force: true });
+
+        cy.intercept(
+            "PUT",
+            "**/UserProfile/*/acceptedterms?api-version=2.0"
+        ).as("updateAcceptedTerms");
+        cy.intercept("GET", "**/UserProfile/*?api-version=2.0").as(
+            "getUpdatedUserProfile"
+        );
+
         cy.get("[data-testid=continue-btn]").should("be.enabled").click();
-        cy.url().should("include", "/home");
+        cy.wait("@updateAcceptedTerms", { timeout: defaultTimeout })
+            .its("response.statusCode")
+            .should("eq", 200);
+        cy.wait("@getUpdatedUserProfile", { timeout: defaultTimeout })
+            .its("response.statusCode")
+            .should("eq", 200);
+        cy.location("pathname", { timeout: defaultTimeout }).should(
+            "eq",
+            "/home"
+        );
     });
 });
